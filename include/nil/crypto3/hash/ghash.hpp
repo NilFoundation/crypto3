d675e444887f698dd6d912ab7d11a229327c5751
Mikhail Komarov <nemo@nil.foundation>
2019-10-03 23:14:50 +0300

    Minor ghash updates done #3

---
+++ include/nil/crypto3/hash/ghash.hpp
@@ -0,0 +1,23 @@
+//---------------------------------------------------------------------------//
+// Copyright (c) 2019 Nil Foundation AG
+// Copyright (c) 2019 Mikhail Komarov <nemo@nil.foundation>
+//
+// Distributed under the Boost Software License, Version 1.0
+// See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt
+//---------------------------------------------------------------------------//
+
+#ifndef CRYPTO3_HASH_GHASH_HPP
+#define CRYPTO3_HASH_GHASH_HPP
+
+namespace nil {
+    namespace crypto3 {
+        namespace hash {
+            class ghash {
+
+            };
+        }
+    }
+}
+
+#endif    // CRYPTO3_GHASH_HPP
---
+++ test/ghash.cpp
@@ -0,0 +1,51 @@
+//---------------------------------------------------------------------------//
+// Copyright (c) 2018-2019 Nil Foundation AG
+// Copyright (c) 2018-2019 Mikhail Komarov <nemo@nil.foundation>
+//
+// Distributed under the Boost Software License, Version 1.0
+// See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt
+//---------------------------------------------------------------------------//
+
+#define BOOST_TEST_MODULE ghash_test
+
+#include <nil/crypto3/hash/algorithm/hash.hpp>
+
+#include <nil/crypto3/hash/ghash.hpp>
+#include <nil/crypto3/hash/hash_state.hpp>
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/data/monomorphic.hpp>
+
+#include <boost/static_assert.hpp>
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+#include <cstdio>
+#include <cstring>
+
+using namespace nil::crypto3::hash;
+
+namespace boost {
+    namespace test_tools {
+        namespace tt_detail {
+            template<template<typename, typename> class P, typename K, typename V>
+            struct print_log_value<P<K, V>> {
+                void operator()(std::ostream&, P<K, V> const&) {
+                }
+            };
+        }    // namespace tt_detail
+    }        // namespace test_tools
+}    // namespace boost
+
+BOOST_TEST_DONT_PRINT_LOG_VALUE(ghash::construction::type::digest_type)
+
+BOOST_AUTO_TEST_SUITE(ghash_test_suite)
+
+BOOST_AUTO_TEST_CASE(ghash_range_hash) {
+}
+
+BOOST_AUTO_TEST_SUITE_END()
