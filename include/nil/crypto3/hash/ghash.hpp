34c2cb5bff98b21937723f9571968661df106f71
Mikhail Komarov <nemo@nil.foundation>
2019-10-29 04:18:27 +0300

    GHash initial implementation started #3

--- include/nil/crypto3/hash/ghash.hpp
+++ include/nil/crypto3/hash/ghash.hpp
@@ -10,7 +10,7 @@
 #ifndef CRYPTO3_HASH_GHASH_HPP
 #define CRYPTO3_HASH_GHASH_HPP
 
-#include <nil/crypto3/hash/detail/ghash_policy.hpp>
+#include <nil/crypto3/hash/detail/ghash/ghash_policy.hpp>
 
 #include <nil/crypto3/hash/detail/merkle_damgard_construction.hpp>
 #include <nil/crypto3/hash/detail/merkle_damgard_stream_processor.hpp>
@@ -21,8 +21,20 @@ namespace nil {
             template<template<typename> class Allocator>
             class ghash_compressor {
                 typedef detail::ghash_policy<Allocator> policy_type;
-            public:
 
+            public:
+                constexpr static const std::size_t rounds = policy_type::rounds;
+
+                constexpr static const std::size_t word_bits = policy_type::word_bits;
+                typedef typename policy_type::word_type word_type;
+
+                constexpr static const std::size_t block_bits = policy_type::block_bits;
+                constexpr static const std::size_t block_words = policy_type::block_words;
+                typedef typename policy_type::block_type block_type;
+
+                constexpr static const std::size_t state_bits = policy_type::state_bits;
+                constexpr static const std::size_t state_words = policy_type::state_words;
+                typedef typename policy_type::state_type state_type;
             };
 
             /*!
