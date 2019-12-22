f6aab37fe308f5bccf4b72fe6e7a574c3a4216a2
Mikhail Komarov <nemo@nil.foundation>
2019-12-22 09:23:20 +0300

    Accumulator set definition updated #1

--- include/nil/crypto3/mac/mac_state.hpp
+++ include/nil/crypto3/mac/mac_state.hpp
@@ -18,19 +18,9 @@
 namespace nil {
     namespace crypto3 {
         namespace mac {
-            /*!
-             * @brief Cipher state managing container
-             *
-             * Meets the requirements of CipherStateContainer, ConceptContainer, SequenceContainer, Container
-             *
-             * @tparam MessageAuthenticationCode
-             * @tparam Endian
-             * @tparam ValueBits
-             * @tparam LengthBits
-             */
             template<typename MessageAuthenticationCode>
-            using mac_accumulator = boost::accumulators::accumulator_set<
-                mac::static_digest<MessageAuthenticationCode::input_block_bits>,
+            using accumulator_set = boost::accumulators::accumulator_set<
+                static_digest<MessageAuthenticationCode::input_block_bits>,
                 boost::accumulators::features<accumulators::tag::mac<MessageAuthenticationCode>>>;
         }    // namespace mac
     }        // namespace crypto3
