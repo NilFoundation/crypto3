c920ea6152977d68a7c0064fcfef3027a2a7bd8c
a1falcon <alexandro9608@gmail.com>
2020-04-19 16:10:09 +0300

    Hex tests rewritten #2

--- test/hex.cpp
+++ test/hex.cpp
@@ -8,10 +8,14 @@
 
 #define BOOST_TEST_MODULE hex_encoding_test
 
-#include <unordered_map>
+#include <string>
+#include <iterator>
 
 #include <boost/test/unit_test.hpp>
 #include <boost/test/data/test_case.hpp>
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
 #include <nil/crypto3/codec/algorithm/encode.hpp>
 #include <nil/crypto3/codec/algorithm/decode.hpp>
@@ -33,118 +37,103 @@ namespace boost {
     }        // namespace test_tools
 }
 
-static const std::unordered_map<std::string, std::string> upper_valid_data_map = {{"ABCD", "41424344"},
-                                                                                  {"AAAA", "41414141"},
-                                                                                  {"f", "66"},
-                                                                                  {"foo", "666F6F"},
-                                                                                  {"foofoo", "666F6F666F6F"}};
+const char *test_data = "data/hex.json";
 
-static const std::unordered_map<std::string, std::string> lower_valid_data_map = {{"ABCD", "41424344"},
-                                                                                  {"AAAA", "41414141"},
-                                                                                  {"f", "66"},
-                                                                                  {"foofoo", "666f6f666f6f"}};
+boost::property_tree::ptree mode_data(const char *mode) {
+    boost::property_tree::ptree root_data;
+    boost::property_tree::read_json(test_data, root_data);
+    boost::property_tree::ptree string_data = root_data.get_child(mode);
 
-static const std::unordered_map<std::string, std::string> invalid_data_map;
+    return string_data;
+}
 
 BOOST_AUTO_TEST_SUITE(hex_codec_algorithm_test_suite)
 
-BOOST_DATA_TEST_CASE(hex_upper_range_range_encode, boost::unit_test::data::make(upper_valid_data_map), array_element) {
+BOOST_DATA_TEST_CASE(hex_upper_range_range_encode, mode_data("upper_mode"), array_element) {
     std::string result = encode<hex<mode::upper>>(array_element.first);
-    BOOST_CHECK_EQUAL(result, array_element.second);
+    BOOST_CHECK_EQUAL(result, array_element.second.data());
 }
 
-BOOST_DATA_TEST_CASE(hex_upper_range_range_decode, boost::unit_test::data::make(upper_valid_data_map), array_element) {
-    std::string result = decode<hex<mode::upper>>(array_element.second);
+BOOST_DATA_TEST_CASE(hex_upper_range_range_decode, mode_data("upper_mode"), array_element) {
+    std::string result = decode<hex<mode::upper>>(array_element.second.data());
     BOOST_CHECK_EQUAL(result, array_element.first);
 }
 
-BOOST_DATA_TEST_CASE(hex_upper_iterator_range_encode, boost::unit_test::data::make(upper_valid_data_map),
-                     array_element) {
+BOOST_DATA_TEST_CASE(hex_upper_iterator_range_encode, mode_data("upper_mode"), array_element) {
     std::string result = encode<hex<mode::upper>>(array_element.first.begin(), array_element.first.end());
-    BOOST_CHECK_EQUAL(result, array_element.second);
+    BOOST_CHECK_EQUAL(result, array_element.second.data());
 }
 
-BOOST_DATA_TEST_CASE(hex_upper_iterator_range_decode, boost::unit_test::data::make(upper_valid_data_map),
-                     array_element) {
-    std::string result = decode<hex<mode::upper>>(array_element.second.begin(), array_element.second.end());
+BOOST_DATA_TEST_CASE(hex_upper_iterator_range_decode, mode_data("upper_mode"), array_element) {
+    std::string result = decode<hex<mode::upper>>(array_element.second.data().begin(), array_element.second.data().end());
     BOOST_CHECK_EQUAL(result, array_element.first);
 }
 
-BOOST_DATA_TEST_CASE(hex_upper_range_iterator_encode, boost::unit_test::data::make(upper_valid_data_map),
-                     array_element) {
+BOOST_DATA_TEST_CASE(hex_upper_range_iterator_encode, mode_data("upper_mode"), array_element) {
     std::string result;
     encode<hex<mode::upper>>(array_element.first, std::back_inserter(result));
-    BOOST_CHECK_EQUAL(result, array_element.second);
+    BOOST_CHECK_EQUAL(result, array_element.second.data());
 }
 
-BOOST_DATA_TEST_CASE(hex_upper_range_iterator_decode, boost::unit_test::data::make(upper_valid_data_map),
-                     array_element) {
+BOOST_DATA_TEST_CASE(hex_upper_range_iterator_decode, mode_data("upper_mode"), array_element) {
     std::string result;
-    decode<hex<mode::upper>>(array_element.second, std::back_inserter(result));
+    decode<hex<mode::upper>>(array_element.second.data(), std::back_inserter(result));
     BOOST_CHECK_EQUAL(result, array_element.first);
 }
 
-BOOST_DATA_TEST_CASE(hex_upper_iterator_iterator_encode, boost::unit_test::data::make(upper_valid_data_map),
-                     array_element) {
+BOOST_DATA_TEST_CASE(hex_upper_iterator_iterator_encode, mode_data("upper_mode"), array_element) {
     std::string result;
     encode<hex<mode::upper>>(array_element.first.begin(), array_element.first.end(), std::back_inserter(result));
-    BOOST_CHECK_EQUAL(result, array_element.second);
+    BOOST_CHECK_EQUAL(result, array_element.second.data());
 }
 
-BOOST_DATA_TEST_CASE(hex_upper_iterator_iterator_decode, boost::unit_test::data::make(upper_valid_data_map),
-                     array_element) {
+BOOST_DATA_TEST_CASE(hex_upper_iterator_iterator_decode, mode_data("upper_mode"), array_element) {
     std::string result;
-    decode<hex<mode::upper>>(array_element.second.begin(), array_element.second.end(), std::back_inserter(result));
+    decode<hex<mode::upper>>(array_element.second.data().begin(), array_element.second.data().end(), std::back_inserter(result));
     BOOST_CHECK_EQUAL(result, array_element.first);
 }
 
-BOOST_DATA_TEST_CASE(hex_lower_range_range_encode, boost::unit_test::data::make(lower_valid_data_map), array_element) {
+BOOST_DATA_TEST_CASE(hex_lower_range_range_encode, mode_data("lower_mode"), array_element) {
     std::string result = encode<hex<mode::lower>>(array_element.first);
-    BOOST_CHECK_EQUAL(result, array_element.second);
+    BOOST_CHECK_EQUAL(result, array_element.second.data());
 }
 
-BOOST_DATA_TEST_CASE(hex_lower_range_range_decode, boost::unit_test::data::make(lower_valid_data_map), array_element) {
-    std::string result = decode<hex<mode::lower>>(array_element.second);
+BOOST_DATA_TEST_CASE(hex_lower_range_range_decode, mode_data("lower_mode"), array_element) {
+    std::string result = decode<hex<mode::lower>>(array_element.second.data());
     BOOST_CHECK_EQUAL(result, array_element.first);
 }
 
-BOOST_DATA_TEST_CASE(hex_lower_iterator_range_encode, boost::unit_test::data::make(lower_valid_data_map),
-                     array_element) {
+BOOST_DATA_TEST_CASE(hex_lower_iterator_range_encode, mode_data("lower_mode"), array_element) {
     std::string result = encode<hex<mode::lower>>(array_element.first.begin(), array_element.first.end());
-    BOOST_CHECK_EQUAL(result, array_element.second);
+    BOOST_CHECK_EQUAL(result, array_element.second.data());
 }
 
-BOOST_DATA_TEST_CASE(hex_lower_iterator_range_decode, boost::unit_test::data::make(lower_valid_data_map),
-                     array_element) {
-    std::string result = decode<hex<mode::lower>>(array_element.second.begin(), array_element.second.end());
+BOOST_DATA_TEST_CASE(hex_lower_iterator_range_decode, mode_data("lower_mode"), array_element) {
+    std::string result = decode<hex<mode::lower>>(array_element.second.data().begin(), array_element.second.data().end());
     BOOST_CHECK_EQUAL(result, array_element.first);
 }
 
-BOOST_DATA_TEST_CASE(hex_lower_range_iterator_encode, boost::unit_test::data::make(lower_valid_data_map),
-                     array_element) {
+BOOST_DATA_TEST_CASE(hex_lower_range_iterator_encode, mode_data("lower_mode"), array_element) {
     std::string result;
     encode<hex<mode::lower>>(array_element.first, std::back_inserter(result));
-    BOOST_CHECK_EQUAL(result, array_element.second);
+    BOOST_CHECK_EQUAL(result, array_element.second.data());
 }
 
-BOOST_DATA_TEST_CASE(hex_lower_range_iterator_decode, boost::unit_test::data::make(lower_valid_data_map),
-                     array_element) {
+BOOST_DATA_TEST_CASE(hex_lower_range_iterator_decode, mode_data("lower_mode"), array_element) {
     std::string result;
-    decode<hex<mode::lower>>(array_element.second, std::back_inserter(result));
+    decode<hex<mode::lower>>(array_element.second.data(), std::back_inserter(result));
     BOOST_CHECK_EQUAL(result, array_element.first);
 }
 
-BOOST_DATA_TEST_CASE(hex_lower_iterator_iterator_encode, boost::unit_test::data::make(lower_valid_data_map),
-                     array_element) {
+BOOST_DATA_TEST_CASE(hex_lower_iterator_iterator_encode, mode_data("lower_mode"), array_element) {
     std::string result;
     encode<hex<mode::lower>>(array_element.first.begin(), array_element.first.end(), std::back_inserter(result));
-    BOOST_CHECK_EQUAL(result, array_element.second);
+    BOOST_CHECK_EQUAL(result, array_element.second.data());
 }
 
-BOOST_DATA_TEST_CASE(hex_lower_iterator_iterator_decode, boost::unit_test::data::make(lower_valid_data_map),
-                     array_element) {
+BOOST_DATA_TEST_CASE(hex_lower_iterator_iterator_decode, mode_data("lower_mode"), array_element) {
     std::string result;
-    decode<hex<mode::lower>>(array_element.second.begin(), array_element.second.end(), std::back_inserter(result));
+    decode<hex<mode::lower>>(array_element.second.data().begin(), array_element.second.data().end(), std::back_inserter(result));
     BOOST_CHECK_EQUAL(result, array_element.first);
 }
 
@@ -152,20 +141,20 @@ BOOST_AUTO_TEST_SUITE_END()
 
 // BOOST_AUTO_TEST_SUITE(hex_codec_adaptor_test_suite)
 //
-//    BOOST_DATA_TEST_CASE(hex_upper_range_encode, boost::unit_test::data::make(upper_valid_data_map), array_element) {
+//    BOOST_DATA_TEST_CASE(hex_upper_range_encode, mode_data("upper_mode"), array_element) {
 //        typedef hex<mode::upper> Codec;
 //        typedef typename range_compressor_traits<typename Codec::stream_encoder_type,
 //                decltype(array_element.first)>::type CompressorState;
 //
-//        BOOST_CHECK_EQUAL((array_element.first | adaptors::encoded<Codec, CompressorState>()), array_element.second);
+//        BOOST_CHECK_EQUAL((array_element.first | adaptors::encoded<Codec, CompressorState>()), array_element.second.data());
 //    }
 //
-//    BOOST_DATA_TEST_CASE(hex_upper_range_decode, boost::unit_test::data::make(upper_valid_data_map), array_element) {
+//    BOOST_DATA_TEST_CASE(hex_upper_range_decode, mode_data("upper_mode"), array_element) {
 //        typedef hex<mode::upper> Codec;
 //        typedef typename range_compressor_traits<typename Codec::stream_encoder_type,
 //                decltype(array_element.first)>::type CompressorState;
 //
-//        BOOST_CHECK_EQUAL((array_element.second | adaptors::decoded<Codec, CompressorState>()), array_element.first);
+//        BOOST_CHECK_EQUAL((array_element.second.data() | adaptors::decoded<Codec, CompressorState>()), array_element.first);
 //    }
 //
 // BOOST_AUTO_TEST_SUITE_END()
