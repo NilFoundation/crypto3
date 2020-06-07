4f829d17565ea9645ebb9ad43c0a891d5cf1fc17
a1falcon <alexandro9608@gmail.com>
2020-06-08 00:34:55 +0300

    Some pack_n functions added #30

--- include/nil/crypto3/detail/pack.hpp
+++ include/nil/crypto3/detail/pack.hpp
@@ -18,8 +18,8 @@
 
 #include <boost/static_assert.hpp>
 #include <boost/utility/enable_if.hpp>
-#include <boost/type_traits/is_same.hpp>
 #include <boost/predef/other/endian.h>
+#include <boost/type_traits/is_same.hpp>
 
 #include <algorithm>
 #include <iterator>
@@ -29,50 +29,91 @@ namespace nil {
     namespace crypto3 {
         namespace detail {
 
-            template<int UnitBits, int InputBits, int OutputBits, typename InT, typename OutT>
+            template<int UnitBits, int ValueBits, typename InT, typename OutT>
             struct host_can_memcpy {
-                constexpr static const bool value = !(UnitBits % CHAR_BIT) && InputBits >= UnitBits &&
-                                                    OutputBits >= UnitBits && sizeof(InT) * CHAR_BIT == InputBits &&
-                                                    sizeof(OutT) * CHAR_BIT == OutputBits;
+                constexpr static const bool value = !(UnitBits % CHAR_BIT) && ValueBits >= UnitBits &&
+                                                    sizeof(InT) * CHAR_BIT == ValueBits &&
+                                                    sizeof(OutT) * CHAR_BIT == ValueBits;
             };
 
-            template<typename InputEndianness, typename OutputEndianness, int InputBits, int OutputBits, typename InT,
-                     typename OutT>
+            template<typename Endianness, int ValueBits, typename InT, typename OutT>
             struct can_memcpy {
-                constexpr static const bool value = InputBits == OutputBits && sizeof(InT) == sizeof(OutT);
+                constexpr static const bool value = sizeof(InT) == sizeof(OutT);
             };
 
-            template<int UnitBits, int InputBits, int OutputBits, typename InT, typename OutT>
-            struct can_memcpy<stream_endian::host_unit<UnitBits>, stream_endian::host_unit<UnitBits>, InputBits,
-                              OutputBits, InT, OutT> : host_can_memcpy<UnitBits, InputBits, OutputBits, InT, OutT> { };
+            template<int UnitBits, int ValueBits, typename InT, typename OutT>
+            struct can_memcpy<stream_endian::host_unit<UnitBits>, ValueBits, InT, OutT> 
+                : host_can_memcpy<UnitBits, ValueBits, InT, OutT> { };
 
 #ifdef CRYPTO3_TARGET_CPU_IS_LITTLE_ENDIAN
+            template<int UnitBits, int ValueBits, typename InT, typename OutT>
+            struct can_memcpy<stream_endian::little_unit_big_bit<UnitBits>, ValueBits, InT, OutT>
+                : host_can_memcpy<UnitBits, ValueBits, InT, OutT> { };
+            template<int UnitBits, int ValueBits, typename InT, typename OutT>
+            struct can_memcpy<stream_endian::little_unit_little_bit<UnitBits>, ValueBits, InT, OutT>
+                : host_can_memcpy<UnitBits, ValueBits, InT, OutT> { };
+
+#elif defined(CRYPTO3_TARGET_CPU_IS_BIG_ENDIAN)
+            template<int UnitBits, int ValueBits, typename InT, typename OutT>
+            struct can_memcpy<stream_endian::big_unit_big_bit<UnitBits>, ValueBits, InT, OutT>
+                : host_can_memcpy<UnitBits, ValueBits, InT, OutT> { };
             template<int UnitBits, int InputBits, int OutputBits, typename InT, typename OutT>
-            struct can_memcpy<stream_endian::little_unit_big_bit<UnitBits>,
-                              stream_endian::little_unit_big_bit<UnitBits>, InputBits, OutputBits, InT, OutT>
-                : host_can_memcpy<UnitBits, InputBits, OutputBits, InT, OutT> { };
-            template<int UnitBits, int InputBits, int OutputBits, typename InT, typename OutT>
-            struct can_memcpy<stream_endian::little_unit_little_bit<UnitBits>,
-                              stream_endian::little_unit_little_bit<UnitBits>, InputBits, OutputBits, InT, OutT>
-                : host_can_memcpy<UnitBits, InputBits, OutputBits, InT, OutT> { };
+            struct can_memcpy<stream_endian::big_unit_little_bit<UnitBits>, ValueBits, InT, OutT>
+                : host_can_memcpy<UnitBits, ValueBits, InT, OutT> { };
 #endif
 
-#ifdef CRYPTO3_TARGET_CPU_IS_BIG_ENDIAN
-            template<int UnitBits, int InputBits, int OutputBits, typename InT, typename OutT>
-            struct can_memcpy<stream_endian::big_unit_big_bit<UnitBits>, 
-                              stream_endian::big_unit_big_bit<UnitBits>, InputBits, OutputBits, InT, OutT>
-                : host_can_memcpy<UnitBits, InputBits, OutputBits, InT, OutT> { };
-            template<int UnitBits, int InputBits, int OutputBits, typename InT, typename OutT>
-            struct can_memcpy<stream_endian::big_unit_little_bit<UnitBits>,
-                              stream_endian::big_unit_little_bit<UnitBits>, InputBits, OutputBits, InT, OutT>
-                : host_can_memcpy<UnitBits, InputBits, OutputBits, InT, OutT> { };
-#endif
+            template<typename InputIterator, typename OutputIterator>
+            struct can_memcpy_itr {
+                constexpr static bool const value = 
+                    boost::is_same<typename std::iterator_traits<InputIterator>::iterator_category, 
+                    std::random_access_iterator_tag>::value && 
+                    boost::is_same<typename std::iterator_traits<OutputIterator>::iterator_category, 
+                    std::random_access_iterator_tag>::value;
+            };
 
             template<typename InputEndianness, typename OutputEndianness, std::size_t InputValueBits,
                      std::size_t OutputValueBits,
                      bool Implode = (InputValueBits < OutputValueBits),
                      bool Explode = (InputValueBits > OutputValueBits)>
             struct packer { };
+
+            template<int UnitBits, template<int> class Endian, std::size_t ValueBits>
+            struct packer<Endian<UnitBits>, Endian<UnitBits>, ValueBits, ValueBits, false, false> {
+
+                typedef Endian<UnitBits> Endianness;
+
+                template<typename InputType, typename OutputType, typename Dummy = void>
+                inline static typename boost::enable_if_c<
+                    can_memcpy<Endianness, ValueBits, InputType, OutputType>::value, Dummy>::type
+                    pack_n(InputType const *in, size_t n, OutputType *out) {
+                    std::memcpy(out, in, n * sizeof(InputType));
+                }
+
+                template<typename InputType, typename OutputType, typename Dummy = void>
+                inline static typename boost::enable_if_c<
+                    can_memcpy<Endianness, ValueBits, InputType, OutputType>::value, Dummy>::type
+                    pack_n(InputType *in, size_t n, OutputType *out) {
+                    std::memcpy(out, in, n * sizeof(InputType));
+                }
+
+                template<typename InputIterator, typename OutputIterator, typename Dummy = void>
+                inline static typename boost::enable_if_c<
+                    can_memcpy<Endianness, ValueBits, typename std::iterator_traits<InputIterator>::value_type,
+                               typename std::iterator_traits<OutputIterator>::value_type>::value &&
+                    can_memcpy_itr<InputIterator, OutputIterator>::value, Dummy>::type
+                    pack(InputIterator first, InputIterator last, OutputIterator out) {
+                    return pack_n(&(*first), std::distance(first, last), &(*out));
+                }
+
+                template<typename InputIterator, typename OutputIterator, typename Dummy = void>
+                inline static typename boost::enable_if_c<
+                    !can_memcpy<Endianness, ValueBits, typename std::iterator_traits<InputIterator>::value_type,
+                               typename std::iterator_traits<OutputIterator>::value_type>::value || 
+                    !can_memcpy_itr<InputIterator, OutputIterator>::value, Dummy>::type 
+                    pack(InputIterator first, InputIterator last, OutputIterator out) {
+                    std::copy(first, last, out);
+                }
+            };
 
             template<int UnitBits, template<int> class InputEndian, 
                      template<int> class OutputEndian, std::size_t ValueBits>
@@ -84,17 +125,8 @@ namespace nil {
                 typedef unit_reverser<InputEndianness, OutputEndianness, UnitBits> units_reverser;
                 typedef bit_reverser<InputEndianness, OutputEndianness, UnitBits> bits_reverser;
 
-                template<typename InputIterator, typename OutputIterator, typename Dummy = void>
-                inline static typename boost::enable_if_c<
-                    boost::is_same<InputEndianness, OutputEndianness>::value, Dummy>::type 
-                    pack(InputIterator first, InputIterator last, OutputIterator out) {
-                    std::copy(first, last, out);
-                }
-
-                template<typename InputIterator, typename OutputIterator, typename Dummy = void>
-                inline static typename boost::enable_if_c<
-                    !boost::is_same<InputEndianness, OutputEndianness>::value, Dummy>::type 
-                    pack(InputIterator first, InputIterator last, OutputIterator out) {
+                template<typename InputIterator, typename OutputIterator>
+                inline static void pack(InputIterator first, InputIterator last, OutputIterator out) {
                     std::transform(first, last, out, 
                         [](typename std::iterator_traits<InputIterator>::value_type const &elem) {
                         return units_reverser::reverse(bits_reverser::reverse(elem));});
@@ -104,6 +136,9 @@ namespace nil {
             template<typename InputEndianness, typename OutputEndianness, std::size_t InputValueBits,
                      std::size_t OutputValueBits>
             struct packer<InputEndianness, OutputEndianness, InputValueBits, OutputValueBits, true, false> {
+
+                BOOST_STATIC_ASSERT(!(OutputValueBits % InputValueBits));
+
                 template<typename InputIterator, typename OutputIterator>
                 inline static void pack(InputIterator first, InputIterator last, OutputIterator out) {
                     typedef typename std::iterator_traits<OutputIterator>::value_type OutValue;
@@ -121,6 +156,9 @@ namespace nil {
             template<typename InputEndianness, typename OutputEndianness, std::size_t InputValueBits,
                      std::size_t OutputValueBits>
             struct packer<InputEndianness, OutputEndianness, InputValueBits, OutputValueBits, false, true> {
+
+                BOOST_STATIC_ASSERT(!(InputValueBits % OutputValueBits));
+
                 template<typename InputIterator, typename OutputIterator>
                 inline static void pack(InputIterator first, InputIterator last, OutputIterator out) {
                     typedef typename std::iterator_traits<InputIterator>::value_type InValue;
@@ -145,16 +183,16 @@ namespace nil {
                      typename InputIterator, typename OutputIterator>
             inline void pack_to(InputIterator first, InputIterator last, OutputIterator out) {
 
-#ifdef BOOST_ENDIAN_BIG_BYTE
+#ifdef BOOST_ENDIAN_BIG_BYTE_AVAILABLE
                 typedef packer<stream_endian::big_octet_big_bit, OutputEndianness, 
                         InputValueBits, OutputValueBits> packer;
-#elif defined(BOOST_ENDIAN_LITTLE_BYTE)
+#elif defined(BOOST_ENDIAN_LITTLE_BYTE_AVAILABLE)
                 typedef packer<stream_endian::little_octet_big_bit, OutputEndianness, 
                         InputValueBits, OutputValueBits> packer;
-#elif defined(BOOST_ENDIAN_BIG_WORD)
+#elif defined(BOOST_ENDIAN_BIG_WORD_AVAILABLE)
                 typedef packer<stream_endian::big_unit_big_bit<CRYPTO3_MP_WORD_BITS>, OutputEndianness, 
                         InputValueBits, OutputValueBits> packer;
-#elif defined(BOOST_ENDIAN_LITTLE_WORD)
+#elif defined(BOOST_ENDIAN_LITTLE_WORD_AVAILABLE)
                 typedef packer<stream_endian::little_unit_big_bit<CRYPTO3_MP_WORD_BITS>, OutputEndianness, 
                         InputValueBits, OutputValueBits> packer;
 #else
@@ -169,16 +207,16 @@ namespace nil {
                      typename InputIterator, typename OutputIterator>
             inline void pack_from(InputIterator first, InputIterator last, OutputIterator out) {
 
-#ifdef BOOST_ENDIAN_BIG_BYTE
+#ifdef BOOST_ENDIAN_BIG_BYTE_AVAILABLE
                 typedef packer<InputEndianness, stream_endian::big_octet_big_bit, 
                         InputValueBits, OutputValueBits> packer;
-#elif defined(BOOST_ENDIAN_LITTLE_BYTE)
+#elif defined(BOOST_ENDIAN_LITTLE_BYTE_AVAILABLE)
                 typedef packer<InputEndianness, stream_endian::little_octet_big_bit, 
                         InputValueBits, OutputValueBits> packer;
-#elif defined(BOOST_ENDIAN_BIG_WORD)
+#elif defined(BOOST_ENDIAN_BIG_WORD_AVAILABLE)
                 typedef packer<InputEndianness, stream_endian::big_unit_big_bit<CRYPTO3_MP_WORD_BITS>, 
                         InputValueBits, OutputValueBits> packer;
-#elif defined(BOOST_ENDIAN_LITTLE_WORD)
+#elif defined(BOOST_ENDIAN_LITTLE_WORD_AVAILABLE)
                 typedef packer<InputEndianness, stream_endian::little_unit_big_bit<CRYPTO3_MP_WORD_BITS>, 
                         InputValueBits, OutputValueBits> packer;
 #else
