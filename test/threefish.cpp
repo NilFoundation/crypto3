182d15dd49d8dc03d56b07eaa81b459532f65540
nkaskov <nikitakaskov@mail.ru>
2020-06-23 19:04:09 -0400

    Twofish tests inited

--- test/threefish.cpp
+++ test/threefish.cpp
@@ -1,12 +1,13 @@
 //---------------------------------------------------------------------------//
 // Copyright (c) 2018-2020 Mikhail Komarov <nemo@nil.foundation>
+// Copyright (c) 2020 Nikita Kaskov <nbering@nil.foundation>
 //
 // Distributed under the Boost Software License, Version 1.0
 // See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt
 //---------------------------------------------------------------------------//
 
-#define BOOST_TEST_MODULE threefish_512_cipher_test
+#define BOOST_TEST_MODULE threefish_cipher_test
 
 #include <iostream>
 #include <unordered_map>
