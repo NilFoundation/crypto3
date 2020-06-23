4bd1804473ff04ddf404ca5851b5b74bac3dd14a
nkaskov <nikitakaskov@mail.ru>
2020-06-23 13:14:14 -0400

    Rijndael tests work #24

--- test/rijndael.cpp
+++ test/rijndael.cpp
@@ -225,49 +225,45 @@ BOOST_AUTO_TEST_CASE(rijndael_128_128_2) {
     
     BOOST_CHECK_EQUAL(out, "69c4e0d86a7b0430d8cdb78070b4c55a");
 }
-/*
+
 BOOST_DATA_TEST_CASE(rijndael_128_128, string_data("key_128_block_128"), triples) {
 
     byte_string const p(triples.first);
 
     BOOST_FOREACH(boost::property_tree::ptree::value_type pair, triples.second) {
-        byte_string const k(pair.first), c(pair.second.data()); 
-        cipher_fixture<rijndael<128, 128>, std::array<uint8_t, 16>, 
-                       std::array<uint8_t, 16>> f(k, p, c);
-        f.encrypt();
-        f.check_encrypt();
-        f.decrypt();
-        f.check_decrypt();
+        byte_string const k(pair.first); 
+
+        std::string out = encrypt<block::rijndael<128, 128>>(p, k);
+
+        BOOST_CHECK_EQUAL(out, pair.second.data());
     }
 }
+
 
 BOOST_DATA_TEST_CASE(rijndael_160_128, string_data("key_160_block_128"), triples) {
 
     byte_string const p(triples.first);
 
     BOOST_FOREACH(boost::property_tree::ptree::value_type pair, triples.second) {
-        byte_string const k(pair.first), c(pair.second.data()); 
-        cipher_fixture<rijndael<160, 128>, std::array<uint8_t, 20>, 
-                       std::array<uint8_t, 16>> f(k, p, c);
-        f.encrypt();
-        f.check_encrypt();
-        f.decrypt();
-        f.check_decrypt();
+        byte_string const k(pair.first); 
+        
+        std::string out = encrypt<block::rijndael<160, 128>>(p, k);
+
+        BOOST_CHECK_EQUAL(out, pair.second.data());
     }
 }
+
 
 BOOST_DATA_TEST_CASE(rijndael_192_128, string_data("key_192_block_128"), triples) {
 
     byte_string const p(triples.first);
 
     BOOST_FOREACH(boost::property_tree::ptree::value_type pair, triples.second) {
-        byte_string const k(pair.first), c(pair.second.data()); 
-        cipher_fixture<rijndael<192, 128>, std::array<uint16_t, 12>, 
-                       std::array<uint8_t, 16>> f(k, p, c);
-        f.encrypt();
-        f.check_encrypt();
-        f.decrypt();
-        f.check_decrypt();
+        byte_string const k(pair.first); 
+        
+        std::string out = encrypt<block::rijndael<192, 128>>(p, k);
+
+        BOOST_CHECK_EQUAL(out, pair.second.data());
     }
 }
 
@@ -276,13 +272,11 @@ BOOST_DATA_TEST_CASE(rijndael_224_128, string_data("key_224_block_128"), triples
     byte_string const p(triples.first);
 
     BOOST_FOREACH(boost::property_tree::ptree::value_type pair, triples.second) {
-        byte_string const k(pair.first), c(pair.second.data()); 
-        cipher_fixture<rijndael<224, 128>, std::array<uint32_t, 7>, 
-                       std::array<uint8_t, 16>> f(k, p, c);
-        f.encrypt();
-        f.check_encrypt();
-        f.decrypt();
-        f.check_decrypt();
+        byte_string const k(pair.first); 
+        
+        std::string out = encrypt<block::rijndael<224, 128>>(p, k);
+
+        BOOST_CHECK_EQUAL(out, pair.second.data());
     }
 }
 
@@ -291,13 +285,11 @@ BOOST_DATA_TEST_CASE(rijndael_256_128, string_data("key_256_block_128"), triples
     byte_string const p(triples.first);
 
     BOOST_FOREACH(boost::property_tree::ptree::value_type pair, triples.second) {
-        byte_string const k(pair.first), c(pair.second.data()); 
-        cipher_fixture<rijndael<256, 128>, std::array<uint64_t, 4>, 
-                       std::array<uint8_t, 16>> f(k, p, c);
-        f.encrypt();
-        f.check_encrypt();
-        f.decrypt();
-        f.check_decrypt();
+        byte_string const k(pair.first); 
+        
+        std::string out = encrypt<block::rijndael<256, 128>>(p, k);
+
+        BOOST_CHECK_EQUAL(out, pair.second.data());
     }
 }
 
@@ -305,56 +297,57 @@ BOOST_AUTO_TEST_SUITE_END()
 
 /*  NIST SP 800-38A AES tests 
     https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-38a.pdf */
-/*
+
 BOOST_AUTO_TEST_SUITE(aes_stream_processor_test_suite)
 // F.1.1, F.1.2
 BOOST_AUTO_TEST_CASE(aes_128_cipher) {
-    cipher_fixture<aes<128>, std::array<uint8_t, 16>, std::array<uint8_t, 64>>
-    f("2b7e151628aed2a6abf7158809cf4f3c", 
-      "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e51"
-      "30c81c46a35ce411e5fbc1191a0a52eff69f2445df4f9b17ad2b417be66c3710", 
-      "3ad77bb40d7a3660a89ecaf32466ef97f5d3d58503b9699de785895a96fdbaaf"
-      "43b1cd7f598ece23881b00e3ed0306887b0c785e27e8ad3f8223207104725dd4");
 
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
+    std::string input = "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e51"
+                        "30c81c46a35ce411e5fbc1191a0a52eff69f2445df4f9b17ad2b417be66c3710";
+
+    std::string key = "2b7e151628aed2a6abf7158809cf4f3c";
+
+    byte_string bk(key), bi(input);
+
+    std::string out = encrypt<block::aes<128>>(bi, bk);
+    
+    BOOST_CHECK_EQUAL(out, "3ad77bb40d7a3660a89ecaf32466ef97f5d3d58503b9699de785895a96fdbaaf"
+                            "43b1cd7f598ece23881b00e3ed0306887b0c785e27e8ad3f8223207104725dd4");
 }
 
 // F.1.3, F.1.4
 BOOST_AUTO_TEST_CASE(aes_192_cipher) {
-    cipher_fixture<aes<192>, std::array<uint16_t, 12>, std::array<uint16_t, 32>>
-    f("8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b", 
-      "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e51"
-      "30c81c46a35ce411e5fbc1191a0a52eff69f2445df4f9b17ad2b417be66c3710", 
-      "bd334f1d6e45f25ff712a214571fa5cc974104846d0ad3ad7734ecb3ecee4eef"
-      "ef7afd2270e2e60adce0ba2face6444e9a4b41ba738d6c72fb16691603c18e0e");
+    std::string input = "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e51"
+                        "30c81c46a35ce411e5fbc1191a0a52eff69f2445df4f9b17ad2b417be66c3710";
 
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
+    std::string key = "8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b";
+
+    byte_string bk(key), bi(input);
+
+    std::string out = encrypt<block::aes<192>>(bi, bk);
+    
+    BOOST_CHECK_EQUAL(out, "bd334f1d6e45f25ff712a214571fa5cc974104846d0ad3ad7734ecb3ecee4eef"
+                            "ef7afd2270e2e60adce0ba2face6444e9a4b41ba738d6c72fb16691603c18e0e");
 }
 
 // F.1.5, F.1.6
 BOOST_AUTO_TEST_CASE(aes_256_cipher) {
-    cipher_fixture<aes<256>, std::array<uint32_t, 8>, std::array<uint32_t, 16>>
-    f("603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4", 
-      "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e51"
-      "30c81c46a35ce411e5fbc1191a0a52eff69f2445df4f9b17ad2b417be66c3710", 
-      "f3eed1bdb5d2a03c064b5a7e3db181f8591ccb10d410ed26dc5ba74a31362870"
-      "b6ed21b99ca6f4f9f153e7b1beafed1d23304b7a39f9f3ff067d8d8f9e24ecc7");
+    std::string input = "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e51"
+                        "30c81c46a35ce411e5fbc1191a0a52eff69f2445df4f9b17ad2b417be66c3710";
 
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
+    std::string key = "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4";
+
+    byte_string bk(key), bi(input);
+
+    std::string out = encrypt<block::aes<256>>(bi, bk);
+    
+    BOOST_CHECK_EQUAL(out, "f3eed1bdb5d2a03c064b5a7e3db181f8591ccb10d410ed26dc5ba74a31362870"
+                            "b6ed21b99ca6f4f9f153e7b1beafed1d23304b7a39f9f3ff067d8d8f9e24ecc7");
 }
 
 BOOST_AUTO_TEST_SUITE_END() 
 
-
+/*
 BOOST_AUTO_TEST_SUITE(aes_various_containers_test_suite)
 
 BOOST_AUTO_TEST_CASE(aes_128_with_array_32) {
@@ -397,302 +390,4 @@ BOOST_AUTO_TEST_CASE(aes_128_with_array_8) {
     f.check_decrypt();
 }
 
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(rijndael_stream_processor_test_suite)
-// B = 128
-BOOST_AUTO_TEST_CASE(rijndael_128_128_cipher) {
-    cipher_fixture<rijndael<128, 128>, std::array<uint8_t, 16>, std::array<uint8_t, 16>>
-    f("000102030405060708090a0b0c0d0e0f", "00112233445566778899aabbccddeeff", 
-      "69c4e0d86a7b0430d8cdb78070b4c55a");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_160_128_cipher) {
-    cipher_fixture<rijndael<160, 128>, std::array<uint8_t, 20>, std::array<uint8_t, 16>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160", "3243f6a8885a308d313198a2e0370734",
-      "231d844639b31b412211cfe93712b880");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_192_128_cipher) {
-    cipher_fixture<rijndael<192, 128>, std::array<uint16_t, 12>, std::array<uint8_t, 16>>
-    f("000102030405060708090a0b0c0d0e0f1011121314151617", 
-      "00112233445566778899aabbccddeeff", "dda97ca4864cdfe06eaf70a0ec0d7191");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_224_128_cipher) {
-    cipher_fixture<rijndael<224, 128>, std::array<uint32_t, 7>, std::array<uint8_t, 16>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160f38b4da56a784d90", 
-      "3243f6a8885a308d313198a2e0370734", "8faa8fe4dee9eb17caa4797502fc9d3f");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_256_128_cipher) {
-    cipher_fixture<rijndael<256, 128>, std::array<uint64_t, 4>, std::array<uint8_t, 16>> 
-    f("000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f", 
-      "00112233445566778899aabbccddeeff","8ea2b7ca516745bfeafc49904b496089");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-// B = 160
-BOOST_AUTO_TEST_CASE(rijndael_128_160_cipher) {
-    cipher_fixture<rijndael<128, 160>, std::array<uint8_t, 16>, std::array<uint16_t, 10>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c", "3243f6a8885a308d313198a2e03707344a409382",
-      "16e73aec921314c29df905432bc8968ab64b1f51");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_160_160_cipher) {
-    cipher_fixture<rijndael<160, 160>, std::array<uint16_t, 10>, std::array<uint16_t, 10>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160", 
-      "3243f6a8885a308d313198a2e03707344a409382","0553eb691670dd8a5a5b5addf1aa7450f7a0e587");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_192_160_cipher) {
-    cipher_fixture<rijndael<192, 160>, std::array<uint16_t, 12>, std::array<uint16_t, 10>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160f38b4da5", 
-      "3243f6a8885a308d313198a2e03707344a409382", "73cd6f3423036790463aa9e19cfcde894ea16623");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_224_160_cipher) {
-    cipher_fixture<rijndael<224, 160>, std::array<uint32_t, 7>, std::array<uint16_t, 10>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160f38b4da56a784d90", 
-      "3243f6a8885a308d313198a2e03707344a409382",
-      "601b5dcd1cf4ece954c740445340bf0afdc048df");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_256_160_cipher) {
-    cipher_fixture<rijndael<256, 160>, std::array<uint64_t, 4>, std::array<uint16_t, 10>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160f38b4da56a784d9045190cfe",
-      "3243f6a8885a308d313198a2e03707344a409382", "579e930b36c1529aa3e86628bacfe146942882cf");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-// B = 192
-BOOST_AUTO_TEST_CASE(rijndael_128_192_cipher) {
-    cipher_fixture<rijndael<128, 192>, std::array<uint8_t, 16>, std::array<uint32_t, 6>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c", "3243f6a8885a308d313198a2e03707344a4093822299f31d",
-      "b24d275489e82bb8f7375e0d5fcdb1f481757c538b65148a");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_160_192_cipher) {
-    cipher_fixture<rijndael<160, 192>, std::array<uint16_t, 10>, std::array<uint32_t, 6>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160",
-      "3243f6a8885a308d313198a2e03707344a4093822299f31d", 
-      "738dae25620d3d3beff4a037a04290d73eb33521a63ea568");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_192_192_cipher) {
-    cipher_fixture<rijndael<192, 192>, std::array<uint32_t, 6>, std::array<uint32_t, 6>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160f38b4da5", 
-      "3243f6a8885a308d313198a2e03707344a4093822299f31d",
-      "725ae43b5f3161de806a7c93e0bca93c967ec1ae1b71e1cf");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_224_192_cipher) {
-    cipher_fixture<rijndael<224, 192>, std::array<uint32_t, 7>, std::array<uint32_t, 6>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160f38b4da56a784d90", 
-      "3243f6a8885a308d313198a2e03707344a4093822299f31d", 
-      "bbfc14180afbf6a36382a061843f0b63e769acdc98769130");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_256_192_cipher) {
-    cipher_fixture<rijndael<256, 192>, std::array<uint64_t, 4>, std::array<uint32_t, 6>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160f38b4da56a784d9045190cfe",
-      "3243f6a8885a308d313198a2e03707344a4093822299f31d", 
-      "0ebacf199e3315c2e34b24fcc7c46ef4388aa475d66c194c");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-// B = 224
-BOOST_AUTO_TEST_CASE(rijndael_128_224_cipher) {
-    cipher_fixture<rijndael<128, 224>, std::array<uint8_t, 16>, std::array<uint32_t, 7>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c", 
-      "3243f6a8885a308d313198a2e03707344a4093822299f31d0082efa9",
-      "b0a8f78f6b3c66213f792ffd2a61631f79331407a5e5c8d3793aceb1");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_160_224_cipher) {
-    cipher_fixture<rijndael<160, 224>, std::array<uint16_t, 10>, std::array<uint32_t, 7>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160", 
-      "3243f6a8885a308d313198a2e03707344a4093822299f31d0082efa9",
-      "08b99944edfce33a2acb131183ab0168446b2d15e958480010f545e3");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_192_224_cipher) {
-    cipher_fixture<rijndael<192, 224>, std::array<uint32_t, 6>, std::array<uint32_t, 7>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160f38b4da5", 
-      "3243f6a8885a308d313198a2e03707344a4093822299f31d0082efa9",
-      "be4c597d8f7efe22a2f7e5b1938e2564d452a5bfe72399c7af1101e2");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_224_224_cipher) {
-    cipher_fixture<rijndael<224, 224>, std::array<uint32_t, 7>, std::array<uint32_t, 7>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160f38b4da56a784d90",
-      "3243f6a8885a308d313198a2e03707344a4093822299f31d0082efa9",
-      "ef529598ecbce297811b49bbed2c33bbe1241d6e1a833dbe119569e8");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_256_224_cipher) {
-    cipher_fixture<rijndael<256, 224>, std::array<uint64_t, 4>, std::array<uint32_t, 7>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160f38b4da56a784d9045190cfe",
-      "3243f6a8885a308d313198a2e03707344a4093822299f31d0082efa9",
-      "02fafc200176ed05deb8edb82a3555b0b10d47a388dfd59cab2f6c11");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-// B = 256
-BOOST_AUTO_TEST_CASE(rijndael_128_256_cipher) {
-    cipher_fixture<rijndael<128, 256>, std::array<uint8_t, 16>, std::array<uint64_t, 4>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c", 
-      "3243f6a8885a308d313198a2e03707344a4093822299f31d0082efa98ec4e6c8",
-      "7d15479076b69a46ffb3b3beae97ad8313f622f67fedb487de9f06b9ed9c8f19");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_160_256_cipher) {
-    cipher_fixture<rijndael<160, 256>, std::array<uint16_t, 10>, std::array<uint64_t, 4>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160", 
-      "3243f6a8885a308d313198a2e03707344a4093822299f31d0082efa98ec4e6c8",
-      "514f93fb296b5ad16aa7df8b577abcbd484decacccc7fb1f18dc567309ceeffd");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_192_256_cipher) {
-    cipher_fixture<rijndael<192, 256>, std::array<uint64_t, 3>, std::array<uint64_t, 4>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160f38b4da5", 
-      "3243f6a8885a308d313198a2e03707344a4093822299f31d0082efa98ec4e6c8",
-      "5d7101727bb25781bf6715b0e6955282b9610e23a43c2eb062699f0ebf5887b2");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_224_256_cipher) {
-    cipher_fixture<rijndael<224, 256>, std::array<uint32_t, 7>, std::array<uint64_t, 4>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160f38b4da56a784d90",
-      "3243f6a8885a308d313198a2e03707344a4093822299f31d0082efa98ec4e6c8",
-      "d56c5a63627432579e1dd308b2c8f157b40a4bfb56fea1377b25d3ed3d6dbf80");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-
-BOOST_AUTO_TEST_CASE(rijndael_256_256_cipher) {
-    cipher_fixture<rijndael<256, 256>, std::array<uint64_t, 4>, std::array<uint64_t, 4>> 
-    f("2b7e151628aed2a6abf7158809cf4f3c762e7160f38b4da56a784d9045190cfe",
-      "3243f6a8885a308d313198a2e03707344a4093822299f31d0082efa98ec4e6c8",
-      "a49406115dfb30a40418aafa4869b7c6a886ff31602a7dd19c889dc64f7e4e7a");
-
-    f.encrypt();
-    f.check_encrypt();
-    f.decrypt();
-    f.check_decrypt();
-}
-*/
-BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()*/
