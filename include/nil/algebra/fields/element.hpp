c21178f7097c0a652221f074381285ff21209ec9
nkaskov <nikitakaskov@mail.ru>
2020-07-29 07:52:35 -0400

    Minor field operations changes

--- include/nil/algebra/fields/element.hpp
+++ include/nil/algebra/fields/element.hpp
@@ -7,8 +7,8 @@
 // http://www.boost.org/LICENSE_1_0.txt
 //---------------------------------------------------------------------------//
 
-#ifndef ALGEBRA_FIELDS_POINT_HPP
-#define ALGEBRA_FIELDS_POINT_HPP
+#ifndef ALGEBRA_FIELDS_ELEMENT_HPP
+#define ALGEBRA_FIELDS_ELEMENT_HPP
 
 #include <array>
 #include <nil/algebra/fields/detail/params/params.hpp>
@@ -26,8 +26,22 @@ namespace nil {
             	using type = std::array<element<fp2, NumberType>, 2>;
             }
             
+            template<typename NumberType>
+            struct element<fp6_3over2> {
+            	using type = std::array<element<fp2, NumberType>, 3>;
+            }
+
+            template<typename NumberType>
+            struct element<fp6_2over3> {
+            	using type = std::array<element<fp3, NumberType>, 2>;
+            }
+
+            template<typename NumberType>
+            struct element<fp12_2over3over2> {
+            	using type = std::array<element<fp6_3over2, NumberType>, 2>;
+            }
         }
     }    // namespace algebra
 }    // namespace nil
 
-#endif    // ALGEBRA_FIELDS_POINT_HPP
+#endif    // ALGEBRA_FIELDS_ELEMENT_HPP
