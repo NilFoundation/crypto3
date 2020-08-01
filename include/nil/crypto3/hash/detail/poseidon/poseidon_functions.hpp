b5298561384b82c19a144f2e8325901d6f1e44b4
Ilias Khairullin <ilias@nil.foundation>
2020-08-01 12:19:49 +0300

    Implementation draft

--- include/nil/crypto3/hash/detail/poseidon/poseidon_functions.hpp
+++ include/nil/crypto3/hash/detail/poseidon/poseidon_functions.hpp
@@ -9,7 +9,14 @@
 #ifndef CRYPTO3_HASH_POSEIDON_FUNCTIONS_HPP
 #define CRYPTO3_HASH_POSEIDON_FUNCTIONS_HPP
 
+#define GRAIN_LFSR_STATE_LEN 80
+
 #include <nil/crypto3/hash/detail/poseidon/poseidon_policy.hpp>
+
+#include <bitset>
+
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
 
 #include <utility>
 
@@ -18,73 +25,167 @@ namespace nil {
         namespace hashes {
             namespace detail {
 
-                template<typename FieldType, std::size_t Arity, std::size_t c, std::size_t DigestBits,
-                         std::size_t M = 128, bool strength = true>
-                struct poseidon_functions : public poseidon_policy<FieldType, Arity, c, DigestBits, M, strength> {
-                    typedef poseidon_policy<FieldType, Arity, c, DigestBits, M, strength> policy_type;
+                // filecoin oriented implementation
+                template<typename FieldT, std::size_t t, std::size_t DigestBits, std::size_t M, bool strength>
+                struct poseidon_functions
+                {
+                    typedef poseidon_policy<FieldT, t, DigestBits, M, strength> policy_type;
 
                     constexpr static std::size_t const state_bits = policy_type::state_bits;
                     constexpr static std::size_t const state_words = policy_type::state_words;
                     typedef typename policy_type::state_type state_type;
 
-                    // TODO: constexpr
-                    // (full_rounds, partial_rounds)
-                    std::pair<std::size_t, std::size_t> num_rounds = get_num_rounds();
-                    typedef std::pair<std::size_t, std::size_t> rounds_type;
+                    constexpr static std::size_t const full_rounds = policy_type::full_rounds;
+                    constexpr static std::size_t const half_full_rounds = policy_type::half_full_rounds;
+                    constexpr static std::size_t const part_rounds = policy_type::part_rounds;
 
-                    // TODO: constexpr
-                    std::size_t const round_constants_size =
-                        (std::get<0>(num_rounds) + std::get<1>(num_rounds)) * Arity;
-                    typedef typename std::array<FieldType, round_constants_size> round_constants_type;
+                    constexpr static std::size_t const modulus_bits = policy_type::modulus_bits;
 
-                    // static variant only for bls12-381 - filecoin oriented implementation
-                    // this round numbers seems not to be right according current version of the script
-                    // https://extgit.iaik.tugraz.at/krypto/hadeshash/-/tree/c984df1034874adf5cb5784bf86b609b0a8d6f99
-                    std::pair<std::size_t, std::size_t> get_num_rounds() {
-                        std::size_t full_rounds = 6;
-                        std::size_t part_rounds;
-                        switch (Arity) {
-                            case 2:
-                            case 3:
-                                part_rounds = 55;
-                                break;
-                            case 4:
-                            case 5:
-                            case 6:
-                            case 7:
-                                part_rounds = 56;
-                                break;
-                            case 8:
-                            case 9:
-                            case 10:
-                            case 11:
-                            case 12:
-                                part_rounds = 57;
-                                break;
-                            case 17:
-                            case 25:
-                                part_rounds = 59;
-                                break;
-                            case 37:
-                                part_rounds = 60;
-                                break;
-                            case 65:
-                                part_rounds = 61;
-                                break;
-                            default:
-                                BOOST_ASSERT_MSG(false, "invalid width of permutation");
+
+                    // TODO: add checks
+                    // TODO: constexpr
+                    struct round_constants_generator {
+
+                        // BOOST_STATIC_ASSERT_MSG(round_const_num > 0 && round_const_num <= t * (r_f + r_p), "round is not in range");
+
+                        inline round_constants_generator() {
+                            int i;
+                            std::size_t offset = 0;
+                            for (i = 1; i >= 0; i--) {
+                                state[offset++] = (1 >> i) & 1; // field - as in filecoin
+                            }
+                            for (i = 3; i >= 0; i--) {
+                                state[offset++] = (1 >> i) & 1; // s-box as in filecoin
+                            }
+                            for (i = 11; i >= 0; i--) {
+                                state[offset++] = (modulus_bits >> i) & 1;
+                            }
+                            for (i = 11; i >= 0; i--) {
+                                state[offset++] = (t >> i) & 1;
+                            }
+                            for (i = 9; i >= 0; i--) {
+                                state[offset++] = (full_rounds >> i) & 1;
+                            }
+                            for (i = 9; i >= 0; i--) {
+                                state[offset++] = (part_rounds >> i) & 1;
+                            }
+                            for (i = 29; i >= 0; i--) {
+                                state[offset++] = 1;
+                            }
+                            cout << state << '\n';
+                            // idling
+                            for (i = 0; i < 160; i++) {
+                                get_next_raw_bit();
+                                cout << state << '\n';
+                            }
                         }
 
-                        if (strength) {
-                            full_rounds += 2;
-                            part_rounds = ceil(part_rounds * 1.075);
+                        // get next element
+                        inline FieldT operator()() {
+                            cpp_int round_const;
+
+                            while (true) {
+                                round_const = 0;
+
+                                round_const |= get_next_bit();
+                                for (std::size_t i = 1; i < modulus_bits; i++) {
+                                    round_const <<= 1;
+                                    round_const |= get_next_bit();
+                                }
+
+                                if (round_const < cpp_int("0x73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001")) // filecoin oriented - remake when integrate in the project
+                                    break;
+                            }
+                            
+                            return FieldT(round_const);
                         }
 
-                        return std::make_pair(full_rounds, part_rounds);
+                        inline bool get_next_bit() {
+                            while (true) {
+                                if (get_next_raw_bit())
+                                    break;
+                                else
+                                    get_next_raw_bit();
+                            }
+                            return get_next_raw_bit();
+                        }
+
+                        inline bool get_next_raw_bit() {
+                            bool next_v = state[0] ^ state[13] ^ state[23] ^ state[38] ^ state[51] ^ state[62];
+                            state >>= 1;
+                            state[GRAIN_LFSR_STATE_LEN - 1] = next_v;
+                            return next_v;
+                        }
+
+                        std::bitset<GRAIN_LFSR_STATE_LEN> state;
+                    };
+
+
+                    // TODO: rewrite before integrating in project, particularly work with field
+                    struct MDS_matrix {
+                        inline boost::numeric::ublas::matrix<FieldT> const get_mds_matrix() const {
+                            static boost::numeric::ublas::matrix<FieldT> const &mds_matrix = [](){
+                                static boost::numeric::ublas::matrix<FieldT> mds_matrix(t, t);
+                                for (std::size_t i = 0; i < t; i++) {
+                                    // TODO: rewrite
+                                    for (std::size_t j = 0; j < t; j++) {
+                                        mds_matrix.insert_element(i, j, FieldT(
+                                            cpp_int(i + (j + t))
+                                        ).get_inverse());
+                                    }
+                                }
+                                return const_cast<boost::numeric::ublas::matrix<FieldT> const&>(mds_matrix);
+                            }();
+                            return mds_matrix;
+                        }
+                        inline void product(state_type &A) const {
+                            boost::numeric::ublas::vector<FieldT> A_vector(state_words);
+                            for (std::size_t i = 0; i < state_words; i++)
+                                A_vector[i] = A[i];
+                            A_vector = boost::numeric::ublas::prod(A_vector, this->get_mds_matrix());
+                            for (std::size_t i = 0; i < state_words; i++)
+                                A[i] = A_vector[i];
+                        }
+                    };
+
+                    static inline void do_full_round(state_type &A, round_constants_generator &const_gen, MDS_matrix &mds_matrix) {
+                        for (std::size_t i = 0; i < state_words; i++) {
+                            A[i] += const_gen();
+                            A[i] *= A[i] * A[i] * A[i] * A[i];
+                        }
+                        mds_matrix.product(A);
                     }
+
+                    static inline void do_part_round(state_type &A, round_constants_generator &const_gen, MDS_matrix &mds_matrix) {
+                        for (std::size_t i = 0; i < state_words; i++) {
+                            A[i] += const_gen();
+                        }
+                        A[0] *= A[0] * A[0] * A[0] * A[0];
+                        mds_matrix.product(A);
+                    }
+
 
                     static inline void permute(state_type &A) {
+                        round_constants_generator const_gen;
+                        MDS_matrix mds_matrix;
+                        
+                        // first half of full rounds
+                        for(std::size_t i = 0; i < half_full_rounds; i++) {
+                            do_full_round(A, const_gen, mds_matrix);
+                        }
+
+                        // partial rounds
+                        for(std::size_t i = 0; i < part_rounds; i++) {
+                            do_part_round(A, const_gen, mds_matrix);
+                        }
+
+                        // second half of full rounds
+                        for(std::size_t i = 0; i < half_full_rounds; i++) {
+                            do_full_round(A, const_gen, mds_matrix);
+                        }
                     }
+
+
                 };
             }    // namespace detail
         }        // namespace hashes
--- include/nil/crypto3/hash/detail/poseidon/poseidon_policy.hpp
+++ include/nil/crypto3/hash/detail/poseidon/poseidon_policy.hpp
@@ -31,40 +31,85 @@ namespace nil {
                  * @tparam M desired security level in bits
                  * @tparam strength
                  */
-                // at this moment only for bls12-381 - filecoin oriented implementation
-                template<typename FieldType, std::size_t Arity, std::size_t c, std::size_t DigestBits, std::size_t M =
-                                                                                                         128,
-                         bool strength = true>
-                struct poseidon_policy {
-
-                    BOOST_STATIC_ASSERT_MSG(Arity > c, "Arity should consist of capacity and rate");
+                // at this moment only for bls12-381 - filecoin oriented im
+                template<typename FieldT, std::size_t t, std::size_t DigestBits, std::size_t M>
+                struct base_poseidon_policy {
 
                     constexpr static std::size_t const digest_bits = DigestBits;
 
-                    constexpr static std::size_t const state_bits = Arity * FieldType::modulus_bits;
-                    constexpr static std::size_t const state_words = Arity;
-                    typedef std::array<typename FieldType::value_type, state_words> state_type;
+                    typedef FieldT word_type;
 
-                    constexpr static std::size_t const block_bits = (Arity - c) * FieldType::modulus_bits;
-                    constexpr static std::size_t const block_words = Arity - c;
-                    typedef std::array<typename FieldType::value_type, block_words> block_type;
+                    constexpr static std::size_t const state_bits = t * FieldT::modulus_bits;
+                    constexpr static std::size_t const state_words = t;
+                    typedef std::array<FieldT, t> state_type;
+
+                    constexpr static std::size_t const block_bits = (t - 1) * FieldT::modulus_bits;
+                    constexpr static std::size_t const block_words = t - 1;
+                    typedef std::array<FieldT, t - 1> block_type;
 
                     constexpr static std::size_t const sec_level = M;
+
+                    constexpr static std::size_t const modulus_bits = FieldT::modulus_bits;
 
                     struct iv_generator {
                         // TODO: return-value seems not to be const in reality
                         // TODO: maybe it would be done in constexpr way
                         state_type const &operator()() const {
-                            static state_type const H0 = []() {
+                            static state_type const H0 = [](){
                                 state_type H;
-                                H.fill(FieldType(0));
-                                return std::move(H);
+                                H.fill(FieldT(0));
+                                return const_cast<state_type>(H);
                             }();
                             return H0;
                         }
                     };
+                };
 
-                };    // struct poseidon_policy
+
+                template<typename FieldT, std::size_t t, std::size_t DigestBits, std::size_t M, bool strength>
+                struct poseidon_policy;
+
+
+                template<typename FieldT, std::size_t DigestBits, std::size_t M, bool strength>
+                struct poseidon_policy<FieldT, 2, DigestBits, M, strength> :
+                    base_poseidon_policy<FieldT, 2, DigestBits, M>
+                {
+                    constexpr static std::size_t const full_rounds = 8;
+                    constexpr static std::size_t const half_full_rounds = 4;
+                    constexpr static std::size_t const part_rounds = strength ? 69 : 55;
+                };
+
+
+                template<typename FieldT, std::size_t DigestBits, std::size_t M, bool strength>
+                struct poseidon_policy<FieldT,3,DigestBits,M,strength> :
+                    base_poseidon_policy<FieldT, 3, DigestBits, M>
+                {
+                    constexpr static std::size_t const full_rounds = 8;
+                    constexpr static std::size_t const half_full_rounds = 4;
+                    constexpr static std::size_t const part_rounds = strength ? 69 : 55;
+                };
+
+
+                template<typename FieldT, std::size_t DigestBits, std::size_t M, bool strength>
+                struct poseidon_policy<FieldT, 4, DigestBits, M, strength> :
+                    base_poseidon_policy< FieldT, 4, DigestBits, M>
+                {
+                    constexpr static std::size_t const full_rounds = 8;
+                    constexpr static std::size_t const half_full_rounds = 4;
+                    constexpr static std::size_t const part_rounds = strength ? 70 : 56;
+                };
+
+                template<typename FieldT, std::size_t DigestBits, std::size_t M, bool strength>
+                struct poseidon_policy<FieldT, 5, DigestBits, M, strength> :
+                    base_poseidon_policy< FieldT, 5, DigestBits, M>
+                {
+                    constexpr static std::size_t const full_rounds = 8;
+                    constexpr static std::size_t const half_full_rounds = 4;
+                    constexpr static std::size_t const part_rounds = strength ? 70 : 56;
+                };
+
+                // continue define partial specialized template classes for each arity separately...
+
             }         // namespace detail
         }             // namespace hashes
     }                 // namespace crypto3
