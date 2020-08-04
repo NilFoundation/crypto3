ccc757dbfae9fd5f2ac0e8b22e89247d6b12e405
nkaskov <nikitakaskov@mail.ru>
2020-08-04 07:50:48 -0400

    Minor curves changes

--- include/nil/algebra/curves/bn128.hpp
+++ include/nil/algebra/curves/bn128.hpp
@@ -14,54 +14,54 @@
 
 namespace nil {
     namespace algebra {
+    	/*
+	    	The curve equation for a BN curve is:
+
+			E/Fp: y^2 = x^3 + b.
+		*/
 
         BOOST_MP_DEFINE_SIZED_CPP_INT_LITERAL(256)
         /*
+        	Over Fp12_2over3over2
 			y^2 = x^3 + b
 			u^2 = -1
 			xi = xi_a + xi_b u
 			v^3 = xi
 			w^2 = v
 		*/
-        struct bn128_snark1 : public curve_jacobian_policy<256> {
+        struct bn128_snark1 : public curve_nist_policy<256> {
 	        typedef typename ec_group_info<256>::number_type number_type;
 
 	        constexpr static const number_type p = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
-	        constexpr static const number_type a =
-	            ?;
+	        constexpr static const number_type a = 0;
 	        constexpr static const number_type b = 0x03;
 	        constexpr static const number_type x = 0x09;
 	        constexpr static const number_type y = 0x01;
-	        constexpr static const number_type order =
-	        	?;
+	        constexpr static const number_type order = ?;
     	};
 
     	// b/xi = 82 / (9 + u) = 9 - u
-        struct bn128_snark2 : public curve_jacobian_policy<256> {
+        struct bn128_snark2 : public curve_nist_policy<256> {
 	        typedef typename ec_group_info<256>::number_type number_type;
 
 	        constexpr static const number_type p = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
-	        constexpr static const number_type a =
-	            ?;
+	        constexpr static const number_type a = 0;
 	        constexpr static const number_type b = 82;
 	        constexpr static const number_type x = 0x09;
 	        constexpr static const number_type y = 0x01;
-	        constexpr static const number_type order =
-	        	?;
+	        constexpr static const number_type order = ?;
     	};
 
     	// herumi curve
-        struct bn128_Fp254BNb : public curve_jacobian_policy<256> {
+        struct bn128_Fp254BNb : public curve_nist_policy<256> {
 	        typedef typename ec_group_info<256>::number_type number_type;
 
 	        constexpr static const number_type p = 16798108731015832284940804142231733909889187121439069848933715426072753864723;
-	        constexpr static const number_type a =
-	            ?;
+	        constexpr static const number_type a = 0;
 	        constexpr static const number_type b = 0x2;
 	        constexpr static const number_type x = 0x01;
 	        constexpr static const number_type y = 0x01;
-	        constexpr static const number_type order =
-	        	?;
+	        constexpr static const number_type order = ?;
     	};
 
     }    // namespace algebra
