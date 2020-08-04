7dcd26de2aad0ddb5312dc1d371e3ff53ed33921
Mikhail Komarov <nemo@nil.foundation>
2020-08-05 02:45:54 +0300

    More implementation updates.

--- include/nil/crypto3/pubkey/ecdh.hpp
+++ include/nil/crypto3/pubkey/ecdh.hpp
@@ -14,214 +14,71 @@
 namespace nil {
     namespace crypto3 {
         namespace pubkey {
-            template<typename FieldType>
+            template<typename CurveType>
             struct ecdh_public_key {
-                typedef FieldType field_type;
+                typedef CurveType curve_type;
 
-                typedef typename field_type::value_type value_type;
+                typedef typename curve_type::value_type value_type;
 
-                constexpr static const std::size_t key_bits = field_type::modulus_bits;
-                typedef typename field_type::modulus_type key_type;
+                constexpr static const std::size_t key_bits = curve_type::modulus_bits;
+                typedef typename curve_type::modulus_type key_type;
 
-                constexpr static const std::size_t key_schedule_bits = field_type::modulus_bits;
-                typedef typename field_type::modulus_type key_schedule_type;
+                constexpr static const std::size_t key_schedule_bits = curve_type::modulus_bits;
+                typedef typename curve_type::modulus_type key_schedule_type;
 
-                constexpr static const std::size_t signature_bits = field_type::modulus_bits * 2;
+                constexpr static const std::size_t signature_bits = curve_type::modulus_bits * 2;
                 typedef std::tuple<value_type, value_type> signature_type;
 
                 inline static bool verify(const signature_type &val, const key_schedule_type &key) {
-
                 }
             };
 
-            template<typename FieldType>
+            template<typename CurveType>
             struct ecdh_private_key {
-                typedef FieldType field_type;
+                typedef CurveType curve_type;
 
-                typedef typename field_type::value_type value_type;
+                typedef typename curve_type::value_type value_type;
 
-                constexpr static const std::size_t key_bits = field_type::modulus_bits;
-                typedef typename field_type::modulus_type key_type;
+                constexpr static const std::size_t key_bits = curve_type::modulus_bits;
+                typedef typename curve_type::modulus_type key_type;
 
-                constexpr static const std::size_t key_schedule_bits = field_type::modulus_bits;
-                typedef typename field_type::modulus_type key_schedule_type;
+                constexpr static const std::size_t key_schedule_bits = curve_type::modulus_bits;
+                typedef typename curve_type::modulus_type key_schedule_type;
 
-                constexpr static const std::size_t signature_bits = field_type::modulus_bits * 2;
+                constexpr static const std::size_t signature_bits = curve_type::modulus_bits * 2;
                 typedef std::tuple<value_type, value_type> signature_type;
 
                 inline static bool agree(const signature_type &val, const key_schedule_type &key) {
+                    pk_operations::key_agreement_with_kdf(kdf), m_group(key.domain()),
+                        m_rng(rng) m_l_times_priv =
+                            inverse_mod(m_group.get_cofactor(), m_group.get_order()) * key.private_value();
+                    //---------------
                     const ec_group m_group;
                     boost::multiprecision::number<Backend, ExpressionTemplates> m_l_times_priv;
                     random_number_generator &m_rng;
                     std::vector<boost::multiprecision::number<Backend, ExpressionTemplates>> m_ws;
-//--------------
+                    //--------------
 
+                    point_gfp input_point = m_group.get_cofactor() * m_group.os2ecp(w, w_len);
+                    input_point.randomize_repr(m_rng);
+
+                    const point_gfp S = m_group.blinded_var_point_multiply(input_point, m_l_times_priv, m_rng, m_ws);
+
+                    if (!S.on_the_curve()) {
+                        throw internal_error("ECDH agreed value was not on the curve");
+                    }
+                    return boost::multiprecision::number<Backend, ExpressionTemplates>::encode_1363(
+                        S.get_affine_x(), m_group.get_p_bytes());
                 }
             };
 
-            template<typename FieldType>
+            template<typename CurveType>
             struct ecdh {
-                typedef FieldType field_type;
+                typedef CurveType curve_type;
 
-                typedef ecdh_public_key<FieldType> public_key_type;
-                typedef ecdh_private_key<FieldType> private_key_type;
+                typedef ecdh_public_key<CurveType> public_key_type;
+                typedef ecdh_private_key<CurveType> private_key_type;
             };
-
-            /**
-             * This class represents ECDH Public Keys.
-             */
-            inline static bool verify(const signature_type &val, const key_schedule_type &key) {
-
-            }template<typename CurveType, typename NumberType = typename CurveType::number_type>
-            class ecdh_public_key : public virtual ec_public_key<CurveType, NumberType> {
-            public:
-                /**
-                 * Create an ECDH public key.
-                 * @param alg_id algorithm identifier
-                 * @param key_bits DER encoded public key bits
-                 */
-                ecdh_public_key(const algorithm_identifier &alg_id, const std::vector<uint8_t> &key_bits) :
-                    ec_public_key(alg_id, key_bits) {
-                }
-
-                /**
-                 * Construct a public key from a given public point.
-                 * @param dom_par the domain parameters associated with this key
-                 * @param public_point the public point defining this key
-                 */
-                ecdh_public_key(const ec_group &dom_par, const point_gfp &public_point) :
-                    ec_public_key(dom_par, public_point) {
-                }
-
-                /**
-                 * Get the OID of the underlying public key scheme.
-                 * @return oid_t of the public key scheme
-                 */
-                static const oid_t oid() {
-                    return oid_t({1, 3, 132, 1, 12});
-                }
-
-                /**
-                 * Get this keys algorithm name.
-                 * @return this keys algorithm name
-                 */
-                std::string algo_name() const override {
-                    return "ECDH";
-                }
-
-                /**
-                 * @return public point value
-                 */
-                std::vector<uint8_t> public_value() const {
-                    return public_point().encode(point_gfp::UNCOMPRESSED);
-                }
-
-                /**
-                 * @return public point value
-                 */
-                std::vector<uint8_t> public_value(point_gfp::compression_type format) const {
-                    return public_point().encode(format);
-                }
-
-            protected:
-                ecdh_public_key() = default;
-            };
-
-            /**
-             * This class represents ECDH Private Keys.
-             */
-            template<typename CurveType, typename NumberType = typename CurveType::number_type>
-            class ecdh_private_key : public ecdh_public_key<CurveType, NumberType>,
-                                     public ec_private_key<CurveType, NumberType>,
-                                     public pk_key_agreement_key {
-            public:
-                /**
-                 * Load a private key.
-                 * @param alg_id the X.509 algorithm identifier
-                 * @param key_bits ECPrivateKey bits
-                 */
-                ecdh_private_key(const algorithm_identifier &alg_id, const secure_vector<uint8_t> &key_bits) :
-                    ec_private_key(alg_id, key_bits) {
-                }
-
-                /**
-                 * Generate a new private key
-                 * @param rng a random number generator
-                 * @param domain parameters to used for this key
-                 * @param x the private key; if zero, a new random key is generated
-                 */
-                ecdh_private_key(random_number_generator &rng, const ec_group &domain,
-                                 const number<Backend, ExpressionTemplates> &x = 0) :
-                    ec_private_key(rng, domain, x) {
-                }
-
-                std::vector<uint8_t> public_value() const
-
-                    override {
-                    return ecdh_public_key::public_value(point_gfp::UNCOMPRESSED);
-                }
-
-                std::vector<uint8_t> public_value(point_gfp::compression_type type) const {
-                    return ecdh_public_key::public_value(type);
-                }
-
-                std::unique_ptr<pk_operations::key_agreement>
-                    create_key_agreement_op(random_number_generator &rng,
-                                            const std::string &params,
-                                            const std::string &provider) const override;
-            };
-
-            template<typename CurveType, typename NumberType = typename CurveType::number_type>
-            class ecdh {
-            public:
-                typedef ecdh_public_key<CurveType, NumberType> public_key_policy;
-                typedef ecdh_private_key<CurveType, NumberType> private_key_policy;
-            };
-
-            namespace {
-
-                /**
-                 * ECDH operation
-                 */
-                class ecdh_ka_operation final : public pk_operations::key_agreement_with_kdf {
-                public:
-                    ecdh_ka_operation(const ecdh_private_key &key, const std::string &kdf,
-                                      random_number_generator &rng) :
-                        pk_operations::key_agreement_with_kdf(kdf),
-                        m_group(key.domain()), m_rng(rng) {
-                        m_l_times_priv = inverse_mod(m_group.get_cofactor(), m_group.get_order()) * key.private_value();
-                    }
-
-                    secure_vector<uint8_t> raw_agree(const uint8_t w[], size_t w_len) override {
-                        point_gfp input_point = m_group.get_cofactor() * m_group.os2ecp(w, w_len);
-                        input_point.randomize_repr(m_rng);
-
-                        const point_gfp S =
-                            m_group.blinded_var_point_multiply(input_point, m_l_times_priv, m_rng, m_ws);
-
-                        if (!S.on_the_curve()) {
-                            throw internal_error("ECDH agreed value was not on the curve");
-                        }
-                        return boost::multiprecision::number<Backend, ExpressionTemplates>::encode_1363(
-                            S.get_affine_x(), m_group.get_p_bytes());
-                    }
-
-                private:
-                    const ec_group m_group;
-                    boost::multiprecision::number<Backend, ExpressionTemplates> m_l_times_priv;
-                    random_number_generator &m_rng;
-                    std::vector<boost::multiprecision::number<Backend, ExpressionTemplates>> m_ws;
-                };
-
-            }    // namespace
-
-            std::unique_ptr<pk_operations::key_agreement>
-                ecdh_private_key::create_key_agreement_op(random_number_generator &rng, const std::string &params,
-                                                          const std::string &provider) const {
-                return std::unique_ptr<pk_operations::key_agreement>(new ecdh_ka_operation(*this, params, rng));
-
-                throw provider_not_found(algo_name(), provider);
-            }
         }    // namespace pubkey
     }        // namespace crypto3
 }    // namespace nil
--- include/nil/crypto3/pubkey/ecgdsa.hpp
+++ include/nil/crypto3/pubkey/ecgdsa.hpp
@@ -17,206 +17,37 @@ namespace nil {
             template<typename CurveType>
             struct ecgdsa_public_key {
                 typedef CurveType curve_type;
-            };
 
-            template<typename CurveType>
-            struct ecgdsa_private_key {
                 typedef CurveType curve_type;
-            };
 
-            template<typename CurveType>
-            struct ecgdsa {
-                typedef ecgdsa_public_key<CurveType> public_key_policy;
-                typedef ecgdsa_private_key<CurveType> private_key_policy;
-            };
+                typedef typename curve_type::value_type value_type;
+                typedef typename curve_type::number_type number_type;
 
-            /**
-             * This class represents ECGDSA public keys.
-             */
-            template<typename CurveType, typename NumberType = typename CurveType::number_type>
-            class ecgdsa_public_key : public ec_public_key<CurveType, NumberType> {
-            public:
-                /**
-                 * Construct a public key from a given public point.
-                 * @param dom_par the domain parameters associated with this key
-                 * @param public_point the public point defining this key
-                 */
-                ecgdsa_public_key(const ec_group<CurveType, NumberType> &dom_par,
-                                  const point_gfp<CurveType> &public_point) :
-                    ec_public_key<CurveType, NumberType>(dom_par, public_point) {
-                }
+                constexpr static const std::size_t key_bits = curve_type::field_type::modulus_bits;
+                typedef typename curve_type::value_type key_type;
 
-                /**
-                 * Load a public key.
-                 * @param alg_id the X.509 algorithm identifier
-                 * @param key_bits DER encoded public key bits
-                 */
-                ecgdsa_public_key(const algorithm_identifier &alg_id, const std::vector<uint8_t> &key_bits) :
-                    ec_public_key<CurveType, NumberType>(alg_id, key_bits) {
-                }
+                constexpr static const std::size_t key_schedule_bits = curve_type::field_type::modulus_bits;
+                typedef typename curve_type::value_type key_schedule_type;
 
-                /**
-                 * Get the OID of the underlying public key scheme.
-                 * @return oid_t of the public key scheme
-                 */
-                static const oid_t oid() {
-                    return oid_t({1, 3, 36, 3, 3, 2, 5, 2, 1});
-                }
+                constexpr static const std::size_t signature_bits = curve_type::field_type::modulus_bits * 2;
+                typedef std::tuple<value_type, value_type> signature_type;
 
-                /**
-                 * Get this keys algorithm name.
-                 * @result this keys algorithm name ("ECGDSA")
-                 */
-                std::string algo_name() const override {
-                    return "ECGDSA";
-                }
+                inline static bool verify(const signature_type &val, const key_schedule_type &key) {
+                    pk_operations::verification_with_emsa(emsa), m_group(ecgdsa.domain()),
+                        m_public_point(ecgdsa.public_point())
 
-                size_t message_parts() const override {
-                    return 2;
-                }
-
-                size_t message_part_size() const override {
-                    return domain().get_order().bytes();
-                }
-
-                std::unique_ptr<pk_operations::verification>
-                    create_verification_op(const std::string &params, const std::string &provider) const override;
-
-            protected:
-                ecgdsa_public_key() = default;
-            };
-
-            /**
-             * This class represents ECGDSA private keys.
-             */
-            template<typename CurveType, typename NumberType = typename CurveType::number_type>
-            class ecgdsa_private_key final : public ecgdsa_public_key<CurveType, NumberType>,
-                                             public ec_private_key<CurveType, NumberType> {
-            public:
-                /**
-                 * Load a private key.
-                 * @param alg_id the X.509 algorithm identifier
-                 * @param key_bits ECPrivateKey bits
-                 */
-                ecgdsa_private_key(const algorithm_identifier &alg_id, const secure_vector<uint8_t> &key_bits) :
-                    ec_private_key<CurveType, NumberType>(alg_id, key_bits, true) {
-                }
-
-                /**
-                 * Generate a new private key.
-                 * @param rng a random number generator
-                 * @param domain parameters to used for this key
-                 * @param x the private key (if zero, generate a new random key)
-                 */
-                template<typename UniformRandomGenerator, typename Backend,
-                         expression_template_option ExpressionTemplates>
-                ecgdsa_private_key(UniformRandomGenerator &rng, const ec_group<CurveType, NumberType> &domain,
-                                   const number<Backend, ExpressionTemplates> &x = 0) :
-                    ec_private_key<CurveType, NumberType>(rng, domain, x, true) {
-                }
-
-                template<typename UniformRandomGenerator>
-                bool check_key(UniformRandomGenerator &rng, bool) const override {
-                    if (!public_point().on_the_curve()) {
-                        return false;
-                    }
-
-                    if (!strong) {
-                        return true;
-                    }
-
-                    return key_pair::signature_consistency_check(rng, *this, "EMSA1(SHA-256)");
-                }
-
-                std::unique_ptr<pk_operations::signature>
-                    create_signature_op(random_number_generator &rng,
-                                        const std::string &params,
-                                        const std::string &provider) const override;
-            };
-
-            namespace {
-
-                /**
-                 * ECGDSA signature operation
-                 */
-                class ecgdsa_signature_operation final : public pk_operations::signature_with_emsa {
-                public:
-                    ecgdsa_signature_operation(const ecgdsa_private_key &ecgdsa, const std::string &emsa) :
-                        pk_operations::signature_with_emsa(emsa), m_group(ecgdsa.domain()),
-                        m_x(ecgdsa.private_value()) {
-                    }
-
-                    secure_vector<uint8_t> raw_sign(const uint8_t msg[], size_t msg_len,
-                                                    random_number_generator &rng) override;
-
-                    size_t max_input_bits() const override {
-                        return m_group.get_order_bits();
-                    }
-
-                private:
-                    const ec_group m_group;
-                    const boost::multiprecision::number<Backend, ExpressionTemplates> &m_x;
-                    std::vector<boost::multiprecision::number<Backend, ExpressionTemplates>> m_ws;
-                };
-
-                secure_vector<uint8_t> ecgdsa_signature_operation::raw_sign(const uint8_t msg[], size_t msg_len,
-                                                                            random_number_generator &rng) {
-                    const boost::multiprecision::number<Backend, ExpressionTemplates> m(msg, msg_len,
-                                                                                        m_group.get_order_bits());
-
-                    const boost::multiprecision::number<Backend, ExpressionTemplates> k = m_group.random_scalar(rng);
-
-                    const boost::multiprecision::number<Backend, ExpressionTemplates> r =
-                        m_group.mod_order(m_group.blinded_base_point_multiply_x(k, rng, m_ws));
-
-                    const boost::multiprecision::number<Backend, ExpressionTemplates> kr =
-                        m_group.multiply_mod_order(k, r);
-
-                    const boost::multiprecision::number<Backend, ExpressionTemplates> s =
-                        m_group.multiply_mod_order(m_x, kr - m);
-
-                    // With overwhelming probability, a bug rather than actual zero r/s
-                    if (r.is_zero() || s.is_zero()) {
-                        throw internal_error("During ECGDSA signature generated zero r/s");
-                    }
-
-                    return boost::multiprecision::number<Backend, ExpressionTemplates>::encode_fixed_length_int_pair(
-                        r, s, m_group.get_order_bytes());
-                }
-
-                /**
-                 * ECGDSA verification operation
-                 */
-                class ecgdsa_verification_operation final : public pk_operations::verification_with_emsa {
-                public:
-                    ecgdsa_verification_operation(const ecgdsa_public_key &ecgdsa, const std::string &emsa) :
-                        pk_operations::verification_with_emsa(emsa), m_group(ecgdsa.domain()),
-                        m_public_point(ecgdsa.public_point()) {
-                    }
-
-                    size_t max_input_bits() const override {
-                        return m_group.get_order_bits();
-                    }
-
-                    bool with_recovery() const override {
-                        return false;
-                    }
-
-                    bool verify(const uint8_t msg[], size_t msg_len, const uint8_t sig[], size_t sig_len) override;
-
-                private:
+                        //----------------------------
                     const ec_group m_group;
                     const point_gfp &m_public_point;
-                };
 
-                bool ecgdsa_verification_operation::verify(const uint8_t msg[], size_t msg_len, const uint8_t sig[],
-                                                           size_t sig_len) {
+                    //---------------------------
+
                     if (sig_len != m_group.get_order_bytes() * 2) {
                         return false;
                     }
 
                     const boost::multiprecision::number<Backend, ExpressionTemplates> e(msg, msg_len,
-                                                                                        m_group.get_order_bits());
+                        m_group.get_order_bits());
 
                     const boost::multiprecision::number<Backend, ExpressionTemplates> r(sig, sig_len / 2);
                     const boost::multiprecision::number<Backend, ExpressionTemplates> s(sig + sig_len / 2, sig_len / 2);
@@ -242,27 +73,65 @@ namespace nil {
                         m_group.mod_order(R.get_affine_x());
                     return (v == r);
                 }
+            };
 
-            }    // namespace
+            template<typename CurveType>
+            struct ecgdsa_private_key {
+                typedef CurveType curve_type;
 
-            std::unique_ptr<pk_operations::verification>
-                ecgdsa_public_key::create_verification_op(const std::string &params,
-                                                          const std::string &provider) const {
-                if (provider == "core" || provider.empty()) {
-                    return std::unique_ptr<pk_operations::verification>(
-                        new ecgdsa_verification_operation(*this, params));
+                typedef CurveType curve_type;
+
+                typedef typename curve_type::value_type value_type;
+                typedef typename curve_type::number_type number_type;
+
+                constexpr static const std::size_t key_bits = curve_type::field_type::modulus_bits;
+                typedef typename CurveType::value_type key_type;
+
+                constexpr static const std::size_t key_schedule_bits = curve_type::field_type::modulus_bits;
+                typedef typename CurveType::value_type key_schedule_type;
+
+                constexpr static const std::size_t signature_bits = curve_type::field_type::modulus_bits * 2;
+                typedef std::tuple<value_type, value_type> signature_type;
+
+                template<typename Hash>
+                inline static bool sign(signature_type &res, const number_type &val, const key_schedule_type &key) {
+                    const ec_group m_group;
+                    const boost::multiprecision::number<Backend, ExpressionTemplates> &m_x;
+                    std::vector<boost::multiprecision::number<Backend, ExpressionTemplates>> m_ws;
+
+                    //-----------------------
+                    pk_operations::signature_with_emsa(emsa), m_group(ecgdsa.domain()),
+                        m_x(ecgdsa.private_value())
+                    //------------------------
+                    const boost::multiprecision::number<Backend, ExpressionTemplates> m(msg, msg_len,
+                        m_group.get_order_bits());
+
+                    const boost::multiprecision::number<Backend, ExpressionTemplates> k = m_group.random_scalar(rng);
+
+                    const boost::multiprecision::number<Backend, ExpressionTemplates> r =
+                        m_group.mod_order(m_group.blinded_base_point_multiply_x(k, rng, m_ws));
+
+                    const boost::multiprecision::number<Backend, ExpressionTemplates> kr =
+                        m_group.multiply_mod_order(k, r);
+
+                    const boost::multiprecision::number<Backend, ExpressionTemplates> s =
+                        m_group.multiply_mod_order(m_x, kr - m);
+
+                    // With overwhelming probability, a bug rather than actual zero r/s
+                    if (r.is_zero() || s.is_zero()) {
+                        throw internal_error("During ECGDSA signature generated zero r/s");
+                    }
+
+                    return boost::multiprecision::number<Backend, ExpressionTemplates>::encode_fixed_length_int_pair(
+                        r, s, m_group.get_order_bytes());
                 }
-                throw Provider_Not_Found(algo_name(), provider);
-            }
+            };
 
-            std::unique_ptr<pk_operations::signature>
-                ecgdsa_private_key::create_signature_op(random_number_generator & /*random*/, const std::string &params,
-                                                        const std::string &provider) const {
-                if (provider == "core" || provider.empty()) {
-                    return std::unique_ptr<pk_operations::signature>(new ecgdsa_signature_operation(*this, params));
-                }
-                throw Provider_Not_Found(algo_name(), provider);
-            }
+            template<typename CurveType>
+            struct ecgdsa {
+                typedef ecgdsa_public_key<CurveType> public_key_policy;
+                typedef ecgdsa_private_key<CurveType> private_key_policy;
+            };
         }    // namespace pubkey
     }        // namespace crypto3
 }    // namespace nil
