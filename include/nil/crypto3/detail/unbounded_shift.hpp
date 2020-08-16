d4f24d919a867fbdeb8f7b8f266bae6c4d6db969
Mikhail Komarov <nemo@nil.foundation>
2020-08-16 16:04:32 +0300

    Initial commit.

---
+++ include/nil/crypto3/detail/unbounded_shift.hpp
@@ -0,0 +1,92 @@
+//---------------------------------------------------------------------------//
+// Copyright (c) 2018-2020 Mikhail Komarov <nemo@nil.foundation>
+// Copyright (c) 2020 Alexander Sokolov <asokolov@nil.foundation>
+// Copyright (c) 2020 Nikita Kaskov <nbering@nil.foundation>
+//
+// Distributed under the Boost Software License, Version 1.0
+// See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt
+//---------------------------------------------------------------------------//
+
+#ifndef CRYPTO3_DETAIL_UNBOUNDED_SHIFT_HPP
+#define CRYPTO3_DETAIL_UNBOUNDED_SHIFT_HPP
+
+#include <boost/assert.hpp>
+
+#include <nil/crypto3/detail/stream_endian.hpp>
+
+namespace nil {
+    namespace crypto3 {
+        namespace detail {
+
+            template<int Shift, typename T>
+            struct unbounded_shifter {
+                static T shl(T x) {
+                    return unbounded_shifter<Shift - 1, T>::shl(T(x << 1));
+                }
+
+                static T shr(T x) {
+                    return unbounded_shifter<Shift - 1, T>::shr(T(x >> 1));
+                }
+            };
+
+            template<typename T>
+            struct unbounded_shifter<0, T> {
+                static T shl(T x) {
+                    return x;
+                }
+
+                static T shr(T x) {
+                    return x;
+                }
+            };
+
+            template<int Shift, typename T>
+            T unbounded_shl(T x) {
+                return unbounded_shifter<Shift, T>::shl(x);
+            }
+
+            template<int Shift, typename T>
+            T unbounded_shr(T x) {
+                return unbounded_shifter<Shift, T>::shr(x);
+            }
+
+            template<typename T>
+            T unbounded_shl(T x, std::size_t n) {
+                return x << n;
+            }
+
+            template<typename T>
+            T unbounded_shr(T x, std::size_t n) {
+                return x >> n;
+            }
+            // FIXME: it wouldn't work when Shift == sizeof(T) * CHAR_BIT
+            template<int Shift, typename T>
+            T low_bits(T x) {
+                T highmask = unbounded_shl<Shift, T>(~T());
+                return T(x & ~highmask);
+            }
+
+            template<size_t Shift, size_t TypeBits, typename T>
+            T low_bits(T x) {
+                constexpr size_t real_shift = TypeBits - Shift;
+                T lowmask = ((bool)Shift) * unbounded_shr<real_shift, T>(~T());
+                return x & lowmask;
+            }
+
+            template<size_t type_bits, typename T>
+            T low_bits(T x, size_t shift) {
+                T lowmask = ((bool)shift) * unbounded_shr<T>(~T(), type_bits - shift);
+                return x & lowmask;
+            }
+
+            template<size_t type_bits, typename T>
+            T high_bits(T x, size_t shift) {
+                T highmask = ((bool)shift) * unbounded_shl<T>(~T(), type_bits - shift);
+                return x & highmask;
+            }
+        }    // namespace detail
+    }        // namespace crypto3
+}    // namespace nil
+
+#endif    // CRYPTO3_HASH_DETAIL_UNBOUNDED_SHIFT_HPP
---
+++ test/padding.cpp
@@ -0,0 +1,28 @@
+//---------------------------------------------------------------------------//
+//
+// Copyright (c) 2018-2020 Mikhail Komarov <nemo@nil.foundation>
+//
+// Distributed under the Boost Software License, Version 1.0
+// See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt
+//---------------------------------------------------------------------------//
+
+#define BOOST_TEST_MODULE padding_test
+
+#include <nil/crypto3/modes/padding.hpp>
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/data/monomorphic.hpp>
+
+#include <iostream>
+#include <string>
+#include <cassert>
+#include <unordered_map>
+
+BOOST_AUTO_TEST_SUITE(padding_hash_test_suite)
+
+BOOST_AUTO_TEST_CASE(adler_iterator_range_hash) {
+}
+
+BOOST_AUTO_TEST_SUITE_END()
