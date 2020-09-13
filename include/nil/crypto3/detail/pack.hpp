4d2356baa75fe31a07f6440242866cd6ce62dce6
Mikhail Komarov <nemo@nil.foundation>
2020-09-13 03:46:03 +0300

    Commons updated.

--- include/nil/crypto3/detail/pack.hpp
+++ include/nil/crypto3/detail/pack.hpp
@@ -13,6 +13,7 @@
 #include <nil/crypto3/detail/stream_endian.hpp>
 #include <nil/crypto3/detail/exploder.hpp>
 #include <nil/crypto3/detail/imploder.hpp>
+#include <nil/crypto3/detail/predef.hpp>
 
 #include <boost/assert.hpp>
 #include <boost/static_assert.hpp>
