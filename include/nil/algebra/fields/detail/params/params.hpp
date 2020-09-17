155737659cd108e57ad593d178a4dc9ddc8b4f46
Mikhail Komarov <nemo@nil.foundation>
2020-09-17 22:17:28 +0300

    Minor pairing params changes.

--- include/nil/algebra/fields/detail/params/params.hpp
+++ include/nil/algebra/fields/detail/params/params.hpp
@@ -18,6 +18,7 @@ namespace nil {
                 template<typename FieldType>
                 struct params {
                     typedef FieldType field_type;
+                    constexpr static const std::size_t number_bits = field_type::number_bits;
                     typedef typename field_type::number_type number_type;
 
                     constexpr static const std::size_t modulus_bits = field_type::modulus_bits;
@@ -50,7 +51,10 @@ namespace nil {
                     typedef extension_params<FieldType> element_policy_type;
 
                 public:
+                    constexpr static const std::size_t number_bits = policy_type::number_bits;
                     typedef typename policy_type::number_type number_type;
+
+                    constexpr static const std::size_t modulus_bits = policy_type::modulus_bits;
                     typedef typename policy_type::modulus_type modulus_type;
 
                     constexpr static const modulus_type modulus = policy_type::modulus;
