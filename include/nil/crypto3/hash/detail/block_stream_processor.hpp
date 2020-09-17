644119c1269973efea5c3cfa8199b3baff6200ad
Mikhail Komarov <nemo@nil.foundation>
2020-09-17 14:42:06 +0300

    Block stream preprocessor updated #43

--- include/nil/crypto3/hash/detail/block_stream_processor.hpp
+++ include/nil/crypto3/hash/detail/block_stream_processor.hpp
@@ -14,6 +14,7 @@
 #include <iterator>
 
 #include <nil/crypto3/detail/pack.hpp>
+#include <nil/crypto3/detail/pack_numeric.hpp>
 
 #include <nil/crypto3/hash/accumulators/bits_count.hpp>
 #include <nil/crypto3/hash/accumulators/parameters/bits.hpp>
