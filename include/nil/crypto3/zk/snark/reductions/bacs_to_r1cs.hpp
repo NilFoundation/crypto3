b6416dc86a89dee2ccae466ecf0eb89b7ce9d466
nkaskov <nikitakaskov@mail.ru>
2020-10-24 16:10:26 -0400

    Reductions policies changed

--- include/nil/crypto3/zk/snark/reductions/bacs_to_r1cs.hpp
+++ include/nil/crypto3/zk/snark/reductions/bacs_to_r1cs.hpp
@@ -34,49 +34,20 @@
 #ifndef CRYPTO3_ZK_BACS_TO_R1CS_HPP
 #define CRYPTO3_ZK_BACS_TO_R1CS_HPP
 
-#include <nil/crypto3/zk/snark/relations/circuit_satisfaction_problems/bacs.hpp>
-#include <nil/crypto3/zk/snark/relations/constraint_satisfaction_problems/r1cs.hpp>
+#include <nil/crypto3/zk/snark/reductions/detail/bacs_to_r1cs.hpp>
 
 namespace nil {
     namespace crypto3 {
         namespace zk {
             namespace snark {
 
-                /**
-                 * Instance map for the BACS-to-R1CS reduction.
-                 */
                 template<typename FieldType>
-                r1cs_constraint_system<FieldType> bacs_to_r1cs_instance_map(const bacs_circuit<FieldType> &circuit) {
-                    assert(circuit.is_valid());
-                    r1cs_constraint_system<FieldType> result;
+                class bacs_to_r1cs : private detail::bacs_to_r1cs_basic_policy<FieldType>{
+                    using policy_type = typename detail::bacs_to_r1cs_basic_policy<FieldType>;
+                public:
 
-                    result.primary_input_size = circuit.primary_input_size;
-                    result.auxiliary_input_size = circuit.auxiliary_input_size + circuit.gates.size();
-
-                    for (auto &g : circuit.gates) {
-                        result.constraints.emplace_back(r1cs_constraint<FieldType>(g.lhs, g.rhs, g.output));
-                    }
-
-                    for (auto &g : circuit.gates) {
-                        if (g.is_circuit_output) {
-                            result.constraints.emplace_back(r1cs_constraint<FieldType>(1, g.output, 0));
-                        }
-                    }
-
-                    return result;
-                }
-
-                /**
-                 * Witness map for the BACS-to-R1CS reduction.
-                 */
-                template<typename FieldType>
-                r1cs_variable_assignment<FieldType>
-                    bacs_to_r1cs_witness_map(const bacs_circuit<FieldType> &circuit,
-                                             const bacs_primary_input<FieldType> &primary_input,
-                                             const bacs_auxiliary_input<FieldType> &auxiliary_input) {
-                    const r1cs_variable_assignment<FieldType> result =
-                        circuit.get_all_wires(primary_input, auxiliary_input);
-                    return result;
+                    using policy_type::instance_map;
+                    using policy_type::witness_map;
                 }
             }    // namespace snark
         }        // namespace zk
--- include/nil/crypto3/zk/snark/reductions/uscs_to_ssp.hpp
+++ include/nil/crypto3/zk/snark/reductions/uscs_to_ssp.hpp
@@ -48,187 +48,21 @@
 #ifndef CRYPTO3_ZK_USCS_TO_SSP_HPP
 #define CRYPTO3_ZK_USCS_TO_SSP_HPP
 
-#include <nil/crypto3/fft/domains/evaluation_domain.hpp>
-#include <nil/crypto3/fft/make_evaluation_domain.hpp>
-
-#include <nil/crypto3/zk/snark/relations/arithmetic_programs/ssp.hpp>
-#include <nil/crypto3/zk/snark/relations/constraint_satisfaction_problems/uscs.hpp>
+#include <nil/crypto3/zk/snark/reductions/detail/uscs_to_ssp.hpp>
 
 namespace nil {
     namespace crypto3 {
         namespace zk {
             namespace snark {
 
-                using namespace nil::crypto3::fft;
-
-                /**
-                 * Instance map for the USCS-to-SSP reduction.
-                 *
-                 * Namely, given a USCS constraint system cs, construct a SSP instance for which:
-                 *   V := (V_0(z),V_1(z),...,V_m(z))
-                 * where
-                 *   m = number of variables of the SSP
-                 * and
-                 *   each V_i is expressed in the Lagrange basis.
-                 */
                 template<typename FieldType>
-                ssp_instance<FieldType> uscs_to_ssp_instance_map(const uscs_constraint_system<FieldType> &cs) {
-                    const std::shared_ptr<evaluation_domain<FieldType>> domain =
-                        make_evaluation_domain<FieldType>(cs.num_constraints());
-                    std::vector<std::map<std::size_t, FieldType>> V_in_Lagrange_basis(cs.num_variables() + 1);
-                    for (std::size_t i = 0; i < cs.num_constraints(); ++i) {
-                        for (std::size_t j = 0; j < cs.constraints[i].terms.size(); ++j) {
-                            V_in_Lagrange_basis[cs.constraints[i].terms[j].index][i] +=
-                                cs.constraints[i].terms[j].coeff;
-                        }
-                    }
-                    for (std::size_t i = cs.num_constraints(); i < domain->m; ++i) {
-                        V_in_Lagrange_basis[0][i] += FieldType::value_type::zero();
-                    }
+                class uscs_to_ssp : private detail::uscs_to_ssp_basic_policy<FieldType>{
+                    using policy_type = typename detail::uscs_to_ssp_basic_policy<FieldType>;
+                public:
 
-                    return ssp_instance<FieldType>(
-                        domain, cs.num_variables(), domain->m, cs.num_inputs(), std::move(V_in_Lagrange_basis));
-                }
-
-                /**
-                 * Instance map for the USCS-to-SSP reduction followed by evaluation of the resulting SSP instance.
-                 *
-                 * Namely, given a USCS constraint system cs and a field element t, construct
-                 * a SSP instance (evaluated at t) for which:
-                 *   Vt := (V_0(t),V_1(t),...,V_m(t))
-                 *   Ht := (1,t,t^2,...,t^n)
-                 *   Zt := Z(t) = "vanishing polynomial of a certain set S, evaluated at t"
-                 * where
-                 *   m = number of variables of the SSP
-                 *   n = degree of the SSP
-                 */
-                template<typename FieldType>
-                ssp_instance_evaluation<FieldType>
-                    uscs_to_ssp_instance_map_with_evaluation(const uscs_constraint_system<FieldType> &cs,
-                                                             const typename FieldType::value_type &t) {
-                    const std::shared_ptr<evaluation_domain<FieldType>> domain =
-                        make_evaluation_domain<FieldType>(cs.num_constraints());
-
-                    std::vector<typename FieldType::value_type> Vt(cs.num_variables() + 1,
-                                                                   FieldType::value_type::zero());
-                    std::vector<typename FieldType::value_type> Ht(domain->m + 1);
-
-                    const typename FieldType::value_type Zt = domain->compute_vanishing_polynomial(t);
-
-                    const std::vector<typename FieldType::value_type> u = domain->evaluate_all_lagrange_polynomials(t);
-                    for (std::size_t i = 0; i < cs.num_constraints(); ++i) {
-                        for (std::size_t j = 0; j < cs.constraints[i].terms.size(); ++j) {
-                            Vt[cs.constraints[i].terms[j].index] += u[i] * cs.constraints[i].terms[j].coeff;
-                        }
-                    }
-                    for (std::size_t i = cs.num_constraints(); i < domain->m; ++i) {
-                        Vt[0] += u[i]; /* dummy constraint: 1^2 = 1 */
-                    }
-                    typename FieldType::value_type ti = FieldType::value_type::zero();
-                    for (std::size_t i = 0; i < domain->m + 1; ++i) {
-                        Ht[i] = ti;
-                        ti *= t;
-                    }
-
-                    return ssp_instance_evaluation<FieldType>(
-                        domain, cs.num_variables(), domain->m, cs.num_inputs(), t, std::move(Vt), std::move(Ht), Zt);
-                }
-
-                /**
-                 * Witness map for the USCS-to-SSP reduction.
-                 *
-                 * The witness map takes zero knowledge into account when d is random.
-                 *
-                 * More precisely, compute the coefficients
-                 *     h_0,h_1,...,h_n
-                 * of the polynomial
-                 *     H(z) := (V(z)^2-1)/Z(z)
-                 * where
-                 *   V(z) := V_0(z) + \sum_{k=1}^{m} w_k V_k(z) + d * Z(z)
-                 *   Z(z) := "vanishing polynomial of set S"
-                 * and
-                 *   m = number of variables of the SSP
-                 *   n = degree of the SSP
-                 *
-                 * This is done as follows:
-                 *  (1) compute evaluations of V on S = {sigma_1,...,sigma_n}
-                 *  (2) compute coefficients of V
-                 *  (3) compute evaluations of V on T = "coset of S"
-                 *  (4) compute evaluation of H on T
-                 *  (5) compute coefficients of H
-                 *  (6) patch H to account for d (i.e., add coefficients of the polynomial 2*d*V(z) + d*d*Z(z) )
-                 *
-                 * The code below is not as simple as the above high-level description due to
-                 * some reshuffling to save space.
-                 */
-                template<typename FieldType>
-                ssp_witness<FieldType> uscs_to_ssp_witness_map(const uscs_constraint_system<FieldType> &cs,
-                                                               const uscs_primary_input<FieldType> &primary_input,
-                                                               const uscs_auxiliary_input<FieldType> &auxiliary_input,
-                                                               const typename FieldType::value_type &d) {
-                    /* sanity check */
-
-                    assert(cs.is_satisfied(primary_input, auxiliary_input));
-
-                    uscs_variable_assignment<FieldType> full_variable_assignment = primary_input;
-                    full_variable_assignment.insert(
-                        full_variable_assignment.end(), auxiliary_input.begin(), auxiliary_input.end());
-
-                    const std::shared_ptr<evaluation_domain<FieldType>> domain =
-                        make_evaluation_domain<FieldType>(cs.num_constraints());
-
-                    std::vector<typename FieldType::value_type> aA(domain->m, FieldType::value_type::zero());
-                    assert(domain->m >= cs.num_constraints());
-                    for (std::size_t i = 0; i < cs.num_constraints(); ++i) {
-                        aA[i] += cs.constraints[i].evaluate(full_variable_assignment);
-                    }
-                    for (std::size_t i = cs.num_constraints(); i < domain->m; ++i) {
-                        aA[i] += FieldType::value_type::zero();
-                    }
-
-                    domain->iFFT(aA);
-
-                    std::vector<typename FieldType::value_type> coefficients_for_H(domain->m + 1,
-                                                                                   FieldType::value_type::zero());
-#ifdef MULTICORE
-#pragma omp parallel for
-#endif
-                    /* add coefficients of the polynomial 2*d*V(z) + d*d*Z(z) */
-                    for (std::size_t i = 0; i < domain->m; ++i) {
-                        coefficients_for_H[i] = typename FieldType::value_type(2) * d * aA[i];
-                    }
-                    domain->add_poly_Z(d.squared(), coefficients_for_H);
-
-                    multiply_by_coset(aA, FieldType::multiplicative_generator);
-                    domain->FFT(aA, FieldType::multiplicative_generator);
-
-                    std::vector<typename FieldType::value_type> &H_tmp =
-                        aA;    // can overwrite aA because it is not used later
-#ifdef MULTICORE
-#pragma omp parallel for
-#endif
-                    for (std::size_t i = 0; i < domain->m; ++i) {
-                        H_tmp[i] = aA[i].squared() - FieldType::value_type::zero();
-                    }
-
-                    domain->divide_by_Z_on_coset(H_tmp);
-
-                    domain->iFFT(H_tmp, FieldType::multiplicative_generator);
-                    multiply_by_coset(H_tmp, FieldType::multiplicative_generator.inversed());
-
-#ifdef MULTICORE
-#pragma omp parallel for
-#endif
-                    for (std::size_t i = 0; i < domain->m; ++i) {
-                        coefficients_for_H[i] += H_tmp[i];
-                    }
-
-                    return ssp_witness<FieldType>(cs.num_variables(),
-                                                  domain->m,
-                                                  cs.num_inputs(),
-                                                  d,
-                                                  full_variable_assignment,
-                                                  std::move(coefficients_for_H));
+                    using policy_type::instance_map;
+                    using policy_type::instance_map_with_evaluation;
+                    using policy_type::witness_map;
                 }
             }    // namespace snark
         }        // namespace zk
