17bc76c9b411e2aead43b014e42dfc680f951a7e
Mikhail Komarov <nemo@nil.foundation>
2021-05-25 02:08:20 +0300

    Library has been made a Boost.Random extension instead of its replacement #6

--- test/test_chacha.cpp
+++ test/test_chacha.cpp
@@ -1,13 +1,26 @@
-/* test_chacha.cpp
- *
- * Copyright Mikhail Komarov 2020
- * Distributed under the Boost Software License, Version 1.0. (See
- * accompanying file LICENSE_1_0.txt or copy at
- * http://www.boost.org/LICENSE_1_0.txt)
- *
- * $Id$
- *
- */
+//---------------------------------------------------------------------------//
+// Copyright (c) 2018-2020 Mikhail Komarov <nemo@nil.foundation>
+//
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//---------------------------------------------------------------------------//
 
 #define BOOST_TEST_MODULE chacha_rng_test
 
@@ -16,11 +29,8 @@
 #include <unordered_map>
 
 #include <boost/test/unit_test.hpp>
-#include <boost/test/data/test_case.hpp>
 
-#include <boost/random/chacha.hpp>
-
-#include <nil/crypto3/stream/chacha.hpp>
+#include <nil/crypto3/random/chacha.hpp>
 
 static const std::unordered_map<std::tuple<std::string, std::string>, std::string> = {
     {{"0000000000000000000000000000000000000000000000000000000000000000",
