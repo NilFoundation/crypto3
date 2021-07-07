36923020c9718dff6155024384a04bbd63c975ee
Ilias Khairullin <ilias@nil.foundation>
2021-07-07 15:44:21 +0300

    Algebraic random device added. #8

---
+++ include/nil/crypto3/random/algebraic_random_device.hpp
@@ -0,0 +1,167 @@
+//---------------------------------------------------------------------------//
+// Copyright (c) 2021 Mikhail Komarov <nemo@nil.foundation>
+// Copyright (c) 2021 Ilias Khairullin <ilias@nil.foundation>
+//
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//---------------------------------------------------------------------------//
+
+#ifndef CRYPTO3_RANDOM_ALGEBRAIC_RANDOM_DEVICE_HPP
+#define CRYPTO3_RANDOM_ALGEBRAIC_RANDOM_DEVICE_HPP
+
+#include <type_traits>
+
+#include <boost/random/random_device.hpp>
+#include <boost/random/uniform_int_distribution.hpp>
+
+#include <nil/crypto3/algebra/type_traits.hpp>
+
+namespace nil {
+    namespace crypto3 {
+        namespace random {
+            /**
+             * The class template algebraic_random_device models a \UniformRandomBitGenerator.
+             * https://www.boost.org/doc/libs/1_75_0/doc/html/boost_random/reference.html#boost_random.reference.concepts.uniform_random_number_generator
+             *
+             * The template parameter AlgebraicType shall denote an some algebraic type (field or curve type).
+             */
+            template<typename AlgebraicType, typename = void>
+            struct algebraic_random_device;
+
+            template<typename AlgebraicType>
+            struct algebraic_random_device<
+                AlgebraicType, typename std::enable_if<algebra::is_field<AlgebraicType>::value &&
+                                                       !algebra::is_extended_field<AlgebraicType>::value>::type> {
+            protected:
+                typedef AlgebraicType field_type;
+                typedef typename field_type::value_type field_value_type;
+                typedef typename field_type::modulus_type modulus_type;
+
+                typedef boost::random_device internal_generator_type;
+                typedef boost::random::uniform_int_distribution<modulus_type> internal_distribution_type;
+
+                constexpr static modulus_type _min = 0;
+                constexpr static modulus_type _max = field_type::modulus - 1;
+
+            public:
+                typedef field_value_type result_type;
+
+                result_type operator()() {
+                    return dist(gen);
+                }
+
+                constexpr static inline result_type min() {
+                    constexpr result_type min_value(_min);
+                    return min_value;
+                }
+
+                constexpr static inline result_type max() {
+                    constexpr result_type max_value(_max);
+                    return max_value;
+                }
+
+            protected:
+                internal_generator_type gen;
+                internal_distribution_type dist = internal_distribution_type(_min, _max);
+            };
+
+            template<typename AlgebraicType>
+            struct algebraic_random_device<
+                AlgebraicType, typename std::enable_if<algebra::is_field<AlgebraicType>::value &&
+                                                       algebra::is_extended_field<AlgebraicType>::value>::type> {
+            protected:
+                typedef AlgebraicType extended_field_type;
+                typedef typename extended_field_type::value_type extended_field_value_type;
+                typedef typename extended_field_type::underlying_field_type underlying_field_type;
+
+                typedef algebraic_random_device<underlying_field_type> internal_generator_type;
+
+            public:
+                typedef extended_field_value_type result_type;
+
+                result_type operator()() {
+                    result_type result;
+                    for (auto &coord : result.data) {
+                        coord = gen();
+                    }
+
+                    return result;
+                }
+
+                // TODO: evaluate min_value at compile-time
+                constexpr static inline result_type min() {
+                    result_type min_value;
+                    for (auto &coord : min_value.data) {
+                        coord = internal_generator_type::min();
+                    }
+
+                    return min_value;
+                }
+
+                // TODO: evaluate max_value at compile-time
+                constexpr static inline result_type max() {
+                    result_type max_value;
+                    for (auto &coord : max_value.data) {
+                        coord = internal_generator_type::max();
+                    }
+
+                    return max_value;
+                }
+
+            protected:
+                internal_generator_type gen;
+            };
+
+            template<typename AlgebraicType>
+            struct algebraic_random_device<
+                AlgebraicType, typename std::enable_if<algebra::is_curve_group<AlgebraicType>::value>::type> {
+            protected:
+                typedef AlgebraicType group_type;
+                typedef typename group_type::value_type group_value_type;
+                typedef typename group_type::curve_type::scalar_field_type scalar_field_type;
+
+                typedef algebraic_random_device<scalar_field_type> internal_generator_type;
+
+            public:
+                typedef group_value_type result_type;
+
+                // TODO: check correctness of the generation method
+                result_type operator()() {
+                    return result_type::one() * gen();
+                }
+
+                // TODO: evaluate returned value at compile-time
+                constexpr static inline result_type min() {
+                    return result_type::zero();
+                }
+
+                // TODO: evaluate returned value at compile-time
+                constexpr static inline result_type max() {
+                    return result_type::one() * (scalar_field_type::modulus - 1);
+                }
+
+            protected:
+                internal_generator_type gen;
+            };
+        }    // namespace random
+    }        // namespace crypto3
+}    // namespace nil
+
+#endif    // CRYPTO3_RANDOM_ALGEBRAIC_RANDOM_DEVICE_HPP
---
+++ test/algebraic_random_device.cpp
@@ -0,0 +1,186 @@
+//---------------------------------------------------------------------------//
+// Copyright (c) 2021 Mikhail Komarov <nemo@nil.foundation>
+// Copyright (c) 2021 Ilias Khairullin <ilias@nil.foundation>
+//
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//---------------------------------------------------------------------------//
+
+#define BOOST_TEST_MODULE algebraic_random_device_test
+
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <sstream>
+
+#include <boost/test/unit_test.hpp>
+
+#include <nil/crypto3/random/algebraic_random_device.hpp>
+
+#include <nil/crypto3/algebra/curves/bls12.hpp>
+#include <nil/crypto3/algebra/curves/mnt4.hpp>
+
+using namespace nil::crypto3;
+
+template<typename FieldParams>
+void print_field_element(std::ostream &os, const typename algebra::fields::detail::element_fp<FieldParams> &e) {
+    os << std::hex << e.data;
+}
+
+template<typename FieldParams>
+void print_field_element(std::ostream &os, const typename algebra::fields::detail::element_fp2<FieldParams> &e) {
+    os << "[" << e.data[0].data << ", " << e.data[1].data << "]";
+}
+
+template<typename FieldParams>
+void print_field_element(std::ostream &os, const typename algebra::fields::detail::element_fp3<FieldParams> &e) {
+    os << "[" << e.data[0].data << ", " << e.data[1].data << ", " << e.data[2].data << "]";
+}
+
+template<typename FpCurveGroupElement>
+void print_fp_curve_group_element(std::ostream &os, const FpCurveGroupElement &e) {
+    os << "( " << e.X.data << " : " << e.Y.data << " : " << e.Z.data << " )";
+}
+
+template<typename Fp2CurveGroupElement>
+void print_fp2_curve_group_element(std::ostream &os, const Fp2CurveGroupElement &e) {
+    os << "(" << e.X.data[0].data << " , " << e.X.data[1].data << ") : (" << e.Y.data[0].data << " , "
+       << e.Y.data[1].data << ") : (" << e.Z.data[0].data << " , " << e.Z.data[1].data << ")";
+}
+
+template<typename Fp3CurveGroupElement>
+void print_fp3_curve_group_element(std::ostream &os, const Fp3CurveGroupElement &e) {
+    os << "(" << e.X.data[0].data << " , " << e.X.data[1].data << " , " << e.X.data[2].data << ") : ("
+       << e.Y.data[0].data << " , " << e.Y.data[1].data << " , " << e.Y.data[2].data << ") : (" << e.Z.data[0].data
+       << " , " << e.Z.data[1].data << " , " << e.Z.data[2].data << ")";
+}
+
+namespace boost {
+    namespace test_tools {
+        namespace tt_detail {
+            template<typename FieldParams>
+            struct print_log_value<typename algebra::fields::detail::element_fp<FieldParams>> {
+                void operator()(std::ostream &os, typename algebra::fields::detail::element_fp<FieldParams> const &e) {
+                    print_field_element(os, e);
+                }
+            };
+
+            template<typename FieldParams>
+            struct print_log_value<typename algebra::fields::detail::element_fp2<FieldParams>> {
+                void operator()(std::ostream &os, typename algebra::fields::detail::element_fp2<FieldParams> const &e) {
+                    print_field_element(os, e);
+                }
+            };
+
+            template<typename FieldParams>
+            struct print_log_value<typename algebra::fields::detail::element_fp6_3over2<FieldParams>> {
+                void operator()(std::ostream &os, typename algebra::fields::detail::element_fp6_3over2<FieldParams> const &e) {
+                    print_field_element(os, e);
+                }
+            };
+
+            template<typename FieldParams>
+            struct print_log_value<typename algebra::fields::detail::element_fp12_2over3over2<FieldParams>> {
+                void operator()(std::ostream &os,
+                                typename algebra::fields::detail::element_fp12_2over3over2<FieldParams> const &e) {
+                    print_field_element(os, e);
+                }
+            };
+
+            template<>
+            struct print_log_value<typename algebra::curves::mnt4<298>::g1_type::value_type> {
+                void operator()(std::ostream &os, typename algebra::curves::mnt4<298>::g1_type::value_type const &e) {
+                    print_fp_curve_group_element(os, e);
+                }
+            };
+
+            template<>
+            struct print_log_value<typename algebra::curves::mnt4<298>::g2_type::value_type> {
+                void operator()(std::ostream &os, typename algebra::curves::mnt4<298>::g2_type::value_type const &e) {
+                    print_fp2_curve_group_element(os, e);
+                }
+            };
+
+            template<>
+            struct print_log_value<typename algebra::curves::bls12<381>::g1_type::value_type> {
+                void operator()(std::ostream &os, typename algebra::curves::bls12<381>::g1_type::value_type const &e) {
+                    print_fp_curve_group_element(os, e);
+                }
+            };
+
+            template<>
+            struct print_log_value<typename algebra::curves::bls12<381>::g2_type::value_type> {
+                void operator()(std::ostream &os, typename algebra::curves::bls12<381>::g2_type::value_type const &e) {
+                    print_fp2_curve_group_element(os, e);
+                }
+            };
+
+            template<template<typename, typename> class P, typename K, typename V>
+            struct print_log_value<P<K, V>> {
+                void operator()(std::ostream &, P<K, V> const &) {
+                }
+            };
+        }    // namespace tt_detail
+    }        // namespace test_tools
+}    // namespace boost
+
+template<typename T>
+void test_UniformRandomBitGenerator() {
+    using generator_type = random::algebraic_random_device<T>;
+    using printer_type = boost::test_tools::tt_detail::print_log_value<typename generator_type::result_type>;
+    generator_type g;
+    printer_type print;
+    std::cout << "min = ";
+    print(std::cout, generator_type::min());
+    std::cout << std::endl;
+    std::cout << "max = ";
+    print(std::cout, generator_type::max());
+    std::cout << std::endl;
+    for (auto i = 0; i < 10; i++) {
+        print(std::cout, g());
+        std::cout << std::endl;
+    }
+}
+
+BOOST_AUTO_TEST_SUITE(algebraic_random_device_tests)
+
+BOOST_AUTO_TEST_CASE(mnt4_test) {
+    using curve_type = algebra::curves::mnt4<298>;
+    using scalar_field_type = typename curve_type::scalar_field_type;
+
+    test_UniformRandomBitGenerator<scalar_field_type>();
+    test_UniformRandomBitGenerator<typename curve_type::g1_type::underlying_field_type>();
+    test_UniformRandomBitGenerator<typename curve_type::g2_type::underlying_field_type>();
+    test_UniformRandomBitGenerator<typename curve_type::g1_type>();
+    test_UniformRandomBitGenerator<typename curve_type::g2_type>();
+}
+
+BOOST_AUTO_TEST_CASE(bls12_381_test) {
+    using curve_type = algebra::curves::bls12<381>;
+    using scalar_field_type = typename curve_type::scalar_field_type;
+
+    test_UniformRandomBitGenerator<scalar_field_type>();
+    test_UniformRandomBitGenerator<typename curve_type::g1_type::underlying_field_type>();
+    test_UniformRandomBitGenerator<typename curve_type::g2_type::underlying_field_type>();
+    test_UniformRandomBitGenerator<typename curve_type::g1_type>();
+    test_UniformRandomBitGenerator<typename curve_type::g2_type>();
+}
+
+BOOST_AUTO_TEST_SUITE_END()
