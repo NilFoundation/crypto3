69c483daea52c44a8ed3e2f9de6009fd68dd3ddd
nkaskov <mail.nikita.kaskov@gmail.com>
2021-07-22 17:47:06 +0300

    Twisted Edwards element g1 in affine form added #53

--- include/nil/crypto3/algebra/curves/detail/forms/twisted_edwards/element_g1_affine.hpp
+++ include/nil/crypto3/algebra/curves/detail/forms/twisted_edwards/element_g1_affine.hpp
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------//
-// Copyright (c) 2020-2021 Mikhail Komarov <nemo@nil.foundation>
-// Copyright (c) 2020-2021 Nikita Kaskov <nbering@nil.foundation>
-// Copyright (c) 2020-2021 Ilias Khairullin <ilias@nil.foundation>
+// Copyright (c) 2021 Mikhail Komarov <nemo@nil.foundation>
+// Copyright (c) 2021 Nikita Kaskov <nbering@nil.foundation>
+// Copyright (c) 2021 Ilias Khairullin <ilias@nil.foundation>
 //
 // MIT License
 //
@@ -24,8 +24,8 @@
 // SOFTWARE.
 //---------------------------------------------------------------------------//
 
-#ifndef CRYPTO3_ALGEBRA_CURVES_TWISTED_EDWARDS_G1_ELEMENT_HPP
-#define CRYPTO3_ALGEBRA_CURVES_TWISTED_EDWARDS_G1_ELEMENT_HPP
+#ifndef CRYPTO3_ALGEBRA_CURVES_TWISTED_EDWARDS_G1_ELEMENT_AFFINE_HPP
+#define CRYPTO3_ALGEBRA_CURVES_TWISTED_EDWARDS_G1_ELEMENT_AFFINE_HPP
 
 #include <nil/crypto3/algebra/curves/detail/scalar_mul.hpp>
 #include <nil/crypto3/algebra/curves/detail/forms.hpp>
@@ -41,7 +41,7 @@ namespace nil {
                      *    @tparam Version version of the curve
                      *
                      */
-                    template<typename CurveBasicPolicy, 
+                    template<typename CurveParams, 
                              algebra::curves::detail::forms, 
                              typename Coordinates>
                     struct element_g1;
@@ -51,42 +51,53 @@ namespace nil {
                      *  Description: https://hyperelliptic.org/EFD/g1p/auto-twisted.html
                      *
                      */
-                    template<typename CurveBasicPolicy>
+                    template<typename CurveParams>
                     struct element_g1<algebra::curves::detail::forms::twisted_edwards, 
                                       algebra::curves::detail::twisted_edwards_coordinates::affine> {
-                        constexpr static const std::size_t version = Version;
 
-                        using group_type = edwards_g1<version>;
-
-                        using policy_type = CurveBasicPolicy;
-                        using field_type = typename policy_type::g1_field_type;
+                        using field_type = typename CurveParams::g1_field_type;
                     private:
+                        using params_type = CurveParams;
                         using field_value_type = typename field_type::value_type;
                     public:
+                        using group_type = typename params_type::group_type;
+
                         constexpr static const algebra::curves::detail::forms form = 
-                        algebra::curves::detail::forms::twisted_edwards;
+                            algebra::curves::detail::forms::twisted_edwards;
+                        constexpr static const 
+                            algebra::curves::detail::twisted_edwards_coordinates coordinates = 
+                            algebra::curves::detail::twisted_edwards_coordinates::affine;
 
                         field_value_type X;
                         field_value_type Y;
-                        field_value_type Z;
 
                         /*************************  Constructors and zero/one  ***********************************/
                         /** @brief
                          *    @return the point at infinity by default
                          *
                          */
-                        constexpr element_g1() : element_g1(policy_type::g1_zero_fill[0], 
-                            policy_type::g1_zero_fill[1], policy_type::g1_zero_fill[2]) {};
+                        constexpr element_g1() : element_g1(params_type::g1_zero_fill[0], 
+                            params_type::g1_zero_fill[1]) {};
 
                         /** @brief
                          *    @return the selected point $(X:Y:Z)$ in the projective coordinates
                          *
                          */
-                        constexpr element_g1(field_value_type in_X, field_value_type in_Y,
-                                           field_value_type in_Z = field_value_type::one()) {
+                        constexpr element_g1(field_value_type in_X, field_value_type in_Y) {
                             this->X = in_X;
                             this->Y = in_Y;
-                            this->Z = in_Z;
+                        };
+
+                        constexpr element_g1(element_g1<params_type, form, 
+                            algebra::curves::detail::twisted_edwards_coordinates::projective> other) {
+
+                            if (other.Z.is_zero()) {
+                                *this = this->zero();
+                            }
+
+                            field_value_type Z_inv = other.Z.inversed();
+                            X[0] = other.X * Z_inv;
+                            Y[1] = other.Y * Z_inv;
                         };
 
                         /** @brief Get the point at infinity
@@ -99,8 +110,7 @@ namespace nil {
                          *
                          */
                         static element_g1 one() {
-                            return element_g1(policy_type::g1_one_fill[0], policy_type::g1_one_fill[1],
-                                                              policy_type::g1_one_fill[2]);
+                            return element_g1(params_type::g1_one_fill[0], params_type::g1_one_fill[1]);
                         }
 
                         /*************************  Comparison operations  ***********************************/
@@ -116,13 +126,11 @@ namespace nil {
 
                             /* now neither is O */
 
-                            // X1/Z1 = X2/Z2 <=> X1*Z2 = X2*Z1
-                            if ((this->X * other.Z) != (other.X * this->Z)) {
+                            if (this->X != other.X) {
                                 return false;
                             }
 
-                            // Y1/Z1 = Y2/Z2 <=> Y1*Z2 = Y2*Z1
-                            if ((this->Y * other.Z) != (other.Y * this->Z)) {
+                            if (this->Y  != other.Y) {
                                 return false;
                             }
 
@@ -137,14 +145,7 @@ namespace nil {
                          * @return true if element from group G1 is the point at infinity
                          */
                         bool is_zero() const {
-                            return (this->X.is_zero() && this->Y.is_one() && this->Z.is_zero());
-                        }
-                        /** @brief
-                         *
-                         * @return true if element from group G1 in affine coordinates
-                         */
-                        bool is_special() const {
-                            return (this->is_zero() || this->Z == field_value_type::one());
+                            return (*this) == element_g1::zero();
                         }
 
                         /** @brief
@@ -161,9 +162,9 @@ namespace nil {
                                 field_value_type XX = this->X.squared();
                                 field_value_type YY = this->Y.squared();
 
-                                return (field_value_type(policy_type::a)*XX + YY == 
+                                return (field_value_type(params_type::a)*XX + YY == 
                                     field_value_type::one() + 
-                                    field_value_type(policy_type::d)*XX*YY);
+                                    field_value_type(params_type::d)*XX*YY);
                             }
                         }
 
@@ -173,7 +174,6 @@ namespace nil {
                             // handle special cases having to do with O
                             this->X = other.X;
                             this->Y = other.Y;
-                            this->Z = other.Z;
 
                             return *this;
                         }
@@ -196,7 +196,7 @@ namespace nil {
                         }
 
                         element_g1 operator-() const {
-                            return element_g1(-(this->X), this->Y, this->Z);
+                            return element_g1(-(this->X), this->Y);
                         }
 
                         element_g1 operator-(const element_g1 &B) const {
@@ -217,17 +217,21 @@ namespace nil {
                         }
 
                     private:
-
+                        /** @brief
+                         * Affine addition formulas: (x1,y1)+(x2,y2)=(x3,y3) where 
+                         *   x3 = (x1*y2+y1*x2)/(1+d*x1*x2*y1*y2)
+                         *   y3 = (y1*y2-a*x1*x2)/(1-d*x1*x2*y1*y2)
+                         */
                         element_g1 add(const element_g1 &other) const {
                             field_value_type XX = (this->X)*(other.X);
                             field_value_type YY = (this->Y)*(other.Y);
                             field_value_type XY = (this->X)*(other.Y);
                             field_value_type YX = (this->Y)*(other.X);
 
-                            field_value_type lambda = d * XX * YY;
+                            field_value_type lambda = params_type::d * XX * YY;
                             field_value_type X3 = (XY + YX) * 
                                 (field_value_type::one() + lambda).inversed();
-                            field_value_type Y3 = (YY - a * XX) * 
+                            field_value_type Y3 = (YY - params_type::a * XX) * 
                                 (field_value_type::one() - lambda).inversed();
 
                             return element_g1(X3, Y3);
@@ -235,83 +239,36 @@ namespace nil {
 
                     public:
                         /*************************  Reducing operations  ***********************************/
-                        /** @brief
-                         *
-                         * @return return the corresponding element from inverted coordinates to affine coordinates
-                         */
-                        element_g1 to_affine() const {
-                            field_value_type p_out[3];
 
-                            if (this->is_zero()) {
-                                p_out[0] = field_value_type::zero();
-                                p_out[1] = field_value_type::one();
-                                p_out[2] = field_value_type::one();
-                            } else {
-                                // go from inverted coordinates to projective coordinates
-                                field_value_type tX = this->Y * this->Z;
-                                field_value_type tY = this->X * this->Z;
-                                field_value_type tZ = this->X * this->Y;
-                                // go from projective coordinates to affine coordinates
-                                field_value_type tZ_inv = tZ.inversed();
-                                p_out[0] = tX * tZ_inv;
-                                p_out[1] = tY * tZ_inv;
-                                p_out[2] = field_value_type::one();
-                            }
+                        // /** @brief
+                        //  *
+                        //  * @return return the corresponding element from twisted edwards form and 
+                        //  * affine coordinates to montgomery form and affine coordinates
+                        //  */
+                        // // This should be moved to montgomery form element constructor
+                        // element_g1<params_type, 
+                        //     algebra::curves::detail::forms::montgomery, coordinates> to_montgomery() const {
+                        //     field_value_type p_out[3];
 
-                            return element_g1(p_out[0], p_out[1], p_out[2]);
-                        }
-                        /** @brief
-                         *
-                         * @return return the corresponding element from projective coordinates to affine coordinates
-                         */
-                        element_g1 to_projective() const {
-                            field_value_type p_out[3];
-
-                            if (this->Z.is_zero()) {
-                                return *this;
-                            }
-
-                            field_value_type Z_inv = this->Z.inversed();
-                            p_out[0] = this->X * Z_inv;
-                            p_out[1] = this->Y * Z_inv;
-                            p_out[2] = field_value_type::one();
-
-                            return element_g1(p_out[0], p_out[1], p_out[2]);
-                        }
-
-                        element_g1 to_montgomery() const {
-                            field_value_type p_out[3];
-
-                            // The only points on the curve with x=0 or y=1 (for which birational equivalence is not valid), 
-                            // are (0,1) and (0,-1), both of which are of low order, and should therefore not occur.
-                            assert(!(this->X.is_zero()) && this->Y != field_value_type::one());
+                        //     // The only points on the curve with x=0 or y=1 (for which birational equivalence is not valid), 
+                        //     // are (0,1) and (0,-1), both of which are of low order, and should therefore not occur.
+                        //     assert(!(this->X.is_zero()) && this->Y != field_value_type::one());
                             
-                            // (x, y) -> (u, v) where
-                            //      u = (1 + y) / (1 - y)
-                            //      v = u / x
-                            field_value_type u = 
-                                (field_value_type::one() + this->Y) * 
-                                (field_value_type::one() - this->Y).inversed();
-                            return element_g1{u, 
-                                    policy_type::scale * u * this->X.inversed()};
-                        }
-
-                    private:
-                        constexpr static const field_value_type a = policy_type::a;
-                        constexpr static const field_value_type d = policy_type::d;
+                        //     // (x, y) -> (u, v) where
+                        //     //      u = (1 + y) / (1 - y)
+                        //     //      v = u / x
+                        //     field_value_type u = 
+                        //         (field_value_type::one() + this->Y) * 
+                        //         (field_value_type::one() - this->Y).inversed();
+                        //     return element_g1<params_type, 
+                        //         algebra::curves::detail::forms::montgomery, coordinates>{u, 
+                        //             params_type::scale * u * this->X.inversed()};
+                        // }
                     };
-
-                    template <std::size_t Version>
-                    constexpr typename element_g1<Version>::field_type::value_type const 
-                        element_g1<Version>::a;
-                    
-                    template <std::size_t Version>
-                    constexpr typename element_g1<Version>::field_type::value_type const 
-                        element_g1<Version>::d;
 
                 }    // namespace detail
             }        // namespace curves
         }            // namespace algebra
     }                // namespace crypto3
 }    // namespace nil
-#endif    // CRYPTO3_ALGEBRA_CURVES_TWISTED_EDWARDS_G1_ELEMENT_HPP
+#endif    // CRYPTO3_ALGEBRA_CURVES_TWISTED_EDWARDS_G1_ELEMENT_AFFINE_HPP
