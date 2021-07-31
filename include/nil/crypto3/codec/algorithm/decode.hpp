5a1e8cde2596d3d3c3c058d5bc82fe5e3fd8518c
Mikhail Komarov <nemo@nil.foundation>
2021-07-31 09:47:43 +0300

    Initializer list encoding/decoding implemented.

--- include/nil/crypto3/codec/algorithm/decode.hpp
+++ include/nil/crypto3/codec/algorithm/decode.hpp
@@ -214,6 +214,94 @@ namespace nil {
 
             return DecoderImpl(r, CodecAccumulator());
         }
+
+        /*!
+         * @brief Decodes the elements with particular codec defined with Decoder
+         * in the range, defined by rng and inserts the result to destination
+         * range beginning at out.
+         *
+         * @ingroup codec_algorithms
+         *
+         * @tparam Decoder Must meet the requirements of Codec which determines the
+         * particular algorithm to be used with range given.
+         * @tparam T Decoded initializer list value type.
+         * @tparam CodecAccumulator Must meet the requirements of AccumulatorSet.
+         *
+         * @param rng Defines the range to be processed by decoder.
+         * @param out Defines the beginning of destination range.
+         * @return CodecAccumulator AccumulatorSet non-const reference equal to acc.
+         */
+        template<typename Decoder, typename T, typename OutputIterator>
+        typename std::enable_if<detail::is_iterator<OutputIterator>::value, OutputIterator>::type
+            decode(std::initializer_list<T> rng, OutputIterator out) {
+
+            typedef typename Decoder::stream_decoder_type DecodingMode;
+            typedef typename codec::accumulator_set<DecodingMode> CodecAccumulator;
+
+            typedef codec::detail::value_codec_impl<CodecAccumulator> DecoderStateImpl;
+            typedef codec::detail::itr_codec_impl<DecoderStateImpl, OutputIterator> DecoderImpl;
+
+            return DecoderImpl(rng, std::move(out), CodecAccumulator());
+        }
+
+        /*!
+         * @brief Decodes the elements with particular codec defined with Decoder
+         * in the range, defined by rng and inserts the result to destination
+         * range beginning at out.
+         *
+         * @ingroup codec_algorithms
+         *
+         * @tparam Decoder Must meet the requirements of Codec which determines the
+         * particular algorithm to be used with range given.
+         * @tparam T Decoded initializer list value type.
+         * @tparam CodecAccumulator Must meet the requirements of AccumulatorSet.
+         *
+         * @param rng Defines the range to be processed by decoder.
+         * @param acc AccumulatorSet defines the destination decoded data would be stored.
+         * @return CodecAccumulator AccumulatorSet non-const reference equal to acc.
+         */
+        template<typename Decoder,
+                 typename T,
+                 typename CodecAccumulator = typename codec::accumulator_set<typename Decoder::stream_decoder_type>>
+        typename std::enable_if<boost::accumulators::detail::is_accumulator_set<CodecAccumulator>::value,
+                                CodecAccumulator>::type &
+            decode(std::initializer_list<T> rng, CodecAccumulator &acc) {
+
+            typedef codec::detail::value_codec_impl<CodecAccumulator> DecoderStateImpl;
+            typedef codec::detail::range_codec_impl<DecoderStateImpl> DecoderImpl;
+
+            return DecoderImpl(rng, std::forward<CodecAccumulator>(acc));
+        }
+
+        /*!
+         * @brief Decodes the elements with particular codec defined with Decoder
+         * in the range, defined by rng and returns the result with any
+         * type convertible to the type satisfying SequenceContainer with the value
+         * type satisfying Integral concept requirements.
+         *
+         * @ingroup codec_algorithms
+         *
+         * @tparam Decoder Must meet the requirements of Codec which determines the
+         * particular algorithm to be used with range given.
+         * @tparam T Decoded initializer list value type.
+         *
+         * @param r Defines the range to be processed by decoder.
+         *
+         * @return Decoded data emplaced in any type convertible to the type
+         * satisfying SequenceContainer with the value type satisfying Integral
+         * concept requirements.
+         */
+        template<typename Decoder,
+                 typename T,
+                 typename CodecAccumulator = typename codec::accumulator_set<typename Decoder::stream_decoder_type>>
+        codec::detail::range_codec_impl<codec::detail::value_codec_impl<CodecAccumulator>>
+            decode(std::initializer_list<T> r) {
+
+            typedef codec::detail::value_codec_impl<CodecAccumulator> DecoderStateImpl;
+            typedef codec::detail::range_codec_impl<DecoderStateImpl> DecoderImpl;
+
+            return DecoderImpl(r, CodecAccumulator());
+        }
     }    // namespace crypto3
 }    // namespace nil
 
--- include/nil/crypto3/codec/algorithm/encode.hpp
+++ include/nil/crypto3/codec/algorithm/encode.hpp
@@ -215,6 +215,95 @@ namespace nil {
 
             return EncoderImpl(r, CodecAccumulator());
         }
+
+        /*!
+         * @brief Encodes the elements with particular codec defined with Encoder
+         * in the range, defined by rng and inserts the result to destination
+         * range beginning at out.
+         *
+         * @ingroup codec_algorithms
+         *
+         * @tparam Encoder Must meet the requirements of Codec which determines the
+         * particular algorithm to be used with range given.
+         * @tparam T Encoded initializer list value type.
+         * @tparam OutputIterator Must meet the requirements of OutputIterator.
+         *
+         * @param rng Defines the range to be processed by encoder.
+         * @param out Defines the beginning of destination range.
+         *
+         * @return Output iterator to the element in the destination range, one past the last element inserted.
+         */
+        template<typename Encoder, typename T, typename OutputIterator>
+        typename std::enable_if<detail::is_iterator<OutputIterator>::value, OutputIterator>::type
+            encode(std::initializer_list<T> rng, OutputIterator out) {
+
+            typedef typename Encoder::stream_encoder_type EncodingMode;
+            typedef typename codec::accumulator_set<EncodingMode> CodecAccumulator;
+
+            typedef codec::detail::value_codec_impl<CodecAccumulator> EncoderStateImpl;
+            typedef codec::detail::itr_codec_impl<EncoderStateImpl, OutputIterator> EncoderImpl;
+
+            return EncoderImpl(rng, std::move(out), CodecAccumulator());
+        }
+
+        /*!
+         * @brief Encodes the elements with particular codec defined with Encoder
+         * in the range, defined by rng and inserts the result to destination
+         * range beginning at out.
+         *
+         * @ingroup codec_algorithms
+         *
+         * @tparam Encoder Must meet the requirements of Codec which determines the
+         * particular algorithm to be used with range given.
+         * @tparam T Encoded initializer list value type.
+         * @tparam CodecAccumulator Must meet the requirements of AccumulatorSet.
+         *
+         * @param rng Defines the range to be processed by encoder.
+         * @param acc AccumulatorSet defines the destination encoded data would be stored.
+         * @return CodecAccumulator AccumulatorSet non-const reference equal to acc.
+         */
+        template<typename Encoder,
+                 typename T,
+                 typename CodecAccumulator = typename codec::accumulator_set<typename Encoder::stream_encoder_type>>
+        typename std::enable_if<boost::accumulators::detail::is_accumulator_set<CodecAccumulator>::value,
+                                CodecAccumulator>::type &
+            encode(std::initializer_list<T> rng, CodecAccumulator &acc) {
+
+            typedef codec::detail::ref_codec_impl<CodecAccumulator> EncoderStateImpl;
+            typedef codec::detail::range_codec_impl<EncoderStateImpl> EncoderImpl;
+
+            return EncoderImpl(rng, std::forward<CodecAccumulator>(acc));
+        }
+
+        /*!
+         * @brief Encodes the elements with particular codec defined with Encoder
+         * in the range, defined by rng and returns the result with any
+         * type convertible to the type satisfying SequenceContainer with the value
+         * type satisfying Integral concept requirements.
+         *
+         * @ingroup codec_algorithms
+         *
+         * @tparam Encoder Must meet the requirements of Codec which determines the
+         * particular algorithm to be used with range given.
+         * @tparam T Encoded initializer list value type.
+         *
+         * @param r Defines the range to be processed by encoder.
+         *
+         * @return Encoded data emplaced in any type convertible to the type
+         * satisfying SequenceContainer with the value type satisfying Integral
+         * concept requirements.
+         */
+        template<typename Encoder,
+                 typename T,
+                 typename CodecAccumulator = typename codec::accumulator_set<typename Encoder::stream_encoder_type>>
+        codec::detail::range_codec_impl<codec::detail::value_codec_impl<CodecAccumulator>>
+            encode(std::initializer_list<T> r) {
+
+            typedef codec::detail::value_codec_impl<CodecAccumulator> EncoderStateImpl;
+            typedef codec::detail::range_codec_impl<EncoderStateImpl> EncoderImpl;
+
+            return EncoderImpl(r, CodecAccumulator());
+        }
     }    // namespace crypto3
 }    // namespace nil
 
