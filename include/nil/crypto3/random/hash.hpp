1b3a950e8b446fe393061e26a4d7c2ede3c6160a
Ilias Khairullin <ilias@nil.foundation>
2021-08-17 12:17:46 +0300

    Hash based generated fixed.

--- include/nil/crypto3/random/hash.hpp
+++ include/nil/crypto3/random/hash.hpp
@@ -48,13 +48,11 @@ namespace nil {
 
             // TODO: replace pack with marshaling
             template<typename Hash, typename ResultType>
-            struct hash<
-                Hash,
-                ResultType,
-                typename std::enable_if<
-                    algebra::is_field<typename ResultType::field_type>::value &&
-                    !algebra::is_extended_field<typename ResultType::field_type>::value &&
-                    (ResultType::field_type::value_bits <= Hash::digest_bits)>::type> {
+            struct hash<Hash,
+                        ResultType,
+                        typename std::enable_if<algebra::is_field<typename ResultType::field_type>::value &&
+                                                !algebra::is_extended_field<typename ResultType::field_type>::value &&
+                                                (ResultType::field_type::value_bits <= Hash::digest_bits)>::type> {
                 typedef Hash hash_type;
                 typedef ResultType result_type;
                 typedef std::uint64_t input_type;
@@ -94,17 +92,14 @@ namespace nil {
                     input_type iter = 0;
                     std::array<std::uint8_t, 2 * sizeof(input_type)> seed_bytes;
                     typename hash_type::digest_type res;
-                    typename result_type::field_type::modulus_type result;
+                    typename result_type::field_type::integral_type result;
                     do {
-                        ::nil::crypto3::detail::pack<stream_endian::little_byte_big_bit,
-                                                     stream_endian::big_byte_big_bit,
-                                                     sizeof(input_type) * 8,
-                                                     8>(
+                        ::nil::crypto3::detail::pack_to<stream_endian::big_byte_big_bit, sizeof(input_type) * 8, 8>(
                             std::vector<input_type> {
                                 state,
                                 iter,
                             },
-                            seed_bytes);
+                            seed_bytes.begin());
                         res = ::nil::crypto3::hash<hash_type>(seed_bytes);
                         ::nil::crypto3::multiprecision::import_bits(
                             result, res.begin(), res.begin() + bincode::modulus_chunks, 8, false);
