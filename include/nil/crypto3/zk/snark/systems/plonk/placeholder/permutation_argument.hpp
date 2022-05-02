f8cab5f63f51e2c3da23c9c234915fee0bc50775
nkaskov <mail.nikita.kaskov@gmail.com>
2022-05-02 23:14:49 +0300

    Placeholder permutation argument polynomials in dfs form usage updated. #43

--- include/nil/crypto3/zk/snark/systems/plonk/placeholder/permutation_argument.hpp
+++ include/nil/crypto3/zk/snark/systems/plonk/placeholder/permutation_argument.hpp
@@ -27,7 +27,7 @@
 #ifndef CRYPTO3_ZK_PLONK_PLACEHOLDER_PERMUTATION_ARGUMENT_HPP
 #define CRYPTO3_ZK_PLONK_PLACEHOLDER_PERMUTATION_ARGUMENT_HPP
 
-#include <nil/crypto3/math/polynomial/polynomial.hpp>
+#include <nil/crypto3/math/polynomial/polynomial_dfs.hpp>
 #include <nil/crypto3/math/polynomial/shift.hpp>
 #include <nil/crypto3/math/domains/evaluation_domain.hpp>
 #include <nil/crypto3/math/algorithms/make_evaluation_domain.hpp>
@@ -59,9 +59,9 @@ namespace nil {
 
                 public:
                     struct prover_result_type {
-                        std::array<math::polynomial<typename FieldType::value_type>, argument_size> F;
+                        std::array<math::polynomial_dfs<typename FieldType::value_type>, argument_size> F;
 
-                        math::polynomial<typename FieldType::value_type> permutation_polynomial;
+                        math::polynomial_dfs<typename FieldType::value_type> permutation_polynomial;
 
                         typename permutation_commitment_scheme_type::precommitment_type
                             permutation_poly_precommitment;
@@ -81,9 +81,9 @@ namespace nil {
 
                         const std::size_t table_rows = table_description.rows_amount;
 
-                        const std::vector<math::polynomial<typename FieldType::value_type>> &S_sigma =
+                        const std::vector<math::polynomial_dfs<typename FieldType::value_type>> &S_sigma =
                             preprocessed_data.permutation_polynomials;
-                        const std::vector<math::polynomial<typename FieldType::value_type>> &S_id =
+                        const std::vector<math::polynomial_dfs<typename FieldType::value_type>> &S_id =
                             preprocessed_data.identity_polynomials;
                         std::shared_ptr<math::evaluation_domain<FieldType>> domain = preprocessed_data.common_data.basic_domain;
 
@@ -93,8 +93,8 @@ namespace nil {
                         typename FieldType::value_type gamma = transcript.template challenge<FieldType>();
 
                         // 2. Calculate id_binding, sigma_binding for j from 1 to N_rows
-                        math::polynomial<typename FieldType::value_type> id_binding = {1};
-                        math::polynomial<typename FieldType::value_type> sigma_binding = {1};
+                        math::polynomial_dfs<typename FieldType::value_type> id_binding = {1};
+                        math::polynomial_dfs<typename FieldType::value_type> sigma_binding = {1};
 
                         for (std::size_t i = 0; i < S_id.size(); i++) {
                             id_binding = id_binding * (column_polynomials[i] + beta * S_id[i] + gamma);
@@ -102,25 +102,13 @@ namespace nil {
                         }
 
                         // 3. Calculate $V_P$
-                        std::vector<typename FieldType::value_type> V_P_interpolation_points(table_rows);
+                        math::polynomial_dfs<typename FieldType::value_type> V_P;
+                        V_P.resize(table_rows);
 
-                        V_P_interpolation_points[0] = FieldType::value_type::one();
+                        V_P[0] = FieldType::value_type::one();
                         for (std::size_t j = 1; j < table_rows; j++) {
-                            typename FieldType::value_type tmp_mul_result = FieldType::value_type::one();
-                            for (std::size_t i = 0; i <= j - 1; i++) {
-                                // TODO: use one division
-                                tmp_mul_result =
-                                    tmp_mul_result * (id_binding.evaluate(domain->get_domain_element(i)) /
-                                                      sigma_binding.evaluate(domain->get_domain_element(i)));
-                            }
-
-                            V_P_interpolation_points[j] = tmp_mul_result;
+                            V_P[j] = V_P[j - 1] * id_binding[j - 1] / sigma_binding[j - 1];
                         }
-
-                        domain->inverse_fft(V_P_interpolation_points);
-
-                        math::polynomial<typename FieldType::value_type> V_P(V_P_interpolation_points.begin(),
-                                                                             V_P_interpolation_points.end());
 
                         // 4. Compute and add commitment to $V_P$ to $\text{transcript}$.
                         typename permutation_commitment_scheme_type::precommitment_type V_P_tree =
@@ -130,19 +118,19 @@ namespace nil {
                         transcript(V_P_commitment);
 
                         // 5. Calculate g_perm, h_perm
-                        math::polynomial<typename FieldType::value_type> g = {1};
-                        math::polynomial<typename FieldType::value_type> h = {1};
+                        math::polynomial_dfs<typename FieldType::value_type> g = {1};
+                        math::polynomial_dfs<typename FieldType::value_type> h = {1};
 
                         for (std::size_t i = 0; i < S_id.size(); i++) {
                             g = g * (column_polynomials[i] + beta * S_id[i] + gamma);
                             h = h * (column_polynomials[i] + beta * S_sigma[i] + gamma);
                         }
 
-                        math::polynomial<typename FieldType::value_type> one_polynomial = {1};
-                        std::array<math::polynomial<typename FieldType::value_type>, argument_size> F;
+                        math::polynomial_dfs<typename FieldType::value_type> one_polynomial = {1};
+                        std::array<math::polynomial_dfs<typename FieldType::value_type>, argument_size> F;
 
-                        math::polynomial<typename FieldType::value_type> V_P_shifted =
-                            math::polynomial_shift<FieldType>(V_P, domain->get_domain_element(1));
+                        math::polynomial_dfs<typename FieldType::value_type> V_P_shifted =
+                            math::polynomial_shift<FieldType>(V_P, 1);
 
 
                         F[0] = preprocessed_data.common_data.lagrange_0 * (one_polynomial - V_P);
@@ -169,9 +157,9 @@ namespace nil {
                                     const typename permutation_commitment_scheme_type::commitment_type &V_P_commitment,
                                     transcript_type &transcript = transcript_type()) {
 
-                        const std::vector<math::polynomial<typename FieldType::value_type>> &S_sigma =
+                        const std::vector<math::polynomial_dfs<typename FieldType::value_type>> &S_sigma =
                             preprocessed_data.permutation_polynomials;
-                        const std::vector<math::polynomial<typename FieldType::value_type>> &S_id =
+                        const std::vector<math::polynomial_dfs<typename FieldType::value_type>> &S_id =
                             preprocessed_data.identity_polynomials;
 
                         // 1. Get beta, gamma
