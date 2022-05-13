dc6d307c29bbb42b1c689c5d8cf0c50a28742bb1
Zerg1996 <Zerg1996@yandex.ru>
2022-05-13 19:49:25 +0200

    Update interfaces #5

--- example/merkle/merkle.cpp
+++ example/merkle/merkle.cpp
@@ -1,6 +1,6 @@
 //---------------------------------------------------------------------------//
 // Copyright (c) 2018-2020 Mikhail Komarov <nemo@nil.foundation>
-// Copyright (c) 2021 Aleksei Moskvin <alalmoskvin@gmail.com>
+// Copyright (c) 2021-2022 Aleksei Moskvin <alalmoskvin@gmail.com>
 //
 // MIT License
 //
@@ -31,15 +31,16 @@
 #include <nil/crypto3/container/merkle/proof.hpp>
 
 using namespace nil::crypto3;
+using namespace nil::crypto3::containers;
 
 int main() {
     std::vector<std::array<char, 1> > data_on_leafs = {{'0'}, {'1'}, {'2'}, {'3'}, {'4'}, {'5'}, {'6'}, {'7'}, {'8'}};
     std::array<char, 1> element_not_in_tree = {'9'};
-    merkle_tree<hashes::blake2b<224>, 3> tree(data_on_leafs);
+    merkle_tree<hashes::blake2b<224>, 3> tree = make_merkle_tree<hashes::blake2b<224>, 3>(data_on_leafs.begin(), data_on_leafs.end());
     merkle_proof<hashes::blake2b<224>, 3> proof_leaf_3(tree, 3);
     merkle_proof<hashes::blake2b<224>, 3> proof_leaf_0(tree, 0);
-    std::cout << "Tree structure:" << std::endl;
-    std::cout << tree << std::endl;
+//    std::cout << "Tree structure:" << std::endl;
+//    std::cout << tree << std::endl;
     std::vector<std::array<char, 1>> data_to_check = {{data_on_leafs[2]}, {data_on_leafs[0]}, element_not_in_tree};
     for (size_t i = 0; i < data_to_check.size(); ++i) {
         std::cout << "Is leaf " << data_to_check[i][0] << " was in tree in position 0: ";
@@ -52,10 +53,10 @@ int main() {
     std::array<char, 7> left = {'\x6d', '\x65', '\x73', '\x73', '\x61', '\x67', '\x65'};
     std::array<char, 7> right = {'\x20', '\x64', '\x69', '\x67', '\x65', '\x73', '\x74'};
     std::vector<std::array<char, 7> > simple_binary_tree_data = {left, right};
-    merkle_tree<hashes::blake2b<224>, 2> simple_binary_tree(simple_binary_tree_data);
+    merkle_tree<hashes::blake2b<224>, 2> simple_binary_tree = make_merkle_tree<hashes::blake2b<224>, 2>(simple_binary_tree_data.begin(), simple_binary_tree_data.end());
     merkle_proof<hashes::blake2b<224>, 2> simple_binary_proof_leaf_1(simple_binary_tree, 1);
-    std::cout << "Tree simple binary structure:" << std::endl;
-    std::cout << simple_binary_tree << std::endl;
+//    std::cout << "Tree simple binary structure:" << std::endl;
+//    std::cout << simple_binary_tree << std::endl;
     std::cout << "Is leaf " << data_on_leafs[1][0] << " was in tree in position 1: ";
     std::cout << std::boolalpha << simple_binary_proof_leaf_1.validate(data_on_leafs[1]) << std::endl;
     std::cout << "Is leaf left was in tree in position 1: ";
