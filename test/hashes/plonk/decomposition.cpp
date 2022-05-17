29a2d4abc9e427e4bb2d1d0bd20483e9d356a53d
Kate <kate@nil.foundation>
2022-05-17 23:20:33 +0300

    added decomposition test

---
+++ test/hashes/plonk/decomposition.cpp
@@ -0,0 +1,83 @@
+//---------------------------------------------------------------------------//
+// Copyright (c) 2022 Ekaterina Chukavina <kate@nil.foundation>
+//
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//---------------------------------------------------------------------------//
+
+#define BOOST_TEST_MODULE plonk_decomposition_test
+#include <fstream>
+#include <boost/test/unit_test.hpp>
+
+#include <nil/crypto3/algebra/curves/pallas.hpp>
+#include <nil/crypto3/algebra/fields/arithmetic_params/pallas.hpp>
+#include <nil/crypto3/algebra/random_element.hpp>
+
+#include <nil/crypto3/hash/algorithm/hash.hpp>
+#include <nil/crypto3/hash/sha2.hpp>
+#include <nil/crypto3/hash/keccak.hpp>
+
+#include <nil/crypto3/zk/snark/arithmetization/plonk/params.hpp>
+
+#include <nil/crypto3/zk/blueprint/plonk.hpp>
+#include <nil/crypto3/zk/assignment/plonk.hpp>
+
+#include <nil/crypto3/zk/components/hashes/sha256/plonk/decomposition.hpp>
+
+#include "test_plonk_component.hpp"
+
+using namespace nil::crypto3;
+
+BOOST_AUTO_TEST_SUITE(blueprint_plonk_test_suite)
+
+BOOST_AUTO_TEST_CASE(blueprint_plonk_decomposition) {
+
+    using curve_type = algebra::curves::pallas;
+    using BlueprintFieldType = typename curve_type::scalar_field_type;
+
+    constexpr std::size_t WitnessColumns = 9;
+    constexpr std::size_t PublicInputColumns = 1;
+    constexpr std::size_t ConstantColumns = 0;
+    constexpr std::size_t SelectorColumns = 1;
+    using hash_type = nil::crypto3::hashes::keccak_1600<256>;
+    constexpr std::size_t Lambda = 40;
+
+    using ArithmetizationParams =
+        zk::snark::plonk_arithmetization_params<WitnessColumns, PublicInputColumns, ConstantColumns, SelectorColumns>;
+    using ArithmetizationType = zk::snark::plonk_constraint_system<BlueprintFieldType, ArithmetizationParams>;
+    using var = zk::snark::plonk_variable<BlueprintFieldType>;
+
+    using AssignmentType = zk::blueprint_assignment_table<ArithmetizationType>;
+    using component_type = zk::components::decomposition<ArithmetizationType, curve_type, 0, 1, 2, 3, 4, 5, 6, 7, 8>;
+
+    std::vector<typename BlueprintFieldType::value_type> public_input = {0x8d741211e928fdd4d33a13970d0ce7f3_cppui255,
+                                                                         0x92f209334030f9ec8fa8a025e987a5dd_cppui255};
+    std::array<var, 2> input_state_var = {var(0, 0, false, var::column_type::public_input),
+                                          var(0, 1, false, var::column_type::public_input)};
+
+    typename component_type::params_type params = {input_state_var};
+
+    auto result_check = [](AssignmentType &assignment, component_type::result_type &real_res) {};
+
+    test_component<component_type, BlueprintFieldType, ArithmetizationParams, hash_type, Lambda>(
+        params, public_input, result_check);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
