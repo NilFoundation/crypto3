f3a30c3ada7ddce419c8b92e5e380539196f52cf
Alice <agreeru@mail.ru>
2022-06-24 12:55:15 +0300

    prover debug output

--- include/nil/crypto3/zk/snark/systems/plonk/placeholder/prover.hpp
+++ include/nil/crypto3/zk/snark/systems/plonk/placeholder/prover.hpp
@@ -239,7 +239,7 @@ namespace nil {
                                 constraint_system, polynomial_table, preprocessed_public_data.common_data.basic_domain, transcript)[0];
 
                         /////TEST
-                        /*for (std::size_t i = 0; i < f_parts; i++) {
+                        for (std::size_t i = 0; i < f_parts; i++) {
                             for (std::size_t j = 0; j < table_description.rows_amount; j++) {
                                 if (F[i].evaluate(preprocessed_public_data.common_data.basic_domain->get_domain_element(j)) != FieldType::value_type::zero()) {
                                     std::cout<<"F["<<i<<"] != 0 at j = "<<j<<std::endl;
@@ -251,13 +251,15 @@ namespace nil {
 
                         for (std::size_t i = 0; i < gates.size(); i++) {
                             for (std::size_t j = 0; j < gates[i].constraints.size(); j++) {
-                                math::polynomial<typename FieldType::value_type> constraint_result =
-                                    gates[i].constraints[j].evaluate(polynomial_table, preprocessed_public_data.common_data.basic_domain);
-                                if (constraint_result.evaluate(preprocessed_public_data.common_data.basic_domain->get_domain_element(0)) != FieldType::value_type::zero()) {
-                                    std::cout<<"constraint "<<j<<" from gate "<<i<<std::endl;
-                                }
+                                math::polynomial_dfs<typename FieldType::value_type> constraint_result =
+                                    gates[i].constraints[j].evaluate(polynomial_table, preprocessed_public_data.common_data.basic_domain)*polynomial_table.selector(gates[i].selector_index);
+                                //for (std::size_t k = 0; k < table_description.rows_amount; k++) {
+                                    if (constraint_result.evaluate(preprocessed_public_data.common_data.basic_domain->get_domain_element(253)) != FieldType::value_type::zero()) {
+                                        std::cout<<"constraint "<<j<<" from gate "<<i<<"on row "<<std::endl; 
+                                    }
+                                //}
                             }
-                        }*/
+                        }
 #ifdef ZK_PLACEHOLDER_PROFILING_ENABLED
                         last = std::chrono::high_resolution_clock::now();
 #endif
