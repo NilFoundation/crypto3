3d7f1a385e0235e5cd7ceabc450c66333c6d4412
Rinat <rinat228qwertyuiop12345@gmail.com>
2022-06-28 16:22:14 +0300

    added some tests for reinforced concrete

--- test/rc_concrete.cpp
+++ test/rc_concrete.cpp
@@ -68,8 +68,22 @@ BOOST_AUTO_TEST_CASE(check_inversity_of_compose_decompose_for_0){
 }
 
 BOOST_AUTO_TEST_CASE(check_inversity_of_compose_decompose_for_1){
-    element_type zero = element_type(integral_type(1));
-    operators::bucket_type after_decompose = operators::decompose(zero);
-    element_type zero_after = operators::compose(after_decompose);
-    BOOST_CHECK_EQUAL(zero, zero_after);
+    element_type one = element_type(integral_type(1));
+    operators::bucket_type after_decompose = operators::decompose(one);
+    element_type one_after = operators::compose(after_decompose);
+    BOOST_CHECK_EQUAL(one, one_after);
+}
+
+BOOST_AUTO_TEST_CASE(bricks_for_bls12fr381){
+    operators::state_vector_type state = {{2, 2, 2}};
+
+    operators::bricks(state);
+    BOOST_CHECK_EQUAL(state[0], element_type(integral_type(32)));
+    BOOST_CHECK_EQUAL(state[1], element_type(integral_type(16)));
+    BOOST_CHECK_EQUAL(state[2], element_type(integral_type(28)));
+}
+
+BOOST_AUTO_TEST_CASE(permute){
+    state_type temp_state = {element_type(integral_type(0)), element_type(integral_type(0)), element_type(integral_type(0))};
+    rc_functions_t::permute(temp_state);
 }
