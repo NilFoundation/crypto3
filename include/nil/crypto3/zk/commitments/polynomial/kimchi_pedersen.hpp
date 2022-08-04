390bdac6e2a87ec1cc9cf9a8ecf06f4c7a1f8eb1
Rinat <rinat228qwertyuiop12345@gmail.com>
2022-08-04 15:27:20 +0300

    kimchi commitment: bug in multiexp #27

--- include/nil/crypto3/zk/commitments/polynomial/kimchi_pedersen.hpp
+++ include/nil/crypto3/zk/commitments/polynomial/kimchi_pedersen.hpp
@@ -566,81 +566,15 @@ namespace nil {
                         rand_base_i *= rand_base;
                         sg_rand_base_i *= sg_rand_base;
 
-                        for(auto &a : points){
-                            std::cout << "point " << std::hex << a.X.data << ' ' << std::hex << a.Y.data << '\n';
-                        }
+                        // for(auto &a : points){
+                        //     std::cout << "point " << std::hex << a.X.data << ' ' << std::hex << a.Y.data << '\n';
+                        // }
                         
-                        for(auto &a : scalars){
-                            std::cout << "scalar " << std::hex << a.data << '\n';
-                        }
+                        // for(auto &a : scalars){
+                        //     std::cout << "scalar " << std::hex << a.data << '\n';
+                        // }
                         // std::cout << "size " << scalars.size() << '\n';
 
-
-                        // std::vector<typename group_type::value_type> my_points = {
-                        //     {0x092060386301c999aab4f263757836369ca27975e28bc7a8e5b2ce5b26262201_cppui256, 0x314fc4d83ae66a509f9d41be6165f2606a209a9b5805ee85ce20249c5ebcbe26_cppui256},
-                        //     {0x121c4426885fd5a9701385aaf8d43e52e7660f1fc5afc5f6468cc55312fc60f8_cppui256, 0x21b439c01247ea3518c5ddeb324e4cb108af617780ddf766d96d3fd8ab028b70_cppui256},
-                        //     {0x26c9349ff7fb4ab230a6f6aef045f451fbbe9b37c43c3274e2aa4b82d131fd26_cppui256, 0x1996274d67ec0464c51f79ccfa1f511c2aabb666abe67733ee8185b71b27a504_cppui256},
-                        //     {0x26985f27306586711466c5b2c28754aa62fe33516d75cef1f7751f1a169713fd_cppui256, 0x2e8930092fe6a18b331ce0e6e27b413aa18e76394f18a2835da9fae10aa3229d_cppui256},
-                        //     {0x014b2db7b753a74d454061fcb3ac537e1b4ba512f9ed258c996a59d9dacd13e5_cppui256, 0x06f392d371494fc39174c4b70c692b96f3b7c42da288f6b7aabf463334a952d0_cppui256},
-                        //     {0x12ca0e2dbf286021cb76b7c12b6c9ad7fdf1d05f722f6ef14bd43e53e7b92120_cppui256, 0x216a80b79d3995d1f39ce19855c475052d1148acbdd379fe98961bfbd0a3e428_cppui256},
-                        //     {0x1d257c1f4ec9872c9e06549bc910f7b7196f2e7cb120aec3fdceb049c7a0c9a5_cppui256, 0x191cbec20ed5ea342b6b395e92996215f7d93c675da56a13d548efb58524d336_cppui256},
-                        //     {0x06236026ed7dc19c44540fbaf0c1c3498f82880a34422547fff519fff744bb48_cppui256, 0x3a02c5410dabde160bd09232a14f00b1ef6cd4d6285c90a8d41fa00bff922f0a_cppui256},
-                        //     {0x079333fde60d3f670068b5a1d486eddd87ddf91d1e1fc000f387991b4ed848b4_cppui256, 0x3f7fc1a39fd74bdedc129195080d298cfc2c2cf714bad9f9334f0dafb035c200_cppui256},
-                        //     {0x069b398c2968553b7987ff840cf0b71359d10f249f08c40898550a63f196d856_cppui256, 0x1b68bb879d6ec4efaa2207e212b59bad0d8e5e2493f99be3f2f24764046cd277_cppui256},
-                        //     {0x2cbd65973ae0be0b9e652cec35efe509e1fa8dd8349dc1e644db494dc2b4fd75_cppui256, 0x1e27b8178e720407694f4ea1413b0cb87af4058cb308bbd68ff42d5078de243e_cppui256},
-                        //     {0x0f29a22ef6949de85427f72ccd04e3f8f56837bb56da17d8fa5de9025e6b9ed5_cppui256, 0x26a2cd91bd2771e20decaacdc6ca96e7759668f3d0b7e8810866d27737627a59_cppui256},
-                        //     typename group_type::value_type(),
-                        //     typename group_type::value_type(),
-                        //     typename group_type::value_type(),
-                        //     typename group_type::value_type(),
-                        //     typename group_type::value_type(),
-                        //     {0x183e89a07fae7189d90bceced87af3a3666ed065d49910212098fa3e3e973d36_cppui256, 0x315eb7367d9d9e0e4e1077918da61500cd2ef0405d63b7a4e18d05ce95d0c95b_cppui256},
-                        //     {0x1b6cac7f49cdf1e2ff16114d3b56746f400ea818a1a3b601b1bea800e4c143b9_cppui256, 0x098c419958c9f484574fde569455be6a1b2489bfcced4f19cdbdf1587286aac7_cppui256},
-                        //     {0x2cbcf92e54f2ad975359f6390a562dd79c52847af729c16c4a1d55234be23f72_cppui256, 0x2b0fb10e330db2df2e65e899ad165b46ca9bd0f25bf86ed2145bab55524826de_cppui256},
-                        //     {0x0de54af192b1e666bf432b369256f28fa7090d320c86e6804a434c07d19ace0f_cppui256, 0x016a01b519f302a1c44cd54372c73bc1d25bd90db7cda7e4e01aed13e7cf26f9_cppui256},
-                        //     {0x0b884baaeaf73060519f5d8b8539a1567333f1df41da6ee06092487174bd9fcb_cppui256, 0x2bef27d89d61a901c97a85bf9dacfa1f5042b8a96fe1adca194a96ade865e745_cppui256},
-                        //     {0x2277a0752d91a0d3917f74f00c0bb91ad4621353da74331a6103838cf386567c_cppui256, 0x2e88cca90ecf1dce78e089eab2c42398df49f61d6762c3edaa1e27861ace0bd8_cppui256},
-                        //     {0x038afa6f58bd597a9b848d71239dfd5db8768302e5c8567e583af19e52821ad2_cppui256, 0x17b1ac3ec136fdf18fb47a079b32bb41935cd7e7327e40e849c754824d9d5ad6_cppui256},
-                        //     {0x330b8e2245ddf677aa75bfe3eb513158ab9bcad4865992139942f41c99c78cf7_cppui256, 0x1d601026f5a2812cdd7e92b99fb5c9c5e4afb84e1b35dfb5695c11a61cc9d7a5_cppui256},
-                        //     {0x0a33f7b5e3e0be1f2478c0829a533b698abfc03c7b1377bc4d5c95d7378aed17_cppui256, 0x38537bab228e76478e4357cccbcb1977b041d68a637b3b90e88c0d4bc987154d_cppui256},
-                        //     {0x063120f148e4c430a4a7e950b02129ebbca7e2b1e4511598b9c705dff3af7893_cppui256, 0x175502fd4397a1a9a6c22384cb6a9ab73a51d82dfeaf82f97d6bf3b80337d84a_cppui256},
-                        //     {0x3a445ac8ca34ae5c12e66c3e9d3cc5df572f109629e9eafb9e7de451302aceb2_cppui256, 0x0280e113c668a50ec5fbc7654a8417176f614cd6e1658edaa43034ada563c9d2_cppui256},
-                        //     {0x1b6cac7f49cdf1e2ff16114d3b56746f400ea818a1a3b601b1bea800e4c143b9_cppui256, 0x098c419958c9f484574fde569455be6a1b2489bfcced4f19cdbdf1587286aac7_cppui256},
-                        //     {0x14e1f73486f2f8deb46b82b291b0d986fa08ef24cb60925813610d996122aa01_cppui256, 0x12fe65849363bd69bf69236dce9d7227c91083bc7876876c26f89fdc889154a1_cppui256},
-                        // };
-
-                        // std::vector<typename scalar_field_type::value_type> my_scalars = {
-                        //     0x0502C4136D13D43292AB621B900693AE14AFAC97E6D44589EB18921420D270DD_cppui256,
-                        //     0x0000000000000000000000000000000000000000000000000000000000000001_cppui256,
-                        //     0x3EF2CFF891B858662ED2F48B539F99450C83383A625A2E3B6AA92F93280E1F54_cppui256,
-                        //     0x33E62A38C5E158DAAAD7F039CD637376FA9434AF4479C0364F5F2F58383C41BF_cppui256,
-                        //     0x276E49C171A4DFEF98815D27CD4756F531E5F0BD16F256FBC8D9145BA68A3CA6_cppui256,
-                        //     0x1E4CB212111CD12F848E6C9FF6450881EB57EB2E766250A56F502A907A7716D6_cppui256,
-                        //     0x14C6956DE17DF51614271610A29E61F76D086680FCEBFF14CBCD6097AC4C568E_cppui256,
-                        //     0x2DBF59B2EFE92056DBB18199DC694B323BD180EB3C89B1AAC6D19A64B1A561B1_cppui256,
-                        //     0x2754558056A6B6C4AB113122DE3A79DA6AAAE253307927EB87AEBFA8DADEE5EE_cppui256,
-                        //     0x1485FB3E58C620F64BC4EBA19AAF8FA75BD58E80747C2C76D32EE2EE00D939B7_cppui256,
-                        //     0x24F71A5034441349590FD1C144D1E13267A350B77DE3E80B211E68E07EF3FFA0_cppui256,
-                        //     0x0E840876CB441EED0E2A4AEC4D1BED1A5C803FAC4D328A04C7983186845FD399_cppui256,
-                        //     0x1503716950CCEDDB07FF2B0C3E557425CA563F3634D14649B5F8521598B93C95_cppui256,
-                        //     0x2E2B87F3DF83EE97D07BCCE513B0B725160C6DD9D25639D9D6FF0A6A80D79414_cppui256,
-                        //     0x37CD916F0E2348C8020A05FCF624A51149A2C7CE18C3B7DF79F0605A94B300D0_cppui256,
-                        //     0x3AB20B4B6A8F0242D0ED74C514A2F6E9A3F2C9F941EA1E8C980136326E347820_cppui256,
-                        //     0x1BD5E1E34EA9D4BE74B413BF0E9C40C2F45588FCDE539DE9925644CFA64ABD20_cppui256,
-                        //     0x39B4F24092436C22519D92277005A423539CCAF3B328915E8C4D2C44012DC28B_cppui256,
-                        //     0x1560807DE44BA19A4C0B926EAD5C5E932D8012ABF94A79C38A134CDCFD3CEFA6_cppui256,
-                        //     0x0FBE30CE70451C441F4BD047DE1530989CC1B219FC4CBEACD067E4674DF0AD58_cppui256,
-                        //     0x17338D95FD83B16ED84F4530CF5C1ECBCB136F5F2CAF3C7E469E7E2EB6BCF4C2_cppui256,
-                        //     0x31754C973F04AF9AF6964C90351DFFD830412CB68715BFBC62C6F5273B2BA814_cppui256,
-                        //     0x3A7A1D96F55359E7D63699C689490352C7699C571542A05D45DCCA95C1FB54C6_cppui256,
-                        //     0x367B5BA1C4DA2C737CAC0B098A5005BD2EEBA87340D6B4BCFA187D14D128D862_cppui256,
-                        //     0x2165872693C67A330AA022DB8ADDCAFC8216AC0EEAC690F33457F2C0A97048D6_cppui256,
-                        //     0x1DD69A916CA3B5A6EB83E9542FDFEE1E61C22C82333A61B7B0D3F730B92C3BF6_cppui256,
-                        //     0x0ECE005AD0DF893B34FCCD582C20586A30C7D13C4351AF3AD05356FE721C9414_cppui256,
-                        //     0x12906C0C1045D7DE2B3AE0532F18F1DD4398F95A569FA557690758D822B2DAF5_cppui256,
-                        //     0x339E425C16C09EB979AF6CE9A515AE30F29E324754B1AC5194F7634814DAF207_cppui256,
-                        //     0x0000000000000000000000000000000000000000000000000000000000000001_cppui256,
-                        // };
                         auto res_point = algebra::multiexp_with_mixed_addition<multiexp_method>(
                                     my_points.begin(), my_points.end(), my_scalars.begin(), my_scalars.end(), 1);
                         
