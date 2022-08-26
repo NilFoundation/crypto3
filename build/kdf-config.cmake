8f598d1eefa07a00a26ff83a17d7925de6d0c2fc
hgedia <haresh.gedia@gmail.com>
2022-08-26 13:09:36 +0100

    Adding missing defs/policies

---
+++ build/kdf-config.cmake
@@ -0,0 +1,11 @@
+
+include(CMakeFindDependencyMacro)
+# Skip Library: crypto3::block
+
+# Skip Library: crypto3::hash
+
+# Skip Library: crypto3::mac
+
+
+include("${CMAKE_CURRENT_LIST_DIR}/crypto3-kdf-targets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/properties-crypto3-kdf-targets.cmake")
---
+++ build/properties-crypto3-kdf-targets.cmake
@@ -0,0 +1,2 @@
+
+set_target_properties(crypto3::kdf PROPERTIES INTERFACE_PKG_CONFIG_NAME kdf)
