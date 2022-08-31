3249fd9ac1571b9eb4832e9387448d6595c648f1
nkaskov <mail.nikita.kaskov@gmail.com>
2022-08-31 14:17:34 +0300

    PLONK component base class updated Constant and Public input columns indices added.

--- include/nil/crypto3/zk/component.hpp
+++ include/nil/crypto3/zk/component.hpp
@@ -29,6 +29,8 @@
 #include <nil/crypto3/zk/snark/arithmetization/plonk/constraint_system.hpp>
 #include <nil/crypto3/zk/snark/arithmetization/constraint_satisfaction_problems/r1cs.hpp>
 
+#include <nil/crypto3/zk/detail/get_component_id.hpp>
+
 namespace nil {
     namespace crypto3 {
         namespace zk {
@@ -41,20 +43,51 @@ namespace nil {
                 template<typename ArithmetizationType, std::uint32_t... ComponentTemplateParams>
                 class component;
 
-                template<typename BlueprintFieldType, typename ArithmetizationParams, std::uint32_t WitnessAmount>
-                class component<snark::plonk_constraint_system<BlueprintFieldType, ArithmetizationParams>, WitnessAmount> {
+                template<typename BlueprintFieldType, typename ArithmetizationParams, std::uint32_t WitnessAmount,
+                    ConstantAmount, PublicInputAmount>
+                class component<snark::plonk_constraint_system<BlueprintFieldType, ArithmetizationParams>, WitnessAmount,
+                    ConstantAmount, PublicInputAmount> {
                 protected:
 
                     using witness_container_type = std::array<std::uint32_t, WitnessAmount>;
-                    witness_container_type _W;
+                    using constant_container_type = std::array<std::uint32_t, ConstantAmount>;
+                    using public_input_container_type = std::array<std::uint32_t, PublicInputAmount>;
+                    
+                private:
 
-                    std::uint32_t _gates_amount;
+                    witness_container_type _W;
+                    constant_container_type _C;
+                    public_input_container_type _PI;
+
                     std::uint32_t _rows_amount;
 
                     using var = snark::plonk_variable<BlueprintFieldType>;
 
-                    typename witness_container_type::value_type W(std::uint32_t index) const {
-                        return _W[index];
+                    /**
+                     * Get Witness column global index by its internal index.
+                     * 
+                     * @param[in] internal witness signed index. For -1, last witness assumed.
+                     */
+                    typename witness_container_type::value_type W(std::int32_t index) const {
+                        return _W[(WitnessAmount + index)%WitnessAmount];
+                    }
+
+                    /**
+                     * Get Constant column global index by its internal index.
+                     * 
+                     * @param[in] internal constant signed index. For -1, last constant assumed.
+                     */
+                    typename constant_container_type::value_type C(std::int32_t index) const {
+                        return _C[(ConstantAmount + index)%ConstantAmount];
+                    }
+
+                    /**
+                     * Get Public Input column global index by its internal index.
+                     * 
+                     * @param[in] internal public input signed index. For -1, last public input assumed.
+                     */
+                    typename public_input_container_type::value_type PI(std::int32_t index) const {
+                        return _PI[(PublicInputAmount + index)%PublicInputAmount];
                     }
 
                 public:
@@ -62,19 +95,44 @@ namespace nil {
                     typedef snark::plonk_constraint_system<BlueprintFieldType, ArithmetizationParams>
                         ArithmetizationType;
 
-                    template <typename ContainerType>
-                    component(ContainerType wires, std::uint32_t rows_amount, std::uint32_t gates_amount) :
-                        _rows_amount(rows_amount), _gates_amount(gates_amount) {
-                        std::copy_n(std::make_move_iterator(wires.begin()), WitnessAmount, _W.begin());
+                    /**
+                     * Constructor from arbitrary container types.
+                     * 
+                     * @tparam WitnessContainerType Input Witness Container Type
+                     * @tparam ConstantContainerType Input Constant Container Type
+                     * @tparam PublicInputContainerType Input PublicInput Container Type
+                     * @param[in] witness Container with witness columns global indices.
+                     * @param[in] constant Container with constant columns global indices.
+                     * @param[in] public_input Container with public input columns global indices.
+                     */
+                    template <typename WitnessContainerType, typename ConstantContainerType,
+                        typename PublicInputContainerType>
+                    component(WitnessContainerType witness, ConstantContainerType constant,
+                            PublicInputContainerType public_input, std::uint32_t rows_amount = 0) :
+                        _rows_amount(rows_amount) {
+                        std::copy_n(std::make_move_iterator(witness.begin()), WitnessAmount, _W.begin());
+                        std::copy_n(std::make_move_iterator(constant.begin()), ConstantAmount, _C.begin());
+                        std::copy_n(std::make_move_iterator(public_input.begin()), PublicInputAmount, _PI.begin());
                     }
 
                     std::uint32_t rows_amount() const {
                         return _rows_amount;
                     }
 
-                    std::uint32_t gates_amount() const {
-                        return _gates_amount;
+                    std::size_t witness_amount() const {
+                        return _W.size();
                     }
+
+                    std::size_t constant_amount() const {
+                        return _C.size();
+                    }
+
+                    std::size_t public_input_amount() const {
+                        return _PI.size();
+                    }
+
+                    template <typename ComponentType>
+                    friend zk::detail::blueprint_component_id_type zk::detail::get_component_id (ComponentType component);
                 };
 
                 template<typename BlueprintFieldType>
