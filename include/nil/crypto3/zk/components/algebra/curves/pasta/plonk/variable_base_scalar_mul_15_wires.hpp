c20b5922e38912ba293f3db018999a15df3c14f2
Alice <agreeru@mail.ru>
2022-09-30 01:06:10 +0300

    variable_base_mul component: update selector column

--- include/nil/crypto3/zk/components/algebra/curves/pasta/plonk/variable_base_scalar_mul_15_wires.hpp
+++ include/nil/crypto3/zk/components/algebra/curves/pasta/plonk/variable_base_scalar_mul_15_wires.hpp
@@ -230,7 +230,7 @@ namespace nil {
                         }
 
                         assignment.enable_selector(first_selector_index, start_row_index + add_component::rows_amount,
-                                                   start_row_index + rows_amount - 3, 2);
+                                                   start_row_index + rows_amount - 4, 2);
                         assignment.enable_selector(first_selector_index + 1, start_row_index + rows_amount - 2);
 
                         typename add_component::params_type addition_params = {{params.T.x, params.T.y},
