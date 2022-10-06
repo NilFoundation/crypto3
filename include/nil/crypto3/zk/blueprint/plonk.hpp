5bc0985de12ac197bc36a96b4b4900060a67885c
Zerg1996 <Zerg1996@yandex.ru>
2022-10-06 16:05:57 +0200

    Some more constructor and methods #69

--- include/nil/crypto3/zk/blueprint/plonk.hpp
+++ include/nil/crypto3/zk/blueprint/plonk.hpp
@@ -57,6 +57,10 @@ namespace nil {
             public:
                 typedef BlueprintFieldType blueprint_field_type;
 
+                blueprint(snark::plonk_constraint_system<BlueprintFieldType,
+                        ArithmetizationParams> arithmetization_type_in, snark::plonk_table_description<BlueprintFieldType, ArithmetizationParams> &table_description) :
+                        ArithmetizationType(arithmetization_type_in), _table_description(table_description) { }
+
                 blueprint(snark::plonk_table_description<BlueprintFieldType, ArithmetizationParams> &table_description) :
                     ArithmetizationType(), _table_description(table_description) {
                     _table_description.rows_amount = 0;
