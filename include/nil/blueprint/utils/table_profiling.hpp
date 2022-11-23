5fbb2bd34fe981996746fc723610fdd6cf7dad8c
Elena Tatuzova <ilerna@gmail.com>
2022-11-23 22:36:39 +0400

    Update assignment table profiling #90

--- include/nil/blueprint/utils/table_profiling.hpp
+++ include/nil/blueprint/utils/table_profiling.hpp
@@ -36,42 +36,73 @@
 namespace nil {
     namespace blueprint {
         template<typename ValueType>
-        void profiling(std::vector<ValueType> column) {
+        void profiling(std::vector<ValueType> column, std::ostream &out = std::cout) {
             for (std::size_t index = 0; index < column.size(); index++) {
-                std::cout << "\t" << column[index].data;
+                out << "\t" << column[index].data;
             }
         }
 
         template<typename FieldType, typename ArithmetizationParams>
-        void profiling(crypto3::zk::snark::plonk_assignment_table<FieldType, ArithmetizationParams> assignments) {
+        void profiling(crypto3::zk::snark::plonk_assignment_table<FieldType, ArithmetizationParams> assignments, std::ostream &out = std::cout) {
 
             for (std::size_t row_index = 0; row_index < assignments.rows_amount(); row_index++) {
-                std::cout << "\t" << row_index;
+                out << "\t" << row_index;
             }
-            std::cout << std::endl;
+            out << std::endl;
 
             for (std::size_t w_index = 0; w_index < ArithmetizationParams::witness_columns; w_index++) {
-                std::cout << "W" << w_index << ":";
-                profiling(assignments.witness(w_index));
-                std::cout << std::endl;
+                out << "W" << w_index << ":";
+                profiling(assignments.witness(w_index), out);
+                out << std::endl;
             }
 
             for (std::size_t pi_index = 0; pi_index < ArithmetizationParams::public_input_columns; pi_index++) {
                 std::cout << "PI" << pi_index << ":";
-                profiling(assignments.public_input(pi_index));
-                std::cout << std::endl;
+                profiling(assignments.public_input(pi_index), out);
+                out << std::endl;
             }
 
             for (std::size_t c_index = 0; c_index < ArithmetizationParams::constant_columns; c_index++) {
-                std::cout << "C" << c_index << ":";
-                profiling(assignments.constant(c_index));
-                std::cout << std::endl;
+                out << "C" << c_index << ":";
+                profiling(assignments.constant(c_index), out);
+                out << std::endl;
             }
 
             for (std::size_t s_index = 0; s_index < ArithmetizationParams::selector_columns; s_index++) {
-                std::cout << "S" << s_index << ":";
-                profiling(assignments.selector(s_index));
-                std::cout << std::endl;
+                out << "S" << s_index << ":";
+                profiling(assignments.selector(s_index), out);
+                out << std::endl;
+            }
+        }
+
+
+        template<typename FieldType, typename ArithmetizationParams>
+        void profiling_assignment_table(
+            crypto3::zk::snark::plonk_assignment_table<FieldType, ArithmetizationParams> assignments, 
+            std::size_t usable_rows, 
+            std::ostream &out = std::cout
+        ) {
+            out << usable_rows << std::endl;
+            out << assignments.rows_amount() << std::endl;
+
+            for (std::size_t w_index = 0; w_index < ArithmetizationParams::witness_columns; w_index++) {
+                profiling(assignments.witness(w_index), out);
+                out << std::endl;
+            }
+
+            for (std::size_t pi_index = 0; pi_index < ArithmetizationParams::public_input_columns; pi_index++) {
+                profiling(assignments.public_input(pi_index), out);
+                out << std::endl;
+            }
+
+            for (std::size_t c_index = 0; c_index < ArithmetizationParams::constant_columns; c_index++) {
+                profiling(assignments.constant(c_index), out);
+                out << std::endl;
+            }
+
+            for (std::size_t s_index = 0; s_index < ArithmetizationParams::selector_columns; s_index++) {
+                profiling(assignments.selector(s_index), out);
+                out << std::endl;
             }
         }
     }    // namespace blueprint
