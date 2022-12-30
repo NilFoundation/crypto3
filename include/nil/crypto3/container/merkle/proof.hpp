e72c14db1fed8f1abb95f51da0c6db84276d3495
Polina <chernyshova.pa@phystech.edu>
2022-12-30 12:52:11 +0000

    friend functions for compressed proofs #107

--- include/nil/crypto3/container/merkle/proof.hpp
+++ include/nil/crypto3/container/merkle/proof.hpp
@@ -107,6 +107,7 @@ namespace nil {
                         std::size_t row_len = tree.leaves();
                         std::size_t row_begin_idx = 0;
                         while (cur_leaf != tree.size() - 1) {    // while it's not _root
+                            std::cout << "cur_leaf: " << cur_leaf << std::endl;
                             std::size_t cur_leaf_pos = cur_leaf % arity;
                             std::size_t cur_leaf_arity_pos = (cur_leaf - row_begin_idx) / arity;
                             std::size_t begin_this_arity = cur_leaf - cur_leaf_pos;
@@ -142,6 +143,13 @@ namespace nil {
                         return (d == _root);
                     }
 
+                    friend std::vector<merkle_proof_impl<NodeType, Arity>> 
+                        generate_compressed_proofs(const merkle_tree<hash_type, arity> &tree, 
+                                                    const std::vector<std::size_t> leaf_idxs);
+
+                    template<typename Hashable>
+                    friend bool validate_compressed_proofs(const std::vector<Hashable> &a_vec);
+
                     std::size_t leaf_index() const {
                         return _li;
                     }
@@ -173,12 +181,86 @@ namespace nil {
                     friend class nil::crypto3::marshalling::types::merkle_proof_marshalling;
                 };
             }    // namespace detail
+            
+            template<typename NodeType, std::size_t Arity>
+            std::vector<detail::merkle_proof_impl<NodeType, Arity>> 
+                generate_compressed_proofs(const containers::merkle_tree<typename NodeType::hash_type, Arity> &tree, 
+                                            const std::vector<std::size_t> leaf_idxs) {
+                typedef typename detail::merkle_proof_impl<NodeType, Arity>::path_element_type path_element_type;
+                typedef std::array<path_element_type, Arity - 1> layer_type;
+                std::vector<detail::merkle_proof_impl<NodeType, Arity>> proofs;
+                proofs.reserve(leaf_idxs.size());
+                std::size_t row_len = tree.leaves();
+                std::vector<bool> known(2 * row_len, false);
+                for (std::size_t i = 0; i < row_len; ++i) {
+                    known[i] = true;   // leaves are known
+                }
+                for (auto leaf_idx : leaf_idxs) {
+                    auto proof = detail::merkle_proof_impl<NodeType, Arity>(leaf_idx, tree.root());
+                    proof._path.resize(tree.row_count() - 1);
+
+                    typename std::vector<layer_type>::iterator v_itr = proof._path.begin();
+                    std::size_t cur_leaf = leaf_idx;
+                    std::size_t row_len = tree.leaves();
+                    std::size_t row_begin_idx = 0;
+                    while (cur_leaf != tree.size() - 1) {    // while it's not _root
+                        std::cout << "cur_leaf: " << cur_leaf << std::endl;
+                        std::size_t cur_leaf_pos = cur_leaf % Arity;
+                        std::size_t cur_leaf_arity_pos = (cur_leaf - row_begin_idx) / Arity;
+                        std::size_t begin_this_arity = cur_leaf - cur_leaf_pos;
+                        typename layer_type::iterator a_itr = v_itr->begin();
+                        for (size_t i = 0; i < cur_leaf_pos; ++i, ++begin_this_arity, ++a_itr) {
+                            if (known[cur_leaf]) {
+                                --a_itr;
+                            } else {
+                                *a_itr = path_element_type(tree[begin_this_arity], i);
+                                known[cur_leaf] = true;
+                            }
+                        }
+                        for (size_t i = cur_leaf_pos + 1; i < Arity; ++i, ++begin_this_arity, ++a_itr) {
+                            if (known[cur_leaf + 1]) {
+                                --a_itr;
+                            } else {
+                                *a_itr = path_element_type(tree[begin_this_arity + 1], i);
+                                known[cur_leaf + 1] = true;
+                            }
+                        }
+                        v_itr++;
+                        cur_leaf = row_len + row_begin_idx + cur_leaf_arity_pos;
+                        row_begin_idx += row_len;
+                        row_len /= Arity;
+                    }
+                    proofs.push_back(proof);
+                }
+                return proofs;
+            }
+
+            template<typename Hashable>
+            bool validate_compressed_proofs(const std::vector<Hashable> &a_vec) {
+                // auto a = a_vec[0];
+                // value_type d = crypto3::hash<hash_type>(a);
+                // for (auto &it : _path) {
+                //     accumulator_set<hash_type> acc;
+                //     size_t i = 0;
+                //     for (; (i < arity - 1) && i == it[i]._position; ++i) {
+                //         crypto3::hash<hash_type>(it[i]._hash.begin(), it[i]._hash.end(), acc);
+                //     }
+                //     crypto3::hash<hash_type>(d.begin(), d.end(), acc);
+                //     for (; i < arity - 1; ++i) {
+                //         crypto3::hash<hash_type>(it[i]._hash.begin(), it[i]._hash.end(), acc);
+                //     }
+                //     d = accumulators::extract::hash<hash_type>(acc);
+                // }
+                // return (d == _root);
+                return true;
+            }
 
             template<typename T, std::size_t Arity>
             using merkle_proof =
                 typename std::conditional<nil::crypto3::detail::is_hash<T>::value,
                                           detail::merkle_proof_impl<detail::merkle_tree_node<T>, Arity>,
                                           detail::merkle_proof_impl<T, Arity>>::type;
+            
         }    // namespace containers
     }        // namespace crypto3
 }    // namespace nil
