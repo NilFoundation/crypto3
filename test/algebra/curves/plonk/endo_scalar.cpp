5a31d3f9f17201f54fc3a37824222c4e625e6cbb
cblpok <aleksandr.cherenkov.3@gmail.com>
2023-01-11 00:43:44 +0300

    update interfaces endo_scalar #98

--- test/algebra/curves/plonk/endo_scalar.cpp
+++ test/algebra/curves/plonk/endo_scalar.cpp
@@ -29,6 +29,8 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include <nil/crypto3/algebra/curves/pallas.hpp>
+#include <nil/crypto3/algebra/fields/arithmetic_params/pallas.hpp>
 #include <nil/crypto3/algebra/curves/vesta.hpp>
 #include <nil/crypto3/algebra/fields/arithmetic_params/vesta.hpp>
 #include <nil/crypto3/algebra/random_element.hpp>
@@ -45,45 +47,56 @@
 
 #include "test_plonk_component.hpp"
 
-using namespace nil::crypto3;
-
-BOOST_AUTO_TEST_SUITE(blueprint_plonk_endo_scalar_test_suite)
-
-BOOST_AUTO_TEST_CASE(blueprint_plonk_unified_addition_addition) {
-
-    using curve_type = algebra::curves::vesta;
-    using BlueprintFieldType = typename curve_type::scalar_field_type;
+template <typename CurveType>
+void test_endo_scalar(std::vector<typename CurveType::scalar_field_type::value_type> public_input,
+    typename CurveType::scalar_field_type::value_type expected_res){
+    using BlueprintFieldType = typename CurveType::scalar_field_type;
     constexpr std::size_t WitnessColumns = 15;
     constexpr std::size_t PublicInputColumns = 1;
     constexpr std::size_t ConstantColumns = 0;
     constexpr std::size_t SelectorColumns = 2;
-    using ArithmetizationParams = zk::snark::plonk_arithmetization_params<WitnessColumns,
+    using ArithmetizationParams = nil::crypto3::zk::snark::plonk_arithmetization_params<WitnessColumns,
         PublicInputColumns, ConstantColumns, SelectorColumns>;
-    using ArithmetizationType = zk::snark::plonk_constraint_system<BlueprintFieldType,
-                ArithmetizationParams>;
-    using AssignmentType = blueprint::assignment<ArithmetizationType>;
-
-    constexpr static const std::size_t num_bits = 128;
-
-    using component_type = zk::components::endo_scalar<ArithmetizationType, curve_type, num_bits,
-                                                            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14>;
-    using hash_type = nil::crypto3::hashes::keccak_1600<256>;
+    using ArithmetizationType = nil::crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType, ArithmetizationParams>;
+    using AssignmentType = nil::blueprint::assignment<ArithmetizationType>;
+	using hash_type = nil::crypto3::hashes::keccak_1600<256>;
     constexpr std::size_t Lambda = 40;
+	constexpr static const std::size_t num_bits = 128;
 
-    using var = zk::snark::plonk_variable<BlueprintFieldType>;
+    using component_type = nil::blueprint::components::endo_scalar<ArithmetizationType, CurveType, num_bits, 15>;
+
+	using var = nil::crypto3::zk::snark::plonk_variable<BlueprintFieldType>;
+
+    var challenge_var(0, 0, false, var::column_type::public_input);
+    typename component_type::input_type instance_input = {challenge_var};
+
+    auto result_check = [&expected_res](AssignmentType &assignment, 
+	    typename component_type::result_type &real_res) {
+            assert(expected_res == var_value(assignment, real_res.output));
+    };
+
+    component_type component_instance({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14},{},{});
+
+    nil::crypto3::test_component<component_type, BlueprintFieldType, ArithmetizationParams, hash_type, Lambda> (component_instance, public_input, result_check, instance_input);
+}
+
+BOOST_AUTO_TEST_SUITE(blueprint_plonk_endo_scalar_test_suite)
+
+BOOST_AUTO_TEST_CASE(blueprint_plonk_unified_addition_addition) {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    using curve_type = nil::crypto3::algebra::curves::vesta;
+    using BlueprintFieldType = typename curve_type::scalar_field_type;
     
     typename BlueprintFieldType::value_type challenge = 0x00000000000000000000000000000000FC93536CAE0C612C18FBE5F6D8E8EEF2_cppui255;
     typename BlueprintFieldType::value_type result = 0x004638173549A4C55A118327904B54E5F6F6314225C8C862F5AFA2506C77AC65_cppui255;
 
-    var challenge_var = {0, 0, false, var::column_type::public_input};
-    typename component_type::params_type params = {challenge_var};
     std::vector<typename BlueprintFieldType::value_type> public_input = {challenge};
-    std::cout<<"Expected result: "<<result.data<<std::endl;
-    auto result_check = [&result](AssignmentType &assignment, 
-        component_type::result_type &real_res) {
-        assert(result == assignment.var_value(real_res.output));
-    };
-    test_component<component_type, BlueprintFieldType, ArithmetizationParams, hash_type, Lambda> (params, public_input, result_check);
+    
+	test_endo_scalar<curve_type>(public_input, result);
+	
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start);
+    std::cout << "endo_scalar: " << duration.count() << "ms" << std::endl;
 }
 
 BOOST_AUTO_TEST_SUITE_END()
