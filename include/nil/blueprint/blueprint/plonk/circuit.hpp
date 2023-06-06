3a856e89c466c2ff0ba5cde2b5fd482f5edf7d84
Iluvmagick <d.tabalin@nil.foundation>
2023-06-06 23:30:29 +0400

    Added assignment and circuit export functions.

--- include/nil/blueprint/blueprint/plonk/circuit.hpp
+++ include/nil/blueprint/blueprint/plonk/circuit.hpp
@@ -94,7 +94,7 @@ namespace nil {
             }
 
             crypto3::zk::snark::plonk_lookup_constraint<BlueprintFieldType>
-                add_lookup_constraint(std::vector<crypto3::math::term<crypto3::zk::snark::plonk_variable<BlueprintFieldType>>> lookup_input, 
+                add_lookup_constraint(std::vector<crypto3::math::term<crypto3::zk::snark::plonk_variable<BlueprintFieldType>>> lookup_input,
                 std::vector<crypto3::zk::snark::plonk_variable<BlueprintFieldType>> lookup_value) {
                 crypto3::zk::snark::plonk_lookup_constraint<BlueprintFieldType> lookup_constraint;
                 lookup_constraint.lookup_input = lookup_input;
@@ -105,6 +105,26 @@ namespace nil {
             void add_lookup_gate(std::size_t selector_index,
                           const std::initializer_list<crypto3::zk::snark::plonk_lookup_constraint<BlueprintFieldType>> &constraints) {
                 this->_lookup_gates.emplace_back(selector_index, constraints);
+            }
+
+            void export_circuit(std::ostream& os) const {
+                std::ios_base::fmtflags os_flags(os.flags());
+                std::size_t gates_size = this->_gates.size(),
+                            copy_constraints_size = this->_copy_constraints.size(),
+                            lookup_gates_size = this->_lookup_gates.size();
+                os << "gates_size: " << gates_size << " "
+                   << "copy_constraints_size: " << copy_constraints_size << " "
+                   << "lookup_gates_size: " << lookup_gates_size << "\n";
+                for (std::size_t i = 0; i < gates_size; i++) {
+                    os << "gate " << i << " selector " << this->_gates[i].selector_index
+                       << " constraints_size " << this->_gates[i].constraints.size() << "\n";
+                    for (std::size_t j = 0; j < this->_gates[i].constraints.size(); j++) {
+                        os << "constraint " << j << " : "
+                           << this->_gates[i].constraints[j] << "\n";
+                    }
+                }
+                os.flush();
+                os.flags(os_flags);
             }
         };
     }    // namespace blueprint
