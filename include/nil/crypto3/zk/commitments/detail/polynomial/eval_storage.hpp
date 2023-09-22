949fa89ce91b526bf9ae565e844b6ba1e83341f3
e.tatuzova <e.tatuzova@nil.foundation>
2023-09-22 13:22:27 +0400

    Minor changes before merge#166

--- include/nil/crypto3/zk/commitments/detail/polynomial/eval_storage.hpp
+++ include/nil/crypto3/zk/commitments/detail/polynomial/eval_storage.hpp
@@ -75,7 +75,7 @@ namespace nil {
                     const std::vector<typename FieldType::value_type> &get(std::size_t batch_id, std::size_t poly_id) const{
                         return z.at(batch_id)[poly_id];
                     }
-                    typename FieldType::value_type get(std::size_t batch_id, std::size_t poly_id, size_t point_id) const{
+                    const typename FieldType::value_type &get(std::size_t batch_id, std::size_t poly_id, size_t point_id) const{
                         return z.at(batch_id)[poly_id][point_id];
                     }
 
@@ -87,7 +87,7 @@ namespace nil {
                         z[batch_id][poly_id] = {};
                         z[batch_id][poly_id].resize(points_number);
                     }
-                    void set(std::size_t batch_id, std::size_t poly_id, size_t point_id, const typename FieldType::value_type value){
+                    void set(std::size_t batch_id, std::size_t poly_id, size_t point_id, const &typename FieldType::value_type value){
                         z[batch_id][poly_id][point_id] = value;
                     }
                 };
