50707f825548ba2c5f3990d18db8049ba4abf5cc
Mikhail Komarov <nemo@nil.foundation>
2024-05-05 18:53:15 +0300

    Unused modifiers removed #67

--- include/nil/crypto3/math/domains/basic_radix2_domain.hpp
+++ include/nil/crypto3/math/domains/basic_radix2_domain.hpp
@@ -61,7 +61,7 @@ namespace nil {
             public:
                 typedef FieldType field_type;
 
-                const field_value_type omega;
+                field_value_type omega;
 
                 basic_radix2_domain(const std::size_t m)
                         : evaluation_domain<FieldType, ValueType>(m),
