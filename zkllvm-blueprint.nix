99a107757f8ee4a1ad4e7a8646641609fc10dbe1
Andrei Malashkin <andrei.malashkin@nil.foundation>
2024-07-31 16:36:08 +0200

    move test run script on bash level; don't generate tests report

--- zkllvm-blueprint.nix
+++ zkllvm-blueprint.nix
@@ -28,76 +28,6 @@ in stdenv.mkDerivation rec {
 
   buildInputs = [cmake_modules crypto3];
 
-  testList = [
-    "blueprint_algebra_fields_plonk_field_operations_test"
-    "blueprint_algebra_fields_plonk_exponentiation_test"
-    "blueprint_algebra_curves_plonk_unified_addition_test"
-    "blueprint_algebra_curves_plonk_variable_base_scalar_mul_test"
-    "blueprint_verifiers_kimchi_sponge_oracles_test"
-    "blueprint_hashes_plonk_poseidon_test"
-    "blueprint_algebra_curves_plonk_endo_scalar_test"
-    "blueprint_algebra_fields_plonk_range_check_test"
-    "blueprint_algebra_fields_plonk_logic_and_flag_test"
-    "blueprint_algebra_fields_plonk_logic_or_flag_test"
-    "blueprint_algebra_fields_plonk_interpolation_test"
-    "blueprint_algebra_fields_plonk_non_native_addition_test"
-    "blueprint_algebra_fields_plonk_non_native_subtraction_test"
-    "blueprint_algebra_fields_plonk_non_native_multiplication_test"
-    "blueprint_algebra_fields_plonk_non_native_range_test"
-    "blueprint_algebra_fields_plonk_non_native_reduction_test"
-    "blueprint_algebra_fields_plonk_non_native_bit_decomposition_test"
-    "blueprint_algebra_fields_plonk_non_native_bit_composition_test"
-    "blueprint_algebra_fields_plonk_non_native_bit_shift_constant_test"
-    "blueprint_algebra_fields_plonk_non_native_logic_ops_test"
-    "blueprint_algebra_fields_plonk_non_native_lookup_logic_ops_test"
-    "blueprint_algebra_fields_plonk_non_native_comparison_checked_test"
-    "blueprint_algebra_fields_plonk_non_native_comparison_unchecked_test"
-    "blueprint_algebra_fields_plonk_non_native_comparison_flag_test"
-    "blueprint_algebra_fields_plonk_non_native_equality_flag_test"
-    "blueprint_algebra_fields_plonk_non_native_division_remainder_test"
-    #blueprint_non_native_plonk_scalar_non_native_range_test, TODO: enable once fixed.
-    "blueprint_non_native_plonk_bool_scalar_multiplication_test"
-    "blueprint_non_native_plonk_add_mul_zkllvm_compatible_test"
-    "blueprint_hashes_plonk_decomposition_test"
-    "blueprint_verifiers_placeholder_fri_cosets_test"
-    "blueprint_hashes_plonk_sha256_process_test"
-    "blueprint_hashes_plonk_sha512_process_test"
-    "blueprint_hashes_plonk_sha256_test"
-    "blueprint_hashes_plonk_sha512_test"
-    "blueprint_algebra_fields_plonk_sqrt_test"
-    "blueprint_verifiers_placeholder_fri_lin_inter_test"
-    "blueprint_verifiers_placeholder_fri_array_swap_test"
-    "blueprint_manifest_test"
-    "blueprint_detail_huang_lu_test"
-    "blueprint_private_input_test"
-    "blueprint_verifiers_placeholder_permutation_argument_verifier_test"
-    "blueprint_verifiers_placeholder_gate_argument_verifier_test"
-    "blueprint_verifiers_placeholder_lookup_argument_verifier_test"
-    "blueprint_verifiers_placeholder_f1_loop_test"
-    "blueprint_verifiers_placeholder_f3_loop_test"
-    "blueprint_verifiers_placeholder_gate_component_test"
-    "blueprint_verifiers_flexible_pow_factor_test"
-    "blueprint_proxy_test"
-    #blueprint_mock_mocked_components_test, TODO: Enable after code and test re-written.
-    "blueprint_component_batch_test"
-    "blueprint_verifiers_placeholder_expression_evaluation_component_test"
-    "blueprint_verifiers_placeholder_final_polynomial_check_test"
-    "blueprint_verifiers_flexible_swap_test"
-    "blueprint_verifiers_flexible_additions_test"
-    "blueprint_verifiers_flexible_multiplications_test"
-    "blueprint_verifiers_flexible_poseidon_test"
-    "blueprint_verifiers_flexible_constant_pow_test"
-    "blueprint_verifiers_placeholder_verifier_test"
-    "blueprint_zkevm_zkevm_word_test"
-    "blueprint_zkevm_bytecode_test"
-    "blueprint_zkevm_state_selector_test"
-    "blueprint_zkevm_state_transition_test"
-    "blueprint_zkevm_opcodes_iszero_test"
-    "blueprint_zkevm_opcodes_add_sub_test"
-    "blueprint_zkevm_opcodes_mul_test"
-    "blueprint_zkevm_opcodes_div_test"
-  ];
-
   cmakeFlags =
   [
       (if runTests then "-DBUILD_TESTS=TRUE" else "")
@@ -105,20 +35,19 @@ in stdenv.mkDerivation rec {
       (if enableDebug then "-DCMAKE_BUILD_TYPE=Debug" else "-DCMAKE_BUILD_TYPE=Release")
       (if enableDebug then "-DCMAKE_CXX_FLAGS=-ggdb" else "")
       (if enableDebug then "-DCMAKE_CXX_FLAGS=-O0" else "")
+      "-G Ninja"
   ];
-  
-  ninjaFlags = lib.strings.concatStringsSep " " (["-k 0"] ++ testList);
 
+  doBuild = false;
   doCheck = runTests;
   dontInstall = true;
 
-  checkPhase = ''
-    # JUNIT file without explicit file name is generated after the name of the master test suite inside `CMAKE_CURRENT_SOURCE_DIR` (/build/source)
-    export BOOST_TEST_LOGGER=JUNIT:HRF
-    ctest --verbose -j $NIX_BUILD_CORES --output-on-failure -R "${lib.concatStringsSep "|" testList}" || true
+  buildPhase = ''
+    echo "skip build"
+  '';
 
-    mkdir -p ${placeholder "out"}/test-logs
-    find .. -type f -name '*_test.xml' -exec cp {} ${placeholder "out"}/test-logs \;
+  checkPhase = ''
+    bash ../run_tests.sh
   '';
 
   shellHook = ''
